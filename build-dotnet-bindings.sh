ea0e13b04d7a777b3cd8c3882ce04df3f6645231
Michael Pollmeier <michael@michaelpollmeier.com>
2018-07-27 14:49:12 +0200

    wire up nuget script with sbt

---
+++ build-dotnet-bindings.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+usage() { echo "Usage: $0 --cpg-version <string> [--publish-to-repo <string>] [--publish-key <string>]" 1>&2; exit 1; }
+
+ARGS=`getopt -o v:r:k: --long cpg-version:,publish-to-repo:,publish-key: -- "$@"`
+eval set -- "$ARGS"
+while true ; do
+    case "$1" in
+        -v|--cpg-version)
+            CPG_VERSION=$2 ; shift 2 ;;
+        -r|--publish-to-repo)
+            PUBLISH_TO_REPO=$2 ; shift 2 ;;
+        -k|--publish-key)
+            PUBLISH_KEY=$2 ; shift 2 ;;
+        --) shift ; break ;;
+        *) echo "Internal error!" ; usage ;;
+    esac
+done
+
+if [ -z "$CPG_VERSION" ]; then
+    echo "error: cpg version not defined!"
+    usage
+fi
+
+echo "generating csharp protoc bindings for cpg.proto"
+rm -rf target/csharp
+mkdir -p target/csharp/io/shiftleft/proto
+cp codepropertygraph/target/resource_managed/main/cpg.proto target/csharp
+cp proto-bindings/cpg-proto-bindings.csproj target/csharp
+cd target/csharp
+protoc --csharp_out=io/shiftleft/proto cpg.proto
+
+echo "packaging as v$CPG_VERSION"
+dotnet pack cpg-proto-bindings.csproj -o . /p:Version=$CPG_VERSION
+PKG_FILE=cpg-proto-bindings.${CPG_VERSION}.nupkg
+echo "generated file: $PKG_FILE"
+
+if [ -n "$PUBLISH_TO_REPO" ] && [ -n "$PUBLISH_KEY" ]; then
+    echo "publishing nuget package to $PUBLISH_TO_REPO with provided api key..."
+    dotnet nuget push cpg-proto-bindings.${CPG_VERSION}.nupkg --source $PUBLISH_TO_REPO --api-key $PUBLISH_KEY
+fi
+
+echo "all done"
--- proto-bindings/build.sbt
+++ proto-bindings/build.sbt
@@ -20,3 +20,18 @@ copyLatestCpgProto := {
   java.nio.file.Files.copy(protoFile.toPath, targetFile.toPath)
 }
 
+/** generate csharp bindings for proto file as a nuget package
+  * 
+  * if `publish-to-repo` and `publish-key` are specified, it will also publish the package, e.g. like this:
+  * `sbt -Dpublish-to-repo=https://shiftleft.jfrog.io/shiftleft/api/nuget/nuget-release-local -Dpublish-key=michael:xxxxxxxxxx generateCsharpBindings`
+  */
+lazy val generateCsharpBindings = taskKey[File]("generate csharp proto bindings")
+generateCsharpBindings := {
+  (codepropertygraph/generateProtobuf).value //ensures this is being run beforehand
+  import sys.process._
+  val dotnetVersion = version.value.replaceAll("\\+", "-") //dotnet is more restrictive with version names
+  val publishToRepo = Option(System.getProperty("publish-to-repo")).map(repo => s"--publish-to-repo $repo")
+  val publishKey = Option(System.getProperty("publish-key")).map(key => s"--publish-key $key")
+  s"""./build-dotnet-bindings.sh --cpg-version $dotnetVersion ${publishToRepo.getOrElse("")} ${publishKey.getOrElse("")}""".!
+  new File(s"cpg-proto-bindings.${dotnetVersion}.nupkg")
+}
