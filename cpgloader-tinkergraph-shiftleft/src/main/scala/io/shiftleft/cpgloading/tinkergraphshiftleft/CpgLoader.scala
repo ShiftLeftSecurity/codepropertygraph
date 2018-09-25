6ce1d67d4569b0566894576066f233fe97bd7a7d
Michael Pollmeier <michael@michaelpollmeier.com>
2018-09-25 15:15:50 +0200

    refactor: abstract CpgLoaderBase

--- cpgloader-tinkergraph-shiftleft/src/main/scala/io/shiftleft/cpgloading/tinkergraphshiftleft/CpgLoader.scala
+++ cpgloader-tinkergraph-shiftleft/src/main/scala/io/shiftleft/cpgloading/tinkergraphshiftleft/CpgLoader.scala
@@ -1,23 +1,11 @@
 package io.shiftleft.cpgloading.tinkergraphshiftleft
 
-import io.shiftleft.cpgenhancements.CpgEnhancements
-import io.shiftleft.cpgloading.{ProtoCpgLoader, ProtoToTinkerShiftleftCpg}
-import io.shiftleft.queryprimitives.steps.starters.Cpg
-
+import io.shiftleft.cpgloading.{CpgLoaderBase, ProtoToTinkerShiftleftCpg}
 
 /* singleton instance for convenience */
 object CpgLoader extends CpgLoader
 
 /** Load cpg proto (typically cpg.bin.zip) into a new Tinkergraph instance */
-class CpgLoader {
-
-  def loadCodePropertyGraph(filename: String, runEnhancements: Boolean = true): Cpg = {
-    val builder = new ProtoToTinkerShiftleftCpg
-    val cpg = new ProtoCpgLoader(builder).loadFromProtoZip(filename)
-    if (runEnhancements) {
-      CpgEnhancements.run(cpg.graph)
-    }
-    cpg
-  }
-
+class CpgLoader extends CpgLoaderBase {
+  override def builder = new ProtoToTinkerShiftleftCpg
 }
--- cpgloader-tinkergraph/src/main/scala/io/shiftleft/cpgloading/tinkergraph/CpgLoader.scala
+++ cpgloader-tinkergraph/src/main/scala/io/shiftleft/cpgloading/tinkergraph/CpgLoader.scala
@@ -1,23 +1,11 @@
 package io.shiftleft.cpgloading.tinkergraph
 
-import io.shiftleft.cpgenhancements.CpgEnhancements
-import io.shiftleft.cpgloading.{ProtoCpgLoader, ProtoToTinkerCpg}
-import io.shiftleft.queryprimitives.steps.starters.Cpg
-
+import io.shiftleft.cpgloading.{CpgLoaderBase, ProtoToTinkerCpg}
 
 /* singleton instance for convenience */
 object CpgLoader extends CpgLoader
 
 /** Load cpg proto (typically cpg.bin.zip) into a new Tinkergraph instance */
-class CpgLoader {
-
-  def loadCodePropertyGraph(filename: String, runEnhancements: Boolean = true): Cpg = {
-    val builder = new ProtoToTinkerCpg
-    val cpg = new ProtoCpgLoader(builder).loadFromProtoZip(filename)
-    if (runEnhancements) {
-      CpgEnhancements.run(cpg.graph)
-    }
-    cpg
-  }
-
+class CpgLoader extends CpgLoaderBase {
+  override def builder = new ProtoToTinkerCpg
 }
