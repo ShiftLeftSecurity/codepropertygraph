b8f9fc9ae0c5a883d7a8b9b95baa9ee51d7a6657
Michael Pollmeier <michael@michaelpollmeier.com>
2018-10-31 16:48:36 +1300

    enforcing full schema strictness too hard for now, allowing nulls again

--- project/DomainClassCreator.scala
+++ project/DomainClassCreator.scala
@@ -819,11 +819,15 @@ object Utils {
 
       getHigherType(property) match {
         case HigherValueType.None =>
+        /** TODO: rather than returning `null`, throw an exception, since this is a schema violation:
           s"""|var _$name: $tpe = null
-              |def $name: $tpe = 
+              |def $name: $tpe =
               |  if (_$name == null) {
               |    throw new AssertionError("property $name is mandatory but hasn't been initialised yet")
-              |} else { _$name }""".stripMargin
+              |} else { _$name } """.stripMargin
+          */
+          s"""|var _$name: $tpe = null
+              |def $name: $tpe = _$name""".stripMargin
         case HigherValueType.Option =>
           s"""var $name: $tpe = None"""
         case HigherValueType.List =>
