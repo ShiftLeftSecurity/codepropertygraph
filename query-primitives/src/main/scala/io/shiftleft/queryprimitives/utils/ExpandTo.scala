6ad786385b00cd6c9fa00392e8cf688f318e06e6
Markus Lottmann <markus@shiftleft.io>
2019-03-15 12:09:24 +0100

    Update ExpandTo.

--- query-primitives/src/main/scala/io/shiftleft/queryprimitives/utils/ExpandTo.scala
+++ query-primitives/src/main/scala/io/shiftleft/queryprimitives/utils/ExpandTo.scala
@@ -8,18 +8,23 @@ import org.apache.tinkerpop.gremlin.structure.{Direction, Vertex}
 import scala.collection.JavaConverters._
 
 object ExpandTo {
-  // The call target is for java always an object instance.
+  // The call receiver is for java always an object instance.
   // For languages which make use of function pointer this can also be the
   // pointer itself.
-  def callTargetOption(callNode: Vertex): Option[Vertex] = {
+  def callReceiverOption(callNode: Vertex): Option[Vertex] = {
     callNode
-      .vertices(Direction.OUT, EdgeTypes.AST)
+      .vertices(Direction.OUT, EdgeTypes.RECEIVER)
       .asScala
-      .find(_.value2(NodeKeys.ARGUMENT_INDEX) == 0)
+      .toList
+      .headOption
   }
 
-  def callTarget(callNode: Vertex): Vertex = {
-    callTargetOption(callNode).get
+  def callReceiver(callNode: Vertex): Vertex = {
+    callReceiverOption(callNode).get
+  }
+
+  def typeCarrierToType(parameterNode: Vertex): Vertex = {
+    parameterNode.vertices(Direction.OUT, EdgeTypes.EVAL_TYPE).nextChecked
   }
 
   def parameterToMethod(parameterNode: Vertex): Vertex = {
@@ -28,6 +33,14 @@ object ExpandTo {
 
   def formalReturnToMethod(formalReturnNode: Vertex): Vertex = {
     formalReturnNode.vertices(Direction.IN, EdgeTypes.AST).nextChecked
+  }
+
+  def methodToFormalReturn(method: Vertex): Vertex = {
+    method
+      .vertices(Direction.OUT, EdgeTypes.AST)
+      .asScala
+      .filter(_.isInstanceOf[nodes.MethodReturn])
+      .next()
   }
 
   def expressionToMethod(expression: Vertex): Vertex = {
@@ -49,5 +62,31 @@ object ExpandTo {
 
   def astParent(expression: Vertex): Vertex = {
     expression.vertices(Direction.IN, EdgeTypes.AST).nextChecked
+  }
+
+  def callToCalledMethod(call: Vertex): Seq[nodes.Method] = {
+    call
+      .vertices(Direction.OUT, EdgeTypes.CALL)
+      .asScala
+      .map(methodInst =>
+        methodInst.vertices(Direction.OUT, EdgeTypes.REF).nextChecked.asInstanceOf[nodes.Method])
+      .toSeq
+  }
+
+  def methodToTypeDecl(method: Vertex): Option[Vertex] = {
+    var typeDeclOption = method.vertices(Direction.IN, EdgeTypes.AST).asScala.toList.headOption
+    while (typeDeclOption.isDefined && !typeDeclOption.get.isInstanceOf[nodes.TypeDecl]) {
+      typeDeclOption =
+        typeDeclOption.get.vertices(Direction.IN, EdgeTypes.AST).asScala.toList.headOption
+    }
+    typeDeclOption
+  }
+
+  def methodToFile(method: Vertex): Option[Vertex] = {
+    var fileOption = method.vertices(Direction.IN, EdgeTypes.AST).asScala.toList.headOption
+    while (fileOption.isDefined && !fileOption.get.isInstanceOf[nodes.File]) {
+      fileOption = fileOption.get.vertices(Direction.IN, EdgeTypes.AST).asScala.toList.headOption
+    }
+    fileOption
   }
 }
