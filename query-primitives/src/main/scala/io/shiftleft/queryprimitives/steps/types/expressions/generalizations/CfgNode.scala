0fb085a62935b6f337eb7695bac8e6119d8eeee1
Michael Pollmeier <michael@michaelpollmeier.com>
2019-03-29 11:18:38 +1300

    move more of schema to cpg-public

--- query-primitives/src/main/scala/io/shiftleft/queryprimitives/steps/types/expressions/generalizations/CfgNode.scala
+++ query-primitives/src/main/scala/io/shiftleft/queryprimitives/steps/types/expressions/generalizations/CfgNode.scala
@@ -1,9 +1,10 @@
-package io.shiftleft.queryprimitives.steps.types.expressions.generalizations
+// TODO markus?
+// package io.shiftleft.queryprimitives.steps.types.expressions.generalizations
 
-import gremlin.scala.GremlinScala
-import io.shiftleft.codepropertygraph.generated.nodes
-import io.shiftleft.queryprimitives.steps.NodeSteps
-import shapeless.HList
+// import gremlin.scala.GremlinScala
+// import io.shiftleft.codepropertygraph.generated.nodes
+// import io.shiftleft.queryprimitives.steps.NodeSteps
+// import shapeless.HList
 
 /*
 class CfgNode[Labels <: HList](raw: GremlinScala.Aux[nodes.CfgNode, Labels])
