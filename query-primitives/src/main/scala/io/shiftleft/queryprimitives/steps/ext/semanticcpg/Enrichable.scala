382b63b491105433da0ccf2b076f5d5c018c7064
Michael Pollmeier <michael@michaelpollmeier.com>
2019-04-16 11:33:07 +1200

    add extension hook for securityprofile and semanticcpg
    re https://github.com/ShiftLeftSecurity/ocular/issues/278

---
+++ query-primitives/src/main/scala/io/shiftleft/queryprimitives/steps/ext/semanticcpg/Enrichable.scala
@@ -0,0 +1,11 @@
+package io.shiftleft.queryprimitives.steps.ext.semanticcpg
+
+/** Sole purpose of this trait is to allow to enrich ('pimp') this DSL without the need of additional
+  * imports a la `import x.y.z.Implicits._`. We're making use of the fact that package objects of
+  * inherited traits are automatically part of implicit scope. This way, these extensions also show
+  * up in scaladoc.
+  *
+  * Important: this project should *not* define anything in the `ext` package object. If it does,
+  * it will be shadowed by enriching libraries.
+  */
+trait Enrichable
