15939cf7eab5e81740eaa8799237a38cd60934cc
behrad <behrad@shiftleft.io>
2019-04-25 18:36:06 +0200

    added test for flow after short hand assignment

--- cpgqueryingtests/src/test/scala/io/shiftleft/cpgqueryingtests/steps/CDataFlowTests.scala
+++ cpgqueryingtests/src/test/scala/io/shiftleft/cpgqueryingtests/steps/CDataFlowTests.scala
@@ -431,4 +431,37 @@ class CDataFlowTests extends CpgDataFlowTests {
            ("z+=a",  6)
        ))
    }
+
+  "Test 13: flow after short hand assignment" in {
+    val cpg = cpgFactory.buildCpg(
+      """
+        | void flow(void) {
+        |    int a = 0x37;
+        |    int b = a;
+        |    int z = b;
+        |    z+=a;
+        |    int w = z;
+        | }
+      """.stripMargin
+    )
+    val source = cpg.call.code("a = 0x37")
+    val sink = cpg.identifier.name("w")
+    val flows = sink.reachableByFlows(source).l
+
+    flows.size shouldBe 2
+
+    flows.map(flow => flowToResultPairs(flow)).toSet shouldBe
+      Set(List[(String, Option[Integer])](
+        ("a = 0x37", 3),
+        ("b = a", 4),
+        ("z = b", 5),
+        ("z+=a",  6),
+        ("w = z", 7)
+      ),
+      List[(String, Option[Integer])](
+          ("a = 0x37", 3),
+          ("z+=a",  6),
+          ("w = z", 7)
+      ))
+  }
 }
