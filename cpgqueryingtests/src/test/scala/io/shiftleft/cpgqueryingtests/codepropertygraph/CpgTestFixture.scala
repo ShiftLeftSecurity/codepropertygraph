2d6380977b6106c2a4b5db34f8e57e7445c35a6e
Markus Lottmann <markus@shiftleft.io>
2019-04-25 18:53:25 +0200

    Load argument definition semantics from file instead of hard coding it. (#133)

--- cpgqueryingtests/src/test/scala/io/shiftleft/cpgqueryingtests/codepropertygraph/CpgTestFixture.scala
+++ cpgqueryingtests/src/test/scala/io/shiftleft/cpgqueryingtests/codepropertygraph/CpgTestFixture.scala
@@ -4,7 +4,7 @@ import gremlin.scala._
 import io.shiftleft.cpgloading.CpgLoader
 
 case class CpgTestFixture(projectName: String) {
-  lazy val cpg = CpgLoader.loadCodePropertyGraph(s"resources/cpgs/$projectName/cpg.bin.zip")
+  lazy val cpg = CpgLoader.loadCodePropertyGraph(s"resources/cpgs/$projectName/cpg.bin.zip", argDefFilename = None)
   implicit val graph: Graph = cpg.graph
   lazy val scalaGraph: ScalaGraph = graph.asScala
 }
--- semanticcpg/src/main/scala/io/shiftleft/passes/propagateedges/PropagateEdgePass.scala
+++ semanticcpg/src/main/scala/io/shiftleft/passes/propagateedges/PropagateEdgePass.scala
@@ -1,6 +1,7 @@
 package io.shiftleft.passes.propagateedges
 
 import gremlin.scala._
+import io.shiftleft.argdefloader.ArgumentDefs
 import io.shiftleft.codepropertygraph.generated._
 import io.shiftleft.diffgraph.DiffGraph
 import io.shiftleft.passes.CpgPass
@@ -9,46 +10,21 @@ import org.apache.tinkerpop.gremlin.structure.Direction
 import scala.collection.JavaConverters._
 
 /**
-  * Create PROPAGATE edges.
-  * Currently we only do this for assignment operators to encode their data flow semantics.
+  * Create PROPAGATE edges which mark parameters defined by a method.
   */
-class PropagateEdgePass(graph: ScalaGraph) extends CpgPass(graph) {
+class PropagateEdgePass(graph: ScalaGraph, argumentDefs: ArgumentDefs) extends CpgPass(graph) {
   var dstGraph: DiffGraph = _
 
   override def run(): Stream[DiffGraph] = {
     dstGraph = new DiffGraph()
-    val assignmentMethodOption =
-      graph.V().hasLabel(NodeTypes.METHOD).has(NodeKeys.FULL_NAME -> Operators.assignment).headOption()
 
-    assignmentMethodOption match {
-      case Some(method) =>
-        addAssignmentSemantic(method)
-      case None =>
-    }
+    argumentDefs.elements.foreach { argDef =>
+      val methodOption =
+        graph.V().hasLabel(NodeTypes.METHOD).has(NodeKeys.FULL_NAME -> argDef.methodFullName).headOption()
 
-    val combinedAssignmentOperators =
-      List(
-        Operators.assignmentAnd,
-        Operators.assignmentArithmeticShiftRight,
-        Operators.assignmentDivision,
-        Operators.assignmentExponentiation,
-        Operators.assignmentLogicalShifRight,
-        Operators.assignmentMinus,
-        Operators.assignmentModulo,
-        Operators.assignmentMultiplication,
-        Operators.assignmentOr,
-        Operators.assignmentPlus,
-        Operators.assignmentShiftLeft,
-        Operators.assignmentXor
-      )
-
-    combinedAssignmentOperators.foreach { operatorFullName =>
-      val operatorMethodOption =
-        graph.V().hasLabel(NodeTypes.METHOD).has(NodeKeys.FULL_NAME -> operatorFullName).headOption()
-
-      operatorMethodOption match {
+      methodOption match {
         case Some(method) =>
-          addCombinedAssignmentSemantic(method)
+          addSelfDefSemantic(method, argDef.parameterIndex)
         case None =>
       }
     }
@@ -56,72 +32,26 @@ class PropagateEdgePass(graph: ScalaGraph) extends CpgPass(graph) {
     Stream(dstGraph)
   }
 
-  private def addAssignmentSemantic(method: Vertex): Unit = {
-    val secondParameterInOption = method
+  private def addSelfDefSemantic(method: Vertex, parameterIndex: Int): Unit = {
+    // From where the PROPAGATE edge is coming does not matter for the open source reachable by.
+    // Thus we let it start from the corresponding METHOD_PARAMETER_IN.
+    val parameterInOption = method
       .vertices(Direction.OUT, EdgeTypes.AST)
       .asScala
-      .find(node => node.label() == NodeTypes.METHOD_PARAMETER_IN && node.value2(NodeKeys.ORDER) == 2)
-    val firstParameterOutOption = method
-      .vertices(Direction.OUT, EdgeTypes.AST)
-      .asScala
-      .find(node => node.label() == NodeTypes.METHOD_PARAMETER_OUT && node.value2(NodeKeys.ORDER) == 1)
-    val methodReturnOption = method
-      .vertices(Direction.OUT, EdgeTypes.AST)
-      .asScala
-      .find(_.label() == NodeTypes.METHOD_RETURN)
+      .find(node => node.label() == NodeTypes.METHOD_PARAMETER_IN && node.value2(NodeKeys.ORDER) == parameterIndex)
 
-    if (secondParameterInOption.nonEmpty) {
-      if (firstParameterOutOption.nonEmpty) {
-        if (methodReturnOption.nonEmpty) {
-          addPropagateEdge(secondParameterInOption.get, firstParameterOutOption.get, isAlias = true)
-          addPropagateEdge(secondParameterInOption.get, methodReturnOption.get, isAlias = true)
-        } else {
-          logger.warn(s"Could not find method return of ${method.value2(NodeKeys.FULL_NAME)}.")
-        }
-      } else {
-        logger.warn(s"Could not find first output parameter of ${method.value2(NodeKeys.FULL_NAME)}.")
-      }
-    } else {
-      logger.warn(s"Could not find second input parameter of ${method.value2(NodeKeys.FULL_NAME)}.")
-    }
-  }
+    val parameterOutOption = method
+      .vertices(Direction.OUT, EdgeTypes.AST)
+      .asScala
+      .find(node => node.label() == NodeTypes.METHOD_PARAMETER_OUT && node.value2(NodeKeys.ORDER) == parameterIndex)
 
-  private def addCombinedAssignmentSemantic(method: Vertex): Unit = {
-    val secondParameterInOption = method
-      .vertices(Direction.OUT, EdgeTypes.AST)
-      .asScala
-      .find(node => node.label() == NodeTypes.METHOD_PARAMETER_IN && node.value2(NodeKeys.ORDER) == 2)
-    val firstParameterOutOption = method
-      .vertices(Direction.OUT, EdgeTypes.AST)
-      .asScala
-      .find(node => node.label() == NodeTypes.METHOD_PARAMETER_OUT && node.value2(NodeKeys.ORDER) == 1)
-    val methodReturnOption = method
-      .vertices(Direction.OUT, EdgeTypes.AST)
-      .asScala
-      .find(_.label() == NodeTypes.METHOD_RETURN)
-    val firstParameterInOption = method
-      .vertices(Direction.OUT, EdgeTypes.AST)
-      .asScala
-      .find(node => node.label() == NodeTypes.METHOD_PARAMETER_IN && node.value2(NodeKeys.ORDER) == 1)
-
-    if (secondParameterInOption.nonEmpty) {
-      if (firstParameterOutOption.nonEmpty) {
-        if (methodReturnOption.nonEmpty) {
-          if (firstParameterInOption.nonEmpty) {
-            addPropagateEdge(firstParameterInOption.get, firstParameterOutOption.get, isAlias = false)
-            addPropagateEdge(secondParameterInOption.get, firstParameterOutOption.get, isAlias = false)
-            addPropagateEdge(secondParameterInOption.get, methodReturnOption.get, isAlias = false)
-          } else {
-            logger.warn(s"Could not find first input parameter of ${method.value2(NodeKeys.FULL_NAME)}.")
-          }
-        } else {
-          logger.warn(s"Could not find method return of ${method.value2(NodeKeys.FULL_NAME)}.")
-        }
-      } else {
-        logger.warn(s"Could not find first output parameter of ${method.value2(NodeKeys.FULL_NAME)}.")
-      }
-    } else {
-      logger.warn(s"Could not find second input parameter of ${method.value2(NodeKeys.FULL_NAME)}.")
+    (parameterInOption, parameterOutOption) match {
+      case (Some(parameterIn), Some(parameterOut)) =>
+        addPropagateEdge(parameterIn, parameterOut, isAlias = false)
+      case (None, _) =>
+        logger.warn(s"Could not find parameter $parameterIndex of ${method.value2(NodeKeys.FULL_NAME)}.")
+      case _ =>
+        logger.warn(s"Could not find output parameter $parameterIndex of ${method.value2(NodeKeys.FULL_NAME)}.")
     }
   }
 
