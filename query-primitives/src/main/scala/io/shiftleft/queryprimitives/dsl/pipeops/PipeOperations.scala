06075f638e1bf26469b3713e630e090257e8c857
Markus Lottmann <markus@shiftleft.io>
2019-04-29 12:05:51 +0200

    Continue.

--- query-primitives/src/main/scala/io/shiftleft/queryprimitives/dsl/pipeops/PipeOperations.scala
+++ query-primitives/src/main/scala/io/shiftleft/queryprimitives/dsl/pipeops/PipeOperations.scala
@@ -1,12 +1,10 @@
 package io.shiftleft.queryprimitives.dsl.pipeops
 
-import io.shiftleft.queryprimitives.dsl.{RealPipe, ShallowPipe}
+import io.shiftleft.queryprimitives.dsl.RealPipe
 
 import scala.collection.GenTraversableOnce
 
-// TODO at Micha: It should be PipeType[+_] as type parameter. But i guess due to type erasure that is
-// not handled because the compiler does not start complaining about countervariant parameters.
-trait PipeOperations[PipeType[_]] {
+trait PipeOperations[PipeType[+_]] {
   def toRealPipe[ElemType](pipe: PipeType[ElemType]): RealPipe[ElemType]
 
   def map[SrcType, DstType](pipe: PipeType[SrcType],
@@ -27,38 +25,38 @@ trait PipeOperations[PipeType[_]] {
 
   def iterator[ElemType](pipe: PipeType[ElemType]): Iterator[ElemType]
 
-  def map[ElemType](pipe: PipeType[ElemType],
-                    function: ElemType => ElemType,
-                    times: Int): PipeType[ElemType] = {
-    var currentPipe = pipe
+  def map[ElemType, SuperType >: ElemType](pipe: PipeType[ElemType],
+                                           function: SuperType => SuperType,
+                                           times: Int): PipeType[SuperType] = {
+    var currentPipe: PipeType[SuperType] = pipe
     for (_ <- 0 until times) {
       currentPipe = map(pipe, function)
     }
     currentPipe
   }
 
-  def map[ElemType](pipe: PipeType[ElemType],
-                    function: ElemType => ElemType,
-                    until: ElemType => Boolean): PipeType[ElemType] = {
+  def map[ElemType, SuperType >: ElemType](pipe: PipeType[ElemType],
+                                           function: SuperType => SuperType,
+                                           until: SuperType => Boolean): PipeType[SuperType] = {
     // TODO implement
     pipe
   }
 
-  def flatMap[ElemType](pipe: PipeType[ElemType],
-                        function: ElemType => GenTraversableOnce[ElemType],
-                        times: Int): RealPipe[ElemType] = {
-    var currentPipe = toRealPipe(pipe)
+  def flatMap[ElemType, SuperType >: ElemType](pipe: PipeType[ElemType],
+                                               function: SuperType => GenTraversableOnce[SuperType],
+                                               times: Int): RealPipe[SuperType] = {
+    var currentPipe: RealPipe[SuperType] = toRealPipe(pipe)
     for (_ <- 0 until times) {
       currentPipe = flatMap2(pipe, function)
     }
     currentPipe
   }
 
-  def flatMap3[ElemType](pipe: PipeType[ElemType],
-                         function: ElemType => GenTraversableOnce[ElemType],
-                         until: ElemType => Boolean): RealPipe[ElemType] = {
-    var stack = toList(pipe)
-    var builder = RealPipe.newBuilder[ElemType]
+  def flatMap3[ElemType, SuperType >: ElemType](pipe: PipeType[ElemType],
+                                                function: SuperType => GenTraversableOnce[SuperType],
+                                                until: SuperType => Boolean): RealPipe[SuperType] = {
+    var stack: List[SuperType] = toList(pipe)
+    var builder = RealPipe.newBuilder[SuperType]
 
     while (stack.nonEmpty) {
       val elem = stack.head
--- query-primitives/src/test/scala/io/shiftleft/queryprimitives/dsl/DslTests.scala
+++ query-primitives/src/test/scala/io/shiftleft/queryprimitives/dsl/DslTests.scala
@@ -5,25 +5,30 @@ import io.shiftleft.queryprimitives.dsl.ShallowPipe.ShallowPipe
 import io.shiftleft.queryprimitives.dsl.pipeops.PipeOperations
 import org.scalatest.{Matchers, WordSpec}
 
-case class A(a: Int = 0)
+class A(a: Int = 0)
+class ADerived() extends A
 
-case class B(b: Int = 0)
+class B(b: Int = 0)
+class BDerived() extends B
 
 class NodeAMethods[PipeType[+_]](val pipe: PipeType[A]) extends AnyVal {
   def toB(implicit ops: PipeOperations[PipeType]): PipeType[B] = {
-    ops.map(pipe, (x: A) => B())
+    ops.map(pipe, (x: A) => new B())
   }
   def toMultipleB(implicit ops: PipeOperations[PipeType]): RealPipe[B] = {
-    ops.flatMap2(pipe, (x: A) => B() :: Nil)
+    ops.flatMap2(pipe, (x: A) => new B() :: Nil)
   }
 }
 
 class NodeBMethods[PipeType[+_]](val pipe: PipeType[B]) extends AnyVal {
   def toA(implicit ops: PipeOperations[PipeType]): PipeType[A] = {
-    ops.map(pipe, (x: B) => A())
+    ops.map(pipe, (x: B) => new A())
   }
   def toMultipleA(implicit ops: PipeOperations[PipeType]): RealPipe[A] = {
-    ops.flatMap2(pipe, (x: B) => A() :: Nil)
+    ops.flatMap2(pipe, (x: B) => new A() :: Nil)
+  }
+  def toAMapTimesX(implicit ops: PipeOperations[PipeType]): PipeType[B] = {
+    ops.map(pipe, (x: B) => new BDerived(), 2)
   }
 }
 
@@ -53,32 +58,32 @@ class DslTests extends WordSpec with Matchers {
   import DslTests._
 
   "Be able to call DSL methods on shallow pipe." in {
-    A().toB shouldBe B()
+    new A().toB shouldBe new B()
   }
 
   "Be able to call multiple DSL methods on shallow pipe." in {
-    A().toB.toA shouldBe A()
+    new A().toB.toA shouldBe new A()
   }
 
   // TODO check/fix "real pipe" in test description is still the correct term.
   "Be able to call multiple DSL methods on real pipe." in {
-    A().toMultipleB.toMultipleA.head shouldBe A()
+    new A().toMultipleB.toMultipleA.head shouldBe new A()
   }
 
   "Be able to use map." in {
-    A().toMultipleB.map(_.toA).head shouldBe A()
+    new A().toMultipleB.map(_.toA).head shouldBe new A()
   }
 
   "Be able to use map with repeat function." in {
-    A().toMultipleB.map(_.toA.toB, 1).head shouldBe B()
+    new A().toMultipleB.map(_.toA.toB, 1).head shouldBe new B()
   }
 
   "Be able to use flatMap on real pipe function." in {
-    A().toMultipleB.flatMap(_.toMultipleA).head shouldBe A()
+    new A().toMultipleB.flatMap(_.toMultipleA).head shouldBe new A()
   }
 
   "Be able to use flatMap on GenTraversableOnce function." in {
-    A().toMultipleB.flatMap2(_ => A()::Nil).head shouldBe A()
+    new A().toMultipleB.flatMap2(_ => new A()::Nil).head shouldBe new A()
   }
 
 }
