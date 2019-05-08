fc3bd69641ff146dd728cf2fc4b5ed13e6afb09d
Markus Lottmann <markus@shiftleft.io>
2019-05-08 13:06:18 +0200

    Get rid of higher kinded type warnings and pretify code.

--- query-primitives/src/main/scala/io/shiftleft/queryprimitives/dsl/DslWriterImplicits.scala
+++ query-primitives/src/main/scala/io/shiftleft/queryprimitives/dsl/DslWriterImplicits.scala
@@ -1,5 +1,7 @@
 package io.shiftleft.queryprimitives.dsl
 
+import scala.language.higherKinds
+
 object DslWriterImplicits {
 
   implicit def toGenericPipeMethods[PipeType[+_], ElemType](pipe: PipeType[ElemType])
--- query-primitives/src/main/scala/io/shiftleft/queryprimitives/dsl/Implicits.scala
+++ query-primitives/src/main/scala/io/shiftleft/queryprimitives/dsl/Implicits.scala
@@ -7,34 +7,41 @@ import io.shiftleft.queryprimitives.dsl.pipetypes.ShallowPipe.ShallowPipe
 import io.shiftleft.queryprimitives.dsl.pipeops.{RealPipeOperations, ShallowPipeOperations}
 import io.shiftleft.queryprimitives.steps.types.structure.{MethodMethods, MethodReturnMethods}
 
+import scala.language.higherKinds
+
 object Implicits extends PipeOperationImplicits with LowPriorityImplicits {
-  implicit def realPipeMethods[ElemType](pipe: RealPipe[ElemType]) = {
+  implicit def realPipeMethods[ElemType](pipe: RealPipe[ElemType])
+  : GenericPipeMethods[RealPipe,ElemType] = {
     new GenericPipeMethods(pipe)
   }
 
-  implicit def methodMethods[PipeType[+_]](pipe: PipeType[nodes.Method]) = {
+  implicit def methodMethods[PipeType[+_]](pipe: PipeType[nodes.Method])
+  : MethodMethods[PipeType] = {
     new MethodMethods(pipe)
   }
 
-  implicit def methodReturnMethods[PipeType[+_]](pipe: PipeType[nodes.MethodReturn]) = {
+  implicit def methodReturnMethods[PipeType[+_]](pipe: PipeType[nodes.MethodReturn])
+  : MethodReturnMethods[PipeType] = {
     new MethodReturnMethods(pipe)
   }
 
 }
 
 class PipeOperationImplicits {
-  implicit val realPipeOps = new RealPipeOperations()
-  implicit val shallowPipeOps = new ShallowPipeOperations()
+  implicit val realPipeOps: RealPipeOperations = new RealPipeOperations()
+  implicit val shallowPipeOps: ShallowPipeOperations = new ShallowPipeOperations()
 
 }
 
 sealed trait LowPriorityImplicits {
 
-  implicit def methodMethods(pipe: nodes.Method) = {
+  implicit def methodMethods(pipe: nodes.Method)
+  : MethodMethods[ShallowPipe] = {
     new MethodMethods(pipe.asInstanceOf[ShallowPipe[nodes.Method]])
   }
 
-  implicit def methodReturnMethods(pipe: nodes.MethodReturn) = {
+  implicit def methodReturnMethods(pipe: nodes.MethodReturn)
+  : MethodReturnMethods[ShallowPipe] = {
     new MethodReturnMethods(pipe.asInstanceOf[ShallowPipe[MethodReturn]])
   }
 
