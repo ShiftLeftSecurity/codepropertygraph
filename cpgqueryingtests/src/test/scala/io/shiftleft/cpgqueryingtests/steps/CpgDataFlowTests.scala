6a15eff1ef24b8d2fdd830c2b7465a49054d370e
behrad <behrad@shiftleft.io>
2019-05-09 19:34:52 +0200

    Revert "test: flows from identifier to method parameter"
    This reverts commit 5fc500e17ebae19706171c417653764f0314c0f0.

--- cpgqueryingtests/src/test/scala/io/shiftleft/cpgqueryingtests/steps/CpgDataFlowTests.scala
+++ cpgqueryingtests/src/test/scala/io/shiftleft/cpgqueryingtests/steps/CpgDataFlowTests.scala
@@ -12,31 +12,22 @@ import shapeless.HNil
 class CpgDataFlowTests extends WordSpec with Matchers {
 
   protected implicit def int2IntegerOption(x: Int): Option[Integer] = {
-      Some(x)
-    }
+    Some(x)
+  }
 
   protected def getMemberOfType(cpg: Cpg, typeName: String, memberName: String): Member[HNil] = {
-      cpg.typeDecl.nameExact(typeName).member.nameExact(memberName)
-    }
+    cpg.typeDecl.nameExact(typeName).member.nameExact(memberName)
+  }
 
   protected def getMethodOfType(cpg: Cpg, typeName: String, methodName: String): Method[HNil] = {
-      cpg.typeDecl.nameExact(typeName).method.nameExact(methodName)
-    }
+    cpg.typeDecl.nameExact(typeName).method.nameExact(methodName)
+  }
 
   protected def getLiteralOfType(cpg: Cpg, typeName: String, literalName: String): Literal[HNil] = {
-      cpg.typeDecl.nameExact(typeName).method.literal.codeExact(literalName)
-    }
+    cpg.typeDecl.nameExact(typeName).method.literal.codeExact(literalName)
+  }
 
   protected def flowToResultPairs(flow: List[nodes.TrackingPoint]): List[(String, Option[Integer])] = {
-      flow.map { point => point match {
-          case methodParamIn: nodes.MethodParameterIn => {
-            val method = point.start.method.head
-            val method_name = method.name
-            val code = s"$method_name(${method.start.parameter.l.sortBy(_.order).map(_.code).mkString(", ")})"
-            (code, point.cfgNode.lineNumber)
-          }
-          case _ => (point.cfgNode.code, point.cfgNode.lineNumber)
-        }
-      }
-    }
+    flow.map(point => (point.cfgNode.code, point.cfgNode.lineNumber))
   }
+}
