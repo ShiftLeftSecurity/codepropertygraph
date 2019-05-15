afaa1c2dfd343edfd69371c8d066c77b50778662
Markus Lottmann <markus@shiftleft.io>
2019-05-15 10:51:34 +0200

    Added a few more constraints.

--- cpgvalidator/src/main/scala/io/shiftleft/cpgvalidator/Constraints.scala
+++ cpgvalidator/src/main/scala/io/shiftleft/cpgvalidator/Constraints.scala
@@ -7,6 +7,11 @@ object Constraints {
 
   val outConstraints = List(
     NodeTypes.METHOD has 1 to 2 outgoing EdgeTypes.AST to NodeTypes.METHOD_RETURN,
+    NodeTypes.METHOD has 0 to N outgoing EdgeTypes.AST to NodeTypes.METHOD_PARAMETER_IN,
+    NodeTypes.METHOD has 0 to N outgoing EdgeTypes.AST to NodeTypes.MODIFIER,
+    NodeTypes.METHOD has 1 outgoing EdgeTypes.AST to NodeTypes.BLOCK,
+    NodeTypes.METHOD has 0 to N outgoing EdgeTypes.AST to NodeTypes.TYPE_PARAMETER,
+
   )
 
   val inConstraints = List(
