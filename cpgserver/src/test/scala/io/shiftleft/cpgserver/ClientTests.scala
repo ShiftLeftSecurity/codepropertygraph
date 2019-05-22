18f9a3708fd0ef92d92ae95df7053b4392b3df53
Fabian Yamaguchi <fabs@shiftleft.io>
2019-05-22 15:22:38 +0200

    Fix paths.

--- cpgserver/src/test/scala/io/shiftleft/cpgserver/ClientTests.scala
+++ cpgserver/src/test/scala/io/shiftleft/cpgserver/ClientTests.scala
@@ -1,13 +1,17 @@
 package io.shiftleft.cpgserver
 
+import java.nio.file.Paths
+
 import org.scalatest.{Matchers, WordSpec}
 
 class ClientTests extends WordSpec with Matchers {
 
   "should execute cpgclientlibtests successfully" in {
     import scala.sys.process._
-    println(new java.io.File(".").getCanonicalPath())
-    sys.process.Process(Seq("/home/tmp/shiftleft/codepropertygraph/cpgclientlib/runtests.sh"), new java.io.File("/home/tmp/shiftleft/codepropertygraph/cpgclientlib/")).!!
+    val currentWorkingDirectory = new java.io.File(".").getAbsolutePath
+    val testDir = Paths.get(currentWorkingDirectory, "cpgclientlib").toAbsolutePath.toString
+    val testScript = Paths.get(testDir.toString, "runtests.sh").toAbsolutePath.toString
+    sys.process.Process(Seq(testScript), new java.io.File(testDir)).!!
   }
 
 }
