d4e19da5c7581493b86f9ce2013c3999272b1f8d
Fabian Yamaguchi <fabs@shiftleft.io>
2019-05-23 18:03:50 +0200

    Use python2 on travis.

--- cpgclientlib/deploy.sh
+++ cpgclientlib/deploy.sh
@@ -4,5 +4,5 @@ SCRIPT_ABS_PATH=$(readlink -f "$0")
 SCRIPT_ABS_DIR=$(dirname $SCRIPT_ABS_PATH)
 cd $SCRIPT_ABS_DIR
 ./setVersion
-python3 setup.py sdist
+python2 setup.py sdist
 # python3 -m twine upload -u fabsx00 -p $PYPI_PASSWORD dist/*
