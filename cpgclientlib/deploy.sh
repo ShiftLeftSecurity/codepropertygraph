7446d8f13103d5a1cd8976d6d78f6b3d45478004
Fabian Yamaguchi <fabs@shiftleft.io>
2019-05-23 19:44:09 +0200

    Revert "Install and execute twine on build (#190)"
    This reverts commit 2e8560ac937b851b2dfd9bd67f97204ce0321777.

--- cpgclientlib/deploy.sh
+++ cpgclientlib/deploy.sh
@@ -5,4 +5,4 @@ SCRIPT_ABS_DIR=$(dirname $SCRIPT_ABS_PATH)
 cd $SCRIPT_ABS_DIR
 ./setVersion
 python2 setup.py sdist
-python2 -m twine upload -u fabsx00 -p $PYPI_PASSWORD dist/*
+# python3 -m twine upload -u fabsx00 -p $PYPI_PASSWORD dist/*
