e8e6e0c51c1b9a383e86437fdf1ae3cda4646ca1
Michael Pollmeier <michael@michaelpollmeier.com>
2019-06-12 09:47:19 +1200

    allow to configure threshold for heap overflow (#221)
    and increase default value to 90% (from 80%)

--- codepropertygraph/src/main/scala/io/shiftleft/codepropertygraph/cpgloading/OnDiskOverflowConfig.scala
+++ codepropertygraph/src/main/scala/io/shiftleft/codepropertygraph/cpgloading/OnDiskOverflowConfig.scala
@@ -1,4 +1,6 @@
 package io.shiftleft.codepropertygraph.cpgloading
 
 /** configure graphdb to use ondisk overflow. by default, system tmp directory is used (e.g. `/tmp`)  */
-case class OnDiskOverflowConfig(alternativeParentDirectory: Option[String] = None)
+case class OnDiskOverflowConfig(
+  alternativeParentDirectory: Option[String] = None,
+  heapPercentageThreshold: Int = 90)
