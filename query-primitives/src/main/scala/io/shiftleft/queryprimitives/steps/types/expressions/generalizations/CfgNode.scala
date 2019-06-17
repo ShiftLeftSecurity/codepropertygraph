e37ffe8a3e78cac7d8b9e5b84e98e182e674b606
Markus Lottmann <markus@shiftleft.io>
2019-06-17 17:02:08 +0200

    Add method step to CfgNode traversal.

--- query-primitives/src/main/scala/io/shiftleft/queryprimitives/steps/types/expressions/generalizations/CfgNode.scala
+++ query-primitives/src/main/scala/io/shiftleft/queryprimitives/steps/types/expressions/generalizations/CfgNode.scala
@@ -1,19 +1,39 @@
 package io.shiftleft.queryprimitives.steps.types.expressions.generalizations
 
 import gremlin.scala.GremlinScala
-import io.shiftleft.codepropertygraph.generated.{NodeTypes, nodes}
+import io.shiftleft.codepropertygraph.generated.{EdgeTypes, NodeTypes, nodes}
 import io.shiftleft.queryprimitives.steps.NodeSteps
 import io.shiftleft.queryprimitives.steps.Implicits.GremlinScalaDeco
 import io.shiftleft.queryprimitives.steps.types.expressions.Call
+import io.shiftleft.queryprimitives.steps.types.structure.Method
+import io.shiftleft.queryprimitives.utils.ExpandTo
+import org.apache.tinkerpop.gremlin.structure.Direction
 import shapeless.HList
 
 class CfgNode[Labels <: HList](raw: GremlinScala.Aux[nodes.CfgNode, Labels])
-  extends NodeSteps[nodes.CfgNode, Labels](raw) {
+    extends NodeSteps[nodes.CfgNode, Labels](raw) {
 
   /**
   Cast to call if applicable
     */
   def call: Call[Labels] =
     new Call[Labels](raw.hasLabel(NodeTypes.CALL).cast[nodes.Call])
+
+  /**
+  Traverse to enclosing method
+    */
+  def method: Method[Labels] = {
+    new Method[Labels](
+      raw
+        .map {
+          case method: nodes.Method =>
+            method
+          case methodReturn: nodes.MethodReturn =>
+            ExpandTo.formalReturnToMethod(methodReturn)
+          case expression =>
+            ExpandTo.expressionToMethod(expression)
+        }
+        .cast[nodes.Method])
+  }
 
 }
