c92fd02d263c2e1963e0cf63c1634bc9ba5fed46
Michael Pollmeier <michael@michaelpollmeier.com>
2019-07-07 21:25:18 +1200

    scaladoc

--- codepropertygraph/src/main/scala/io/shiftleft/codepropertygraph/cpgloading/OnDiskOverflowConfig.scala
+++ codepropertygraph/src/main/scala/io/shiftleft/codepropertygraph/cpgloading/OnDiskOverflowConfig.scala
@@ -3,6 +3,7 @@ package io.shiftleft.codepropertygraph.cpgloading
 import scala.compat.java8.OptionConverters._
 
 /** configure graphdb to use ondisk overflow.
+  * if the file specified by `graphLocation` already exists, we'll initialize the graph from there
   * if `graphLocation` is specified, graph will be saved there on close, and can be reloaded by just instantiating one with the same setting
   * otherwise, system tmp directory is used (e.g. `/tmp`) and graph won't be saved on close */
 case class OnDiskOverflowConfig(graphLocation: Option[String] = None,
