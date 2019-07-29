8d35f990d7ae79f10170001e53cb072f57498e31
Fabian Yamaguchi <fabs@shiftleft.io>
2019-07-29 13:37:22 +0200

    Rework CpgLoaderConfig/OnDiskOverflowConfig. (#276)

--- cpgqueryingtests/src/main/scala/io/shiftleft/cpgqueryingtests/codepropertygraph/CpgTestFixture.scala
+++ cpgqueryingtests/src/main/scala/io/shiftleft/cpgqueryingtests/codepropertygraph/CpgTestFixture.scala
@@ -2,13 +2,13 @@ package io.shiftleft.cpgqueryingtests.codepropertygraph
 
 import gremlin.scala.{Graph, ScalaGraph}
 import io.shiftleft.SerializedCpg
-import io.shiftleft.codepropertygraph.cpgloading.{CpgLoader, CpgLoaderConfig, OnDiskOverflowConfig}
+import io.shiftleft.codepropertygraph.cpgloading.{CpgLoader, CpgLoaderConfig, OverflowDbConfig}
 import io.shiftleft.layers.{DataFlowRunner, EnhancementRunner}
 import io.shiftleft.semanticsloader.SemanticsLoader
 
 class CpgTestFixture(projectName: String) {
   lazy val cpg = CpgLoader.load(s"resources/cpgs/$projectName/cpg.bin.zip",
-                                CpgLoaderConfig.default.withOverflowConfig(OnDiskOverflowConfig.disabled))
+                                CpgLoaderConfig().withOverflowConfig(OverflowDbConfig.disabled))
   new EnhancementRunner().run(cpg, new SerializedCpg())
   new DataFlowRunner(SemanticsLoader.emptySemantics).run(cpg, new SerializedCpg());
   implicit val graph: Graph = cpg.graph
