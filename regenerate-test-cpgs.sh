dcd57992645e2d17bf0c49a74b1e5d42c8bc796b
Michael Pollmeier <michael@michaelpollmeier.com>
2019-07-31 17:00:41 +0200

    PR comment

--- regenerate-test-cpgs.sh
+++ regenerate-test-cpgs.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env sh
 
-export CPG_ROOT_DIR=`pwd`
+export CPG_HOME=`pwd`
 
 # https://stackoverflow.com/a/28085062
-: ${DEFAULT_JAVA2CPG_ROOT:=`readlink -f ../java2cpg`}
-# : ${DEFAULT_JAVA2CPG_ROOT:=${readlink -f "../java2cpg"}}
-echo "java2cpg root directory [$DEFAULT_JAVA2CPG_ROOT]:"
+: ${DEFAULT_JAVA2CPG_HOME:=`readlink -f ../java2cpg`}
+echo "java2cpg root directory [$DEFAULT_JAVA2CPG_HOME]:"
 read INSTALL_DIR
-if [ -z "$JAVA2CPG_ROOT" ]; then
-  JAVA2CPG_ROOT=$DEFAULT_JAVA2CPG_ROOT
+if [ -z "$JAVA2CPG_HOME" ]; then
+  JAVA2CPG_HOME=$DEFAULT_JAVA2CPG_HOME
 fi
 
-cd $JAVA2CPG_ROOT
+cd $JAVA2CPG_HOME
 sbt stage
 
-for jar in `find $CPG_ROOT_DIR/resources/cpgs -name '*.jar'`; do
+for jar in `find $CPG_HOME/resources/cpgs -name '*.jar'`; do
   OUT_DIR=`dirname $jar`
   ./java2cpg.sh $jar -o $OUT_DIR/cpg.bin.zip
 done;
