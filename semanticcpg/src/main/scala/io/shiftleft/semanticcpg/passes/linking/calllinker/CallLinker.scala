0f1f4e8cd46065bf3347ce35d4fdfb433992cd6d
Markus Lottmann <markus@shiftleft.io>
2019-08-02 11:47:49 +0200

    Implement linking CALL via METHOD_REF.

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/linking/calllinker/CallLinker.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/linking/calllinker/CallLinker.scala
@@ -1,20 +1,30 @@
 package io.shiftleft.semanticcpg.passes.linking.calllinker
 
+import gremlin.scala._
 import io.shiftleft.codepropertygraph.Cpg
-import io.shiftleft.codepropertygraph.generated.{EdgeTypes, nodes}
+import io.shiftleft.codepropertygraph.generated.{EdgeTypes, NodeTypes, nodes}
 import io.shiftleft.passes.{CpgPass, DiffGraph}
 import io.shiftleft.semanticcpg.language._
 import io.shiftleft.Implicits._
 import org.apache.tinkerpop.gremlin.structure.Direction
+import org.slf4j.LoggerFactory
 
 import scala.collection.JavaConverters._
 
 class CallLinker(cpg: Cpg) extends CpgPass(cpg){
+  import CallLinker._
+  private var methodFullNameToNode = Map.empty[String, nodes.StoredNode]
   /**
     * Main method of enhancement - to be implemented by child class
     **/
   override def run(): Iterator[DiffGraph] = {
     val dstGraph = new DiffGraph()
+
+    cpg.graph.V.hasLabel(NodeTypes.METHOD).sideEffectWithTraverser { traverser =>
+      val method = traverser.get.asInstanceOf[nodes.Method]
+      methodFullNameToNode += (method.fullName -> method)
+    }.iterate()
+
     cpg.call.sideEffect { call =>
       try {
         linkCall(call, dstGraph)
@@ -28,19 +38,31 @@ class CallLinker(cpg: Cpg) extends CpgPass(cpg){
   }
 
   private def linkCall(call: nodes.Call, dstGraph: DiffGraph): Unit = {
-    call.vertices(Direction.OUT, EdgeTypes.RECEIVER).nextOption.foreach { receiver =>
-      val receiverTypeDecl = receiver.vertices(Direction.OUT, EdgeTypes.EVAL_TYPE).next
-        .vertices(Direction.OUT, EdgeTypes.REF).next
-
-      val resolvedMethodOption =
-        receiverTypeDecl.vertices(Direction.OUT, EdgeTypes.BINDS).asScala.collectFirst {
-          case binding: nodes.Binding if binding.name == call.name && binding.signature == call.signature =>
-            binding.vertices(Direction.OUT, EdgeTypes.REF).nextChecked.asInstanceOf[nodes.Method]
+    call.vertices(Direction.OUT, EdgeTypes.RECEIVER).nextOption.foreach {
+      case methodRef: nodes.MethodRef =>
+        methodFullNameToNode.get(methodRef.methodInstFullName) match {
+          case Some(method) =>
+            dstGraph.addEdgeInOriginal(call, method, EdgeTypes.CALL)
+          case None =>
+            logger.warn(s"Unable to link METHOD_REF with METHOD_FULL_NAME ${methodRef.methodInstFullName}.")
         }
+      case receiver =>
+        val receiverTypeDecl = receiver.vertices(Direction.OUT, EdgeTypes.EVAL_TYPE).nextChecked
+          .vertices(Direction.OUT, EdgeTypes.REF).nextChecked
 
-      resolvedMethodOption.foreach { method =>
-        dstGraph.addEdgeInOriginal(call, method, EdgeTypes.CALL)
-      }
+        val resolvedMethodOption =
+          receiverTypeDecl.vertices(Direction.OUT, EdgeTypes.BINDS).asScala.collectFirst {
+            case binding: nodes.Binding if binding.name == call.name && binding.signature == call.signature =>
+              binding.vertices(Direction.OUT, EdgeTypes.REF).nextChecked.asInstanceOf[nodes.Method]
+          }
+
+        resolvedMethodOption.foreach { method =>
+          dstGraph.addEdgeInOriginal(call, method, EdgeTypes.CALL)
+        }
     }
   }
+}
+
+object CallLinker {
+  private val logger = LoggerFactory.getLogger(getClass)
 }
