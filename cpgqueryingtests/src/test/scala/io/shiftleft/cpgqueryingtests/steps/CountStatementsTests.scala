7f56d440076ac70de58f5853256970d3d76550ca
Max Leuthäuser <max.leuthaeuser@gmail.com>
2019-08-05 14:52:48 +0200

    Adjusted number of statements in CountStatementsTests.scala.

--- cpgqueryingtests/src/test/scala/io/shiftleft/cpgqueryingtests/steps/CountStatementsTests.scala
+++ cpgqueryingtests/src/test/scala/io/shiftleft/cpgqueryingtests/steps/CountStatementsTests.scala
@@ -8,7 +8,7 @@ class CountStatementsTests extends WordSpec with Matchers {
 
   "Class Statements" should CpgTestFixture("method") { fixture =>
     "count statements correctly" in {
-      Statements.countAll(fixture.cpg) should be(9)
+      Statements.countAll(fixture.cpg) should be(13)
     }
   }
 }
