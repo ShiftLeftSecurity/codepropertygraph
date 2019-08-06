92b3f85dcabb7f57d4625cf9a092ee9cba81f671
Michael Pollmeier <michael@michaelpollmeier.com>
2019-08-06 08:44:41 +0200

    remove test jars from the repository, generate them instead
    script now generates cpgs from sources, by compiling them and creating
    jars, rather than having the jars checked into the repository
    
    it also allows for 'pregenerated' jars like helloshiftleft, since we
    don't want to invoke the full maven build

--- semanticcpg/src/test/scala/io/shiftleft/semanticcpg/Fixture.scala
+++ semanticcpg/src/test/scala/io/shiftleft/semanticcpg/Fixture.scala
@@ -7,7 +7,7 @@ import io.shiftleft.layers.{DataFlowRunner, EnhancementRunner}
 import io.shiftleft.semanticsloader.SemanticsLoader
 
 class Fixture(projectName: String) {
-  val cpg = CpgLoader.load(s"resources/cpgs/$projectName/cpg.bin.zip",
+  val cpg = CpgLoader.load(s"resources/testcode/cpgs/$projectName/cpg.bin.zip",
                            CpgLoaderConfig().withOverflowConfig(OverflowDbConfig.disabled))
   new EnhancementRunner().run(cpg, new SerializedCpg())
   new DataFlowRunner(SemanticsLoader.emptySemantics)
