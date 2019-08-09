558df3f6a6215ea0634200f89291435691233ead
Michael Pollmeier <michael@michaelpollmeier.com>
2019-08-09 17:16:09 +0200

    swap TinkerGraph for OverflowDB, simplify codegen (#297)

--- cpgvalidator/src/main/scala/io/shiftleft/cpgvalidator/CpgValidatorMain.scala
+++ cpgvalidator/src/main/scala/io/shiftleft/cpgvalidator/CpgValidatorMain.scala
@@ -1,13 +1,11 @@
 package io.shiftleft.cpgvalidator
 
-import io.shiftleft.codepropertygraph.cpgloading.{CpgLoader, CpgLoaderConfig, OverflowDbConfig}
+import io.shiftleft.codepropertygraph.cpgloading.{CpgLoader, CpgLoaderConfig}
 
 object CpgValidatorMain extends App {
   val cpgFileName = args(0)
 
-  val loaderConfig = CpgLoaderConfig().withOverflowConfig(OverflowDbConfig.disabled)
-  val cpg = CpgLoader.load(cpgFileName, loaderConfig)
-
+  val cpg = CpgLoader.load(cpgFileName, CpgLoaderConfig.withoutOverflow)
   val validator = new CpgValidator(cpg)
   val cpgValid = validator.validate()
   validator.logValidationErrors()
