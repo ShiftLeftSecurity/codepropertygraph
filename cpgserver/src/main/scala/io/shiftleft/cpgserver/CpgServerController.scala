35258952332708c7a82d93fbcc90b43c6007e8ae
Fabian Yamaguchi <fabs@shiftleft.io>
2019-08-23 13:21:51 +0200

    Revert "Add import to CpgServerController.scala."
    This reverts commit d194a453f6e2d971eabb80947baa76660a7e8ce7.

--- cpgserver/src/main/scala/io/shiftleft/cpgserver/CpgServerController.scala
+++ cpgserver/src/main/scala/io/shiftleft/cpgserver/CpgServerController.scala
@@ -100,7 +100,6 @@ class CpgServerController(impl: ServerImpl, system: ActorSystem = ActorSystem())
       e.eval(s"""
                 import io.shiftleft.codepropertygraph.Cpg
                 | import io.shiftleft.semanticcpg.language._
-                | import io.shiftleft.dataflowengine.language._
                 | import io.shiftleft.semanticcpg.language.NoResolve
                 | implicit val resolver = NoResolve
                 | val cpg = aCpg.asInstanceOf[io.shiftleft.codepropertygraph.Cpg]
