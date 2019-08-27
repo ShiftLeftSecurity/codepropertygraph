2f2025cc338be83a058271f62f908b3390e32dc5
Denis Yermakov <lfecdr@gmail.com>
2019-08-27 19:35:11 +0300

    Changed metrics namespace to avoid conflict with mesh project.

--- codepropertygraph/src/main/scala/io/shiftleft/utils/tracing/Measure.scala
+++ codepropertygraph/src/main/scala/io/shiftleft/utils/tracing/Measure.scala
@@ -1,4 +1,4 @@
-package io.shiftleft.metrics
+package io.shiftleft.utils.metrics
 
 trait Measure {
   def currentValue: Long
--- codepropertygraph/src/main/scala/io/shiftleft/utils/tracing/Metric.scala
+++ codepropertygraph/src/main/scala/io/shiftleft/utils/tracing/Metric.scala
@@ -1,4 +1,4 @@
-package io.shiftleft.metrics
+package io.shiftleft.utils.metrics
 
 import java.io.PrintStream
 import java.text.DecimalFormat
