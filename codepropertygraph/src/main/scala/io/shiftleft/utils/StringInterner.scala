c1a61afbf60ad25ea0a827c1c7adf4df18c275e0
Denis Yermakov <lfecdr@gmail.com>
2019-09-03 20:02:02 +0300

    Added StringInterner

---
+++ codepropertygraph/src/main/scala/io/shiftleft/utils/StringInterner.scala
@@ -0,0 +1,34 @@
+package io.shiftleft.utils
+
+import gnu.trove.map.hash.THashMap
+
+trait StringInterner {
+  def intern(s: String): String
+}
+
+object StringInterner {
+  val DefaultMaxStringLength: Int = 64
+  val DefaultInitialSize: Int = 64 * 1024
+
+  val noop: StringInterner = new StringInterner {
+    override def intern(s: String): String = s
+  }
+  def makeStrongInterner(maxStringLength: Int = DefaultMaxStringLength,
+                         initialSize: Int = DefaultInitialSize): StringInterner = new StringInterner {
+    private val stringCache = new THashMap[String, String](initialSize)
+
+    def intern(s: String): String = {
+      if (s.length < maxStringLength) {
+        val cached = stringCache.get(s)
+        if (cached == null) {
+          stringCache.put(s, s)
+          s
+        } else {
+          cached
+        }
+      } else {
+        s
+      }
+    }
+  }
+}
