0ac5b48837764ce59bffef1fd3933e320f236bd8
Denis Yermakov <lfecdr@gmail.com>
2019-09-05 17:14:15 +0300

    Added docs.

--- codepropertygraph/src/main/scala/io/shiftleft/utils/StringInterner.scala
+++ codepropertygraph/src/main/scala/io/shiftleft/utils/StringInterner.scala
@@ -2,6 +2,12 @@ package io.shiftleft.utils
 
 import gnu.trove.map.hash.THashMap
 
+/**
+  * Interface for deduplicating strings used by CPGLoader.
+  * JVM with G1 GC does this automatically but it is not guaranteed to happen
+  * and when performing CPU heavy calculations doing this manually ensures
+  * that we won't run out of memory.
+  */
 trait StringInterner {
   def intern(s: String): String
 }
@@ -13,6 +19,13 @@ object StringInterner {
   val noop: StringInterner = new StringInterner {
     override def intern(s: String): String = s
   }
+
+  /** Creates a string interner that will hold strong references to the interned objects
+    * and they wont be GC'ed until reference to the interner is released.
+    * @param maxStringLength Maximum string length that will be considered for interning
+    * @param initialSize Initial string table size
+    * @return Instance of the StringInterner which uses strong references
+    */
   def makeStrongInterner(maxStringLength: Int = DefaultMaxStringLength,
                          initialSize: Int = DefaultInitialSize): StringInterner = new StringInterner {
     private val stringCache = new THashMap[String, String](initialSize)
