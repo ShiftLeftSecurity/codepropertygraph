67d85e15de0fd4833c6be25c7d7082ffdd0561d2
Markus Lottmann <markus@shiftleft.io>
2019-09-11 18:05:58 +0200

    Adjust tests to fixed bindings.

--- semanticcpg/src/test/scala/io/shiftleft/semanticcpg/language/types/structure/BindingTests.scala
+++ semanticcpg/src/test/scala/io/shiftleft/semanticcpg/language/types/structure/BindingTests.scala
@@ -12,48 +12,42 @@ class BindingTests extends WordSpec with Matchers {
       val queryResult: List[nodes.Binding] =
         fixture.cpg.typeDecl.name("BindingTest").methodBinding.l
 
-      queryResult.size shouldBe 1
-      queryResult.map(_.name).toSet shouldBe Set("boundMethod")
+      queryResult.map(_.name) should contain theSameElementsAs List("<init>", "boundMethod")
     }
 
     "expand from method binding to bound method" in {
       val queryResult: List[nodes.Method] =
         fixture.cpg.typeDecl.name("BindingTest").methodBinding.boundMethod.l
 
-      queryResult.size shouldBe 1
-      queryResult.map(_.name).toSet shouldBe Set("boundMethod")
+      queryResult.map(_.name) should contain theSameElementsAs List("<init>", "boundMethod")
     }
 
     "expand from bound method to method binding" in {
       val queryResult: List[nodes.Binding] =
         fixture.cpg.method.name("boundMethod").referencingBinding.l
 
-      queryResult.size shouldBe 1
-      queryResult.map(_.name).toSet shouldBe Set("boundMethod")
+      queryResult.map(_.name) should contain theSameElementsAs List("boundMethod")
     }
 
     "expand from method binding to binding type decl" in {
       val queryResult: List[nodes.TypeDecl] =
         fixture.cpg.method.name("boundMethod").referencingBinding.bindingTypeDecl.l
 
-      queryResult.size shouldBe 1
-      queryResult.map(_.name).toSet shouldBe Set("BindingTest")
+      queryResult.map(_.name) should contain theSameElementsAs List("BindingTest")
     }
 
     "expand from BindingTest class to bound method" in {
       val queryResult: List[nodes.Method] =
         fixture.cpg.typeDecl.name("BindingTest").boundMethod.l
 
-      queryResult.size shouldBe 1
-      queryResult.map(_.name).toSet shouldBe Set("boundMethod")
+      queryResult.map(_.name) should contain theSameElementsAs List("<init>", "boundMethod")
     }
 
     "expand from bound method to binding class" in {
       val queryResult: List[nodes.TypeDecl] =
         fixture.cpg.method.name("boundMethod").bindingTypeDecl.l
 
-      queryResult.size shouldBe 1
-      queryResult.map(_.name).toSet shouldBe Set("BindingTest")
+      queryResult.map(_.name) should contain theSameElementsAs List("BindingTest")
     }
   }
 }
