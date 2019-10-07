358c64a94c638f6a255657a650fb5ff83c0fb85f
Markus Lottmann <markus@shiftleft.io>
2019-10-07 23:02:40 +0200

    Add BINDING node, related steps and compatibility pass. (#382)

--- cpgvalidator/src/main/scala/io/shiftleft/cpgvalidator/facts/OutFactsImporter.scala
+++ cpgvalidator/src/main/scala/io/shiftleft/cpgvalidator/facts/OutFactsImporter.scala
@@ -22,7 +22,7 @@ class OutFactsImporter extends FactsImporter {
       NodeTypes.TYPE_DECL has 0 to N outgoing EdgeTypes.AST to NodeTypes.MEMBER,
       NodeTypes.TYPE_DECL has 0 to N outgoing EdgeTypes.AST to NodeTypes.MODIFIER,
       NodeTypes.TYPE_DECL has 0 to N outgoing EdgeTypes.AST to NodeTypes.ANNOTATION,
-      NodeTypes.TYPE_DECL has 0 to N outgoing EdgeTypes.VTABLE to NodeTypes.METHOD,
+      NodeTypes.TYPE_DECL has 0 to N outgoing EdgeTypes.BINDS to NodeTypes.BINDING,
       NodeTypes.MEMBER has 0 to N outgoing EdgeTypes.AST to NodeTypes.ANNOTATION,
       NodeTypes.MEMBER has 0 to N outgoing EdgeTypes.AST to NodeTypes.MODIFIER,
       NodeTypes.LITERAL has 1 to N outgoing EdgeTypes.CFG to SuperTypes.Expression or NodeTypes.METHOD_RETURN,
@@ -40,6 +40,7 @@ class OutFactsImporter extends FactsImporter {
       NodeTypes.METHOD_REF has 1 to N outgoing EdgeTypes.CFG to SuperTypes.Expression,
       NodeTypes.METHOD_REF has 0 to N outgoing EdgeTypes.CAPTURE to NodeTypes.CLOSURE_BINDING,
       NodeTypes.CLOSURE_BINDING has 1 outgoing EdgeTypes.REF to NodeTypes.LOCAL or NodeTypes.METHOD_PARAMETER_IN,
+      NodeTypes.BINDING has 1 outgoing EdgeTypes.REF to NodeTypes.METHOD,
     )
 
 }
---
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/types/structure/Binding.scala
@@ -0,0 +1,30 @@
+package io.shiftleft.semanticcpg.language.types.structure
+
+import gremlin.scala.GremlinScala
+import io.shiftleft.codepropertygraph.generated.{EdgeTypes, nodes}
+import io.shiftleft.semanticcpg.language._
+import io.shiftleft.semanticcpg.language.types.propertyaccessors.{NameAccessors, SignatureAccessors}
+
+class Binding(raw: GremlinScala[nodes.Binding])
+    extends NodeSteps[nodes.Binding](raw)
+    with NameAccessors[nodes.Binding]
+    with SignatureAccessors[nodes.Binding] {
+
+  /**
+    * Traverse to the method bound by this method binding.
+    */
+  def boundMethod: Method = {
+    new Method(
+      raw.out(EdgeTypes.REF).cast[nodes.Method]
+    )
+  }
+
+  /**
+    * Traverse to the method bound by this method binding.
+    */
+  def bindingTypeDecl: TypeDecl = {
+    new TypeDecl(
+      raw.in(EdgeTypes.BINDS).cast[nodes.TypeDecl]
+    )
+  }
+}
