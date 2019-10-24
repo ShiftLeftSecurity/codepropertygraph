b9c04f19f51a210d16d03e78f374863e7d8ac1e1
Fabian Yamaguchi <fabs@shiftleft.io>
2019-10-24 11:12:18 +0200

    Failing test for inheritance.

---
+++ semanticcpg/src/test/scala/io/shiftleft/semanticcpg/language/types/structure/InheritanceTests.scala
@@ -0,0 +1,27 @@
+package io.shiftleft.semanticcpg.language.types.structure
+
+import io.shiftleft.semanticcpg.testfixtures.CodeToCpgFixture
+import org.scalatest.{Matchers, WordSpec}
+import io.shiftleft.semanticcpg.language._
+
+class InheritanceTests extends WordSpec with Matchers {
+
+  val code = """
+               | class Foo { int i; };
+               |
+               | class Bar : public Foo {
+               |   char x;
+               |   int y;
+               |   int method () {}
+               | };
+               |
+    """.stripMargin
+
+  "should find that Bar is derived from Foo" in
+    CodeToCpgFixture().buildCpg(code) {
+      { cpg =>
+        cpg.typeDecl.name("Bar").baseType.name.l shouldBe List("Foo")
+      }
+    }
+
+}
