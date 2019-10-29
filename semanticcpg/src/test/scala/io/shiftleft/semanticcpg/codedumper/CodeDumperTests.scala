25cbd45322d7dc3a1f786d67ece65693171d8651
Fabian Yamaguchi <fabs@shiftleft.io>
2019-10-29 16:57:52 +0100

    `dump` must return `List[String]` to work with browse (#444)
    * `dump` must return `List[String]` to work with browse.
    
    * Wrap arrow in C-style comment.
    
    * Fix test.

--- semanticcpg/src/test/scala/io/shiftleft/semanticcpg/codedumper/CodeDumperTests.scala
+++ semanticcpg/src/test/scala/io/shiftleft/semanticcpg/codedumper/CodeDumperTests.scala
@@ -1,5 +1,7 @@
 package io.shiftleft.semanticcpg.codedumper
 
+import java.util.regex.Pattern
+
 import io.shiftleft.semanticcpg.testfixtures.CodeToCpgFixture
 import org.scalatest.{Matchers, WordSpec}
 import io.shiftleft.semanticcpg.language._
@@ -13,12 +15,14 @@ class CodeDumperTests extends WordSpec with Matchers {
 
   CodeToCpgFixture(code) { cpg =>
     "should return empty string for empty traversal" in {
-      CodeDumper.dump(cpg.method.name("notinthere"), false) shouldBe ""
+      CodeDumper
+        .dump(cpg.method.name("notinthere"), false)
+        .mkString("\n") shouldBe ""
     }
 
     "should be able to dump complete function" in {
       val query = cpg.method.name("my_func")
-      val code = CodeDumper.dump(query, false)
+      val code = CodeDumper.dump(query, false).mkString("\n")
       code should startWith(CodeDumper.arrow.toString)
       code should include("foo(param1)")
       code should endWith("}")
@@ -26,9 +30,9 @@ class CodeDumperTests extends WordSpec with Matchers {
 
     "should dump method with arrow for expression (a call)" in {
       val query = cpg.call.name("foo")
-      val code = CodeDumper.dump(query, false)
+      val code = CodeDumper.dump(query, false).mkString("\n")
       code should startWith("int")
-      code should include regex (CodeDumper.arrow + ".*" + "int x = foo" + ".*")
+      code should include regex (Pattern.quote(CodeDumper.arrow.toString) + ".*" + "int x = foo" + ".*")
       code should endWith("}")
     }
 
@@ -37,13 +41,13 @@ class CodeDumperTests extends WordSpec with Matchers {
     }
 
     "should allow dumping via .dump" in {
-      val code = cpg.method.name("my_func").dump
+      val code = cpg.method.name("my_func").dump.mkString("\n")
       code should startWith(CodeDumper.arrow.toString)
     }
 
     "should allow dumping callIn" in {
       implicit val resolver: ICallResolver = NoResolve
-      val code = cpg.method.name("foo").callIn.dump
+      val code = cpg.method.name("foo").callIn.dump.mkString("\n")
       code should startWith("int")
     }
 
