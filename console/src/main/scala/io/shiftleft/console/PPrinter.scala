ef2fd31e1693678f7d21ad39b0db986735a6b5d9
Michael Pollmeier <michael@michaelpollmeier.com>
2019-10-31 14:02:12 +1300

    cleanup

--- console/src/main/scala/io/shiftleft/console/PPrinter.scala
+++ console/src/main/scala/io/shiftleft/console/PPrinter.scala
@@ -7,39 +7,28 @@ import pprint.{PPrinter, Renderer, Result, Tree, Truncated}
 object pprinter {
 
   def create(original: PPrinter): PPrinter =
-    new PPrinter(
-      defaultHeight = 99999,
-      additionalHandlers = myAdditionalHandlers(original)) {
+    new PPrinter(defaultHeight = 99999, additionalHandlers = myAdditionalHandlers(original)) {
       override def tokenize(x: Any,
                             width: Int = defaultWidth,
                             height: Int = defaultHeight,
                             indent: Int = defaultIndent,
-                            initialOffset: Int = 0): Iterator[fansi.Str] = x match {
-        case highlightedSource: HighlightedSource =>
-          println("yyyy0: in io.shiftleft.console.PPrinter for HighlightedSource")
-          Iterator.single(fansi.Str(highlightedSource.fixedForFansi))
-//        case x => super.tokenize(x, width, height, indent, initialOffset)
-        case x =>
-          val tree = this.treeify(x)
-          val renderer = new Renderer(width, colorApplyPrefix, colorLiteral, indent) {
-            override def rec(x: Tree, leftOffset: Int, indentCount: Int): Result = x match {
-              case Tree.Literal(body) =>
-                println("zzz1")
-                val fixedString = HighlightedSource(body).fixedForFansi
-                Result.fromString(fixedString)
-              case _ => super.rec(x, leftOffset, indentCount)
-            }
+                            initialOffset: Int = 0): Iterator[fansi.Str] = {
+        val tree = this.treeify(x)
+        val renderer = new Renderer(width, colorApplyPrefix, colorLiteral, indent) {
+          override def rec(x: Tree, leftOffset: Int, indentCount: Int): Result = x match {
+            case Tree.Literal(body) =>
+              println("zzz1")
+              val fixedString = HighlightedSource(body).fixedForFansi
+              Result.fromString(fixedString)
+            case _ => super.rec(x, leftOffset, indentCount)
           }
-          val rendered = renderer.rec(tree, initialOffset, 0).iter
-          new Truncated(rendered, width, height)
+        }
+        val rendered = renderer.rec(tree, initialOffset, 0).iter
+        new Truncated(rendered, width, height)
       }
     }
 
   private def myAdditionalHandlers(original: PPrinter): PartialFunction[Any, Tree] = {
-    // this fixes the crash, but it leads to re-parsing which removes the initial encoding
-    case highlightedSource: HighlightedSource =>
-      println("yyyy1")
-      Tree.Literal(highlightedSource.value)
     case node: nodes.Node =>
       Tree.Apply(
         node.productPrefix,
