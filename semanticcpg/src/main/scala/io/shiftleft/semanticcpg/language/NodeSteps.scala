e09d3b1090ebc8c81e56b75f1aa638c05a7d10ab
Michael Pollmeier <michael@michaelpollmeier.com>
2019-10-31 14:41:37 +1300

    dump doesn't highlight, only dumpc does

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/NodeSteps.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/NodeSteps.scala
@@ -42,7 +42,7 @@ class NodeSteps[NodeType <: nodes.StoredNode](raw: GremlinScala[NodeType]) exten
     * dump the method code along with an arrow pointing
     * to the expression.
     * */
-  def dump: List[String] = CodeDumper.dump(this, true)
+  def dump: List[String] = CodeDumper.dump(this, false)
 
   /**
     * Dump with colored (syntax highlighted output)
