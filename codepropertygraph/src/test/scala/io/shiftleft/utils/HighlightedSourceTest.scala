c269a6c08d094b79c6b636f792a46581d299eec1
Michael Pollmeier <michael@michaelpollmeier.com>
2019-10-31 13:36:41 +1300

    tests

--- codepropertygraph/src/test/scala/io/shiftleft/utils/HighlightedSourceTest.scala
+++ codepropertygraph/src/test/scala/io/shiftleft/utils/HighlightedSourceTest.scala
@@ -2,29 +2,37 @@ package io.shiftleft.utils
 
 import org.scalatest.{Matchers, WordSpec}
 
+/** We use source-highlight to encode source as ansi strings, e.g. the .dump step
+  * Ammonite uses fansi for it's colour-coding, and while both pledge to follow the ansi codec, they aren't compatible
+  * TODO: PR for fansi to support these standard encodings out of the box */
 class HighlightedSourceTest extends WordSpec with Matchers {
+  // ansi colour-encoded strings as source-highlight produces them
+  val IntGreenForeground = "\u001b[32mint\u001b[m"
+  val IfBlueBold = "\u001b[01;34mif\u001b[m"
+  val FBold = "\u001b[01mF\u001b[m"
 
-  "fixes ansi encoding ending" in {
-    // `int` in green
+  "fansi encoding fix" must {
+    "handle different ansi encoding termination" in {
+      //encoding ends with [39m for fansi instead of [m
+      val fixedForFansi = HighlightedSource(IntGreenForeground).fixedForFansi
+      fixedForFansi shouldBe "\u001b[32mint\u001b[39m"
+      fansi.Str(fixedForFansi) shouldBe fansi.Color.Green("int")
+    }
 
-    '\u001b'
-    '['
-    '3'
-    '2'
-    'm'
-    'i'
-    'n'
-    't'
-    '\u001b'
-    '['
-    'm'
-    '\n'
+    "handle different single-digit encodings" in {
+      //`[01m` is encoded as `[1m` in fansi for all single digit numbers
+      val fixedForFansi = HighlightedSource(FBold).fixedForFansi
+      fixedForFansi shouldBe "\u001b[1mF\u001b[39m"
+      fansi.Str(fixedForFansi) shouldBe fansi.Str("F").overlay(fansi.Bold.On)
+    }
 
-    println(fansi.Color.Blue("if").overlay(fansi.Bold.On, 0, 2).render)
-//    HighlightedSource("").fixedForFansi shouldBe ""
-    //.replaceAll("""\u001b\[m""", """\u001b[39m""") //encoding ends differently for fansi
+    "handle multi-encoded parts" in {
+      // `[01;34m` is encoded as `[1m[34m` in fansi
+      val fixedForFansi = HighlightedSource(IfBlueBold).fixedForFansi
+      fixedForFansi shouldBe "\u001b[1m\u001b[34mif\u001b[39m"
+      fansi.Str(fixedForFansi) shouldBe fansi.Color.Blue("if").overlay(fansi.Bold.On)
+    }
   }
-
 
 }
 
