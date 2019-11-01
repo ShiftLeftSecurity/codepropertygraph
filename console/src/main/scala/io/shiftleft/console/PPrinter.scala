adce14a66fb1f7ff915ee74e9f2423f73429e201
Michael Pollmeier <michael@michaelpollmeier.com>
2019-11-01 13:52:30 +1300

    handle full color 256 colours

--- console/src/main/scala/io/shiftleft/console/PPrinter.scala
+++ console/src/main/scala/io/shiftleft/console/PPrinter.scala
@@ -5,7 +5,7 @@ import pprint.{PPrinter, Renderer, Result, Tree, Truncated}
 
 object pprinter {
 
-  val AnsiEncodedRegexp = """\u001b\[\d+m""".r
+  val AnsiEncodedRegexp = """\u001b\[[\d;]+m""".r
   def isAnsiEncoded(s: String): Boolean =
     AnsiEncodedRegexp.findFirstIn(s).isDefined
 
@@ -17,7 +17,8 @@ object pprinter {
     ansiEncoded
       .replaceAll("""\u001b\[m""", """\u001b[39m""") //encoding ends with [39m for fansi instead of [m
       .replaceAll("""\u001b\[0(\d)m""", """\u001b[$1m""") // `[01m` is encoded as `[1m` in fansi for all single digit numbers
-      .replaceAll("""\u001b\[[0?](\d);(\d+)m""", """\u001b[$1m\u001b[$2m""") // `[01;34m` is encoded as `[1m[34m` in fansi
+      .replaceAll("""\u001b\[0?(\d+);0?(\d+)m""", """\u001b[$1m\u001b[$2m""") // `[01;34m` is encoded as `[1m[34m` in fansi
+      .replaceAll("""\u001b\[[00]+;0?(\d+);0?(\d+);0?(\d+)m""", """\u001b[$1;$2;$3m""") // `[00;38;05;70m` is encoded as `[38;5;70m` in fansi - 8bit color encoding
 
   def create(original: PPrinter): PPrinter =
     new PPrinter(defaultHeight = 99999, additionalHandlers = myAdditionalHandlers(original)) {
@@ -29,8 +30,8 @@ object pprinter {
         val tree = this.treeify(x)
         val renderer = new Renderer(width, colorApplyPrefix, colorLiteral, indent) {
           override def rec(x: Tree, leftOffset: Int, indentCount: Int): Result = x match {
-            case Tree.Literal(body)
-                if isAnsiEncoded(body) => // this is the part we're overriding, everything else is just boilerplate
+            case Tree.Literal(body) if isAnsiEncoded(body) =>
+              // this is the part we're overriding, everything else is just boilerplate
               Result.fromString(fixForFansi(body))
             case _ => super.rec(x, leftOffset, indentCount)
           }
