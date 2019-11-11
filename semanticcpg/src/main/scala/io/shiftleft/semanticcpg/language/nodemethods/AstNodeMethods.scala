918b25e09129c586cf201496e39ded1a510e2e8f
Fabian Yamaguchi <fabs@shiftleft.io>
2019-11-11 15:04:58 +0100

    Added comments.

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/AstNodeMethods.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/AstNodeMethods.scala
@@ -18,8 +18,17 @@ class AstNodeMethods(val node: nodes.AstNode) extends AnyVal {
     * */
   def astMinusRoot: AstNode = node.start.astMinusRoot
 
+  /**
+    * Indicate whether the AST node represents a control structure,
+    * e.g., `if`, `for`, `while`.
+    * */
   def isControlStructure: Boolean = node.start.isControlStructure.size == 1
 
+  /**
+    * The depth of the AST rooted in this node. Upon walking
+    * the tree to its leaves, the depth is only increased for
+    * nodes where `p(node)` is true.
+    * */
   def depth(p: nodes.AstNode => Boolean = _ => true): Int = {
 
     val additionalDepth = if (p(node)) { 1 } else { 0 }
