c93a5ddc1f64950923448fae4694889010752549
Fabian Yamaguchi <fabs@shiftleft.io>
2019-11-13 18:05:44 +0100

    Fix comment.

--- queries/src/test/scala/io/shiftleft/queries/MallocMemcpyTests.scala
+++ queries/src/test/scala/io/shiftleft/queries/MallocMemcpyTests.scala
@@ -21,15 +21,26 @@ class MallocMemcpyTests extends WordSpec with Matchers {
     """
 
   DataFlowCodeToCpgFixture(code) { cpg =>
-    "find calls to malloc where first argument contains addition" in {
 
-      val src = cpg.call("malloc").filter(_.argument(1).containsCallTo("<operator>.add.*"))
+    /**
+      * Find calls to malloc where the first argument contains an addition,
+      * and the return value flows into memcpy as the third argument, and
+      * the third argument of that memcpy is unequal to the first argument
+      * of malloc. This is an adaption of the old-joern query first shown
+      * at 31C3 that found a buffer overflow in VLC's MP4 demuxer (CVE-2014-9626).
+      * */
+
+    "find calls to malloc/memcpy system with different additions in arguments" in {
+
+      val src = cpg.call("malloc")
+        .filter(_.argument(1).containsCallTo("<operator>.add.*"))
 
       cpg
         .call("memcpy")
         .whereNonEmpty { call =>
           val codeInThirdArg = call.argument(3).code
-          call.argument(1).reachableBy(src).filterNot(_.argument(1).codeExact(codeInThirdArg))
+          call.argument(1).reachableBy(src)
+            .filterNot(_.argument(1).codeExact(codeInThirdArg))
         }
         .code
         .l shouldBe List("memcpy(dst, src, len + 7)")
