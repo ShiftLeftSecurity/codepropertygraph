43dd7591dde6f2119988ca0a4551bef3d88f2d7d
Denis Yermakov <lfecdr@gmail.com>
2019-11-22 16:51:46 +0100

    Added ParallelIteratorExecutor for BindingMethodOverridesPass

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/BindingMethodOverridesPass.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/BindingMethodOverridesPass.scala
@@ -1,32 +1,39 @@
 package io.shiftleft.semanticcpg.passes
 
 import io.shiftleft.codepropertygraph.Cpg
+import io.shiftleft.codepropertygraph.generated.nodes.{Binding, Method, TypeDecl}
 import io.shiftleft.codepropertygraph.generated.{NodeKeys, nodes}
-import io.shiftleft.passes.{CpgPass, DiffGraph}
+import io.shiftleft.passes.{CpgPass, DiffGraph, ParallelIteratorExecutor}
 import io.shiftleft.semanticcpg.language._
 import org.apache.logging.log4j.LogManager
 
 class BindingMethodOverridesPass(cpg: Cpg) extends CpgPass(cpg) {
   override def run(): Iterator[DiffGraph] = {
-    cpg.method.map(processMethod).toList().iterator
+    val methodIterator = cpg.method.toIterator().grouped(1024)
+    new ParallelIteratorExecutor(methodIterator).map(processMethods)
   }
 
-  def processMethod(method: nodes.Method): DiffGraph = {
+  private def processMethods(methods: Seq[Method]): DiffGraph = {
     val diff: DiffGraph = new DiffGraph
-    method.start.referencingBinding
-      .toIterator()
-      .foreach(binding => {
-        val typeDecl: Option[nodes.TypeDecl] = binding.start.bindingTypeDecl.headOption()
-        assert(typeDecl.isDefined,
-               "No binding typeDecl found in BindingMethodOverridesPass: " + method.name + " " + method.signature)
-        val neverOverriddenFlag = isMethodNeverOverridden(typeDecl.get, method.name, method.signature)
-        diff.addNodeProperty(
-          binding,
-          NodeKeys.IS_METHOD_NEVER_OVERRIDDEN.name,
-          neverOverriddenFlag.asInstanceOf[AnyRef]
-        )
-      })
+    methods.foreach(method => processSingleMethod(diff, method))
     diff
+  }
+
+  private def processSingleMethod(diff: DiffGraph, method: nodes.Method): Unit = {
+      method.start.referencingBinding.toIterator()
+        .foreach(binding => processBinding(method, diff, binding))
+  }
+
+  private def processBinding(method: Method, diff: DiffGraph, binding: Binding) = {
+    val typeDecl: Option[TypeDecl] = binding.start.bindingTypeDecl.headOption()
+    assert(typeDecl.isDefined,
+      "No binding typeDecl found in BindingMethodOverridesPass: " + method.name + " " + method.signature)
+    val neverOverriddenFlag = isMethodNeverOverridden(typeDecl.get, method.name, method.signature)
+    diff.addNodeProperty(
+      binding,
+      NodeKeys.IS_METHOD_NEVER_OVERRIDDEN.name,
+      neverOverriddenFlag.asInstanceOf[AnyRef]
+    )
   }
 
   def isMethodNeverOverridden(typeDecl: nodes.TypeDecl, methodName: String, methodSignature: String): Boolean = {
