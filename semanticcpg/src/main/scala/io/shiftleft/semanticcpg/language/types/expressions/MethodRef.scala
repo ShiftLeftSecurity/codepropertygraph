995462858ca6e8a9851003e2f1223b260e128b94
Markus Lottmann <markus@shiftleft.io>
2019-11-25 13:25:44 +0100

    Add referencedMethod step to METHOD_REF. (#499)

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/types/expressions/MethodRef.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/types/expressions/MethodRef.scala
@@ -1,12 +1,20 @@
 package io.shiftleft.semanticcpg.language.types.expressions
 
 import gremlin.scala.GremlinScala
-import io.shiftleft.codepropertygraph.generated.nodes
-import io.shiftleft.semanticcpg.language.NodeSteps
+import io.shiftleft.codepropertygraph.generated.{EdgeTypes, nodes}
+import io.shiftleft.semanticcpg.language._
 import io.shiftleft.semanticcpg.language.types.expressions.generalizations.ExpressionBase
 import io.shiftleft.semanticcpg.language.types.propertyaccessors.LineNumberAccessors
+import io.shiftleft.semanticcpg.language.types.structure.Method
 
 class MethodRef(raw: GremlinScala[nodes.MethodRef])
     extends NodeSteps[nodes.MethodRef](raw)
     with ExpressionBase[nodes.MethodRef]
-    with LineNumberAccessors[nodes.MethodRef] {}
+    with LineNumberAccessors[nodes.MethodRef] {
+
+  /**
+    * Traverse to referenced method.
+    * */
+  def referencedMethod: Method =
+    new Method(raw.out(EdgeTypes.REF).cast[nodes.Method])
+}
