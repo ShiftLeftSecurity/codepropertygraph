7765d4826540b88b33039d04ca2b993f9b0bdd8b
Fabian Yamaguchi <fabs@shiftleft.io>
2019-11-25 11:41:48 +0100

    Update semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/nodes/AssignSource.scala
    Co-Authored-By: Jacob Taylor-Hindle <GlassAndOneHalf@users.noreply.github.com>

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/nodes/AssignSource.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/nodes/AssignSource.scala
@@ -2,4 +2,4 @@ package io.shiftleft.semanticcpg.language.operatorextension.nodes
 
 import io.shiftleft.codepropertygraph.generated.{nodes => basenodes}
 
-class AssignSource(val expr: basenodes.Expression) extends AnyRef {}
+class AssignSource(val expr: basenodes.Expression) {}
