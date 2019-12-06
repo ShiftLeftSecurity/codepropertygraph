bef4362e257e1719b4bb9c0746d5b446d2f5f34a
Max Leuthäuser <max.leuthaeuser@gmail.com>
2019-12-06 12:47:22 +0100

    Several fixes for validator facts.

--- cpgvalidator/src/main/scala/io/shiftleft/cpgvalidator/facts/OutFactsImporter.scala
+++ cpgvalidator/src/main/scala/io/shiftleft/cpgvalidator/facts/OutFactsImporter.scala
@@ -15,7 +15,7 @@ class OutFactsImporter extends FactsImporter {
       NodeTypes.METHOD has 1 outgoing EdgeTypes.AST to NodeTypes.BLOCK,
       NodeTypes.METHOD has 0 to N outgoing EdgeTypes.AST to NodeTypes.TYPE_PARAMETER,
       NodeTypes.METHOD has 0 to N outgoing EdgeTypes.AST to NodeTypes.ANNOTATION,
-      NodeTypes.METHOD has 0 to N outgoing EdgeTypes.CFG to SuperTypes.Expression,
+      NodeTypes.METHOD has 0 to N outgoing EdgeTypes.CFG to SuperTypes.Expression or NodeTypes.METHOD_RETURN,
       NodeTypes.METHOD_PARAMETER_IN has 0 to N outgoing EdgeTypes.AST to NodeTypes.ANNOTATION,
       NodeTypes.TYPE has 0 to N outgoing EdgeTypes.AST to NodeTypes.TYPE_ARGUMENT,
       NodeTypes.TYPE_DECL has 0 to N outgoing EdgeTypes.AST to NodeTypes.TYPE_PARAMETER,
@@ -26,25 +26,26 @@ class OutFactsImporter extends FactsImporter {
       NodeTypes.MEMBER has 0 to N outgoing EdgeTypes.AST to NodeTypes.ANNOTATION,
       NodeTypes.MEMBER has 0 to N outgoing EdgeTypes.AST to NodeTypes.MODIFIER,
       NodeTypes.LITERAL has 1 to N outgoing EdgeTypes.CFG to SuperTypes.Expression or NodeTypes.METHOD_RETURN,
-      NodeTypes.CALL has 0 to N outgoing EdgeTypes.AST to SuperTypes.Expression,
+      NodeTypes.CALL has 0 to N outgoing EdgeTypes.AST to SuperTypes.Expression or NodeTypes.CONTROL_STRUCTURE,
       NodeTypes.CALL has 1 to N outgoing EdgeTypes.CFG to SuperTypes.Expression or NodeTypes.METHOD_RETURN,
       NodeTypes.CALL has 0 to 1 outgoing EdgeTypes.RECEIVER to
         NodeTypes.CALL or NodeTypes.IDENTIFIER or NodeTypes.LITERAL or NodeTypes.METHOD_REF or NodeTypes.BLOCK,
       NodeTypes.CALL has 0 to N outgoing EdgeTypes.ARGUMENT to
         NodeTypes.CALL or NodeTypes.IDENTIFIER or NodeTypes.LITERAL or NodeTypes.METHOD_REF or NodeTypes.BLOCK,
-      NodeTypes.IDENTIFIER has 1 to N outgoing EdgeTypes.CFG to SuperTypes.Expression,
+      NodeTypes.IDENTIFIER has 1 to N outgoing EdgeTypes.CFG to SuperTypes.Expression or NodeTypes.METHOD_RETURN,
       NodeTypes.IDENTIFIER has 0 to 1 outgoing EdgeTypes.REF to NodeTypes.LOCAL or NodeTypes.METHOD_PARAMETER_IN,
-      NodeTypes.RETURN has 0 to 1 outgoing EdgeTypes.AST to SuperTypes.Expression,
+      NodeTypes.RETURN has 0 to 1 outgoing EdgeTypes.AST to SuperTypes.Expression or NodeTypes.CONTROL_STRUCTURE,
       NodeTypes.RETURN has 1 outgoing EdgeTypes.CFG to NodeTypes.METHOD_RETURN,
       NodeTypes.RETURN has 0 to 1 outgoing EdgeTypes.ARGUMENT to
         NodeTypes.CALL or NodeTypes.IDENTIFIER or NodeTypes.LITERAL or NodeTypes.METHOD_REF or NodeTypes.BLOCK,
-      NodeTypes.BLOCK has 0 to N outgoing EdgeTypes.AST to SuperTypes.Expression,
+      NodeTypes.BLOCK has 0 to N outgoing EdgeTypes.AST to SuperTypes.Expression or NodeTypes.CONTROL_STRUCTURE,
       NodeTypes.BLOCK has 0 to N outgoing EdgeTypes.AST to NodeTypes.LOCAL,
       NodeTypes.BLOCK has 0 to N outgoing EdgeTypes.CFG to SuperTypes.Expression or NodeTypes.METHOD_RETURN,
-      NodeTypes.METHOD_REF has 1 to N outgoing EdgeTypes.CFG to SuperTypes.Expression,
+      NodeTypes.METHOD_REF has 1 to N outgoing EdgeTypes.CFG to SuperTypes.Expression or NodeTypes.METHOD_RETURN,
       NodeTypes.METHOD_REF has 0 to N outgoing EdgeTypes.CAPTURE to NodeTypes.CLOSURE_BINDING,
       NodeTypes.CLOSURE_BINDING has 1 outgoing EdgeTypes.REF to NodeTypes.LOCAL or NodeTypes.METHOD_PARAMETER_IN,
       NodeTypes.BINDING has 1 outgoing EdgeTypes.REF to NodeTypes.METHOD,
+      NodeTypes.METHOD_RETURN has 0 to 1 outgoing EdgeTypes.CFG to NodeTypes.METHOD,
     )
 
 }
