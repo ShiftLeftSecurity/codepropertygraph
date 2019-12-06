78e0d40dea553600e8015a9336a89ec7c7c5b814
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2019-12-06 14:54:47 +0100

    Fixed Cpg loading. (#513)
    Throw a FileSystemNotFoundException if filename refers to a non-existing file during CpgLoader#load. (issue #511)

--- codepropertygraph/src/main/scala/io/shiftleft/codepropertygraph/cpgloading/ProtoCpgLoader.scala
+++ codepropertygraph/src/main/scala/io/shiftleft/codepropertygraph/cpgloading/ProtoCpgLoader.scala
@@ -7,8 +7,9 @@ import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.proto.cpg.Cpg.{CpgOverlay, CpgStruct}
 import org.apache.logging.log4j.LogManager
 import java.util.{List => JList}
+
 import scala.jdk.CollectionConverters._
-import scala.util.{Try, Using}
+import scala.util.{Failure, Success, Try, Using}
 import io.shiftleft.overflowdb.OdbConfig
 
 object ProtoCpgLoader {
@@ -22,21 +23,19 @@ object ProtoCpgLoader {
           val inputStream = use(Files.newInputStream(entry))
           builder.addNodes(getNextProtoCpgFromStream(inputStream).getNodeList)
         }
-      }
-
-      /* second pass so we can stream for the edges
-       * -> holding them all in memory is potentially too much
-       * -> adding them as we go isn't an option because we may only have one of the adjacent vertices
-       * TODO double check: is that really so? protos don't really allow for streaming, so this may be unnecessary overhead
-       */
-      Using.Manager { use =>
+        /* second pass so we can stream for the edges
+         * -> holding them all in memory is potentially too much
+         * -> adding them as we go isn't an option because we may only have one of the adjacent vertices
+         * TODO double check: is that really so? protos don't really allow for streaming, so this may be unnecessary overhead
+         */
         use(new ZipArchive(fileName)).entries.foreach { entry =>
           val inputStream = use(Files.newInputStream(entry))
           builder.addEdges(getNextProtoCpgFromStream(inputStream).getEdgeList)
         }
+      } match {
+        case Failure(exception) => throw exception
+        case Success(_)         => builder.build()
       }
-
-      builder.build()
     }
 
   def loadFromListOfProtos(cpgs: Seq[CpgStruct], overflowDbConfig: OdbConfig): Cpg = {
