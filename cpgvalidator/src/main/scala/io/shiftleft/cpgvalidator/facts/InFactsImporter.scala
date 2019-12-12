ac814c32a7148d476f30e724cb9e073737346ba0
Max Leuthäuser <max.leuthaeuser@gmail.com>
2019-12-12 15:27:28 +0100

    Allow for AST edges between METHOD <-> BLOCK, TYPE_DECL <-> METHOD, METHOD <-> NAMESPACE_BLOCK.

--- cpgvalidator/src/main/scala/io/shiftleft/cpgvalidator/facts/InFactsImporter.scala
+++ cpgvalidator/src/main/scala/io/shiftleft/cpgvalidator/facts/InFactsImporter.scala
@@ -8,7 +8,10 @@ class InFactsImporter extends FactsImporter {
 
   override def loadFacts: List[InFact] =
     List(
+      NodeTypes.METHOD has 0 to N incoming EdgeTypes.AST from NodeTypes.BLOCK,
+      NodeTypes.METHOD has 0 to 1 incoming EdgeTypes.AST from NodeTypes.TYPE_DECL,
       NodeTypes.METHOD has 0 to N incoming EdgeTypes.REF from NodeTypes.BINDING,
+      NodeTypes.METHOD has 0 to 1 incoming EdgeTypes.AST from NodeTypes.NAMESPACE_BLOCK,
       NodeTypes.METHOD_PARAMETER_IN has 1 incoming EdgeTypes.AST from NodeTypes.METHOD,
       NodeTypes.METHOD_PARAMETER_IN has 0 to N incoming EdgeTypes.REF from NodeTypes.IDENTIFIER or NodeTypes.CLOSURE_BINDING,
       NodeTypes.METHOD_RETURN has 1 incoming EdgeTypes.AST from NodeTypes.METHOD,
@@ -20,6 +23,8 @@ class InFactsImporter extends FactsImporter {
       NodeTypes.TYPE_PARAMETER has 0 to N incoming EdgeTypes.BINDS_TO from NodeTypes.TYPE_ARGUMENT,
       NodeTypes.MEMBER has 1 incoming EdgeTypes.AST from NodeTypes.TYPE_DECL,
       NodeTypes.NAMESPACE_BLOCK has 0 to 1 incoming EdgeTypes.AST from NodeTypes.FILE,
+      NodeTypes.NAMESPACE_BLOCK has 0 to N incoming EdgeTypes.AST from NodeTypes.METHOD,
+      NodeTypes.NAMESPACE_BLOCK has 0 to N incoming EdgeTypes.AST from NodeTypes.TYPE_DECL,
       NodeTypes.LITERAL has 1 incoming EdgeTypes.AST from SuperTypes.Expression or NodeTypes.CONTROL_STRUCTURE,
       NodeTypes.LITERAL has 0 to N incoming EdgeTypes.CFG from SuperTypes.Expression or NodeTypes.METHOD,
       NodeTypes.LITERAL has 0 to 1 incoming EdgeTypes.RECEIVER from NodeTypes.CALL,
