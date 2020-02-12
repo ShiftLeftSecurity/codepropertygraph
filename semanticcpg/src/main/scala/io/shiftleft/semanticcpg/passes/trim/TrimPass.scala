7ebb714dcac1c3511d9b802677585ec0cf6d8555
Bernhard <bernhard@shiftleft.io>
2020-02-12 18:24:38 +0100

    improve readability

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/trim/TrimPass.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/trim/TrimPass.scala
@@ -4,19 +4,15 @@ import io.shiftleft.overflowdb.{NodeRef, OdbNode}
 import io.shiftleft.passes.{CpgPass, DiffGraph}
 import org.apache.logging.log4j.{LogManager, Logger}
 import io.shiftleft.semanticcpg.language._
-import io.shiftleft.codepropertygraph.generated.nodes
 
 class TrimPass(cpg: Cpg) extends CpgPass(cpg) {
   override def run():Iterator[DiffGraph] = {
     val reduction = cpg.all.toStream().mapToLong {
-      _ match {
-        case (onode: io.shiftleft.overflowdb.OdbNode) => onode.trim()
-        case oref if oref.isInstanceOf[io.shiftleft.overflowdb.NodeRef[_]] => oref.asInstanceOf[io.shiftleft.overflowdb.NodeRef[OdbNode]].get().trim()
-      }
-    }.sum()
-    val oldsz = reduction >> 32
-    val newsz = reduction & 0x00000000ffffffffL
-    TrimPass.logger.debug(s"Trim pass: reduced number of refs from ${oldsz} to ${newsz}, i.e. by factor of ${1.0 - newsz.toFloat / oldsz.toFloat}")
+      _.asInstanceOf[io.shiftleft.overflowdb.NodeRef[OdbNode]].get().trim()
+      }.sum()
+    val oldSize = reduction >>> 32
+    val newSize = reduction & 0x00000000ffffffffL
+    TrimPass.logger.debug(s"Trim pass: reduced number of refs from ${oldSize} to ${newSize}, i.e. by factor of ${1.0 - newSize.toFloat / oldSize.toFloat}")
     Iterator.empty[DiffGraph]
   }
 }
