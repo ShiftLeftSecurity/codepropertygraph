ade813d9f96b6800c2348a1abf5c8021609ac074
Bernhard <bernhard@shiftleft.io>
2020-02-12 19:00:21 +0100

    scalafmt

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/trim/TrimPass.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/trim/TrimPass.scala
@@ -6,13 +6,17 @@ import org.apache.logging.log4j.{LogManager, Logger}
 import io.shiftleft.semanticcpg.language._
 
 class TrimPass(cpg: Cpg) extends CpgPass(cpg) {
-  override def run():Iterator[DiffGraph] = {
-    val reduction = cpg.all.toStream().mapToLong {
-      _.asInstanceOf[io.shiftleft.overflowdb.NodeRef[OdbNode]].get().trim()
-      }.sum()
+  override def run(): Iterator[DiffGraph] = {
+    val reduction = cpg.all
+      .toStream()
+      .mapToLong {
+        _.asInstanceOf[io.shiftleft.overflowdb.NodeRef[OdbNode]].get().trim()
+      }
+      .sum()
     val oldSize = reduction >>> 32
     val newSize = reduction & 0x00000000ffffffffL
-    TrimPass.logger.debug(s"Trim pass: reduced number of refs from ${oldSize} to ${newSize}, i.e. by factor of ${1.0 - newSize.toFloat / oldSize.toFloat}")
+    TrimPass.logger.debug(
+      s"Trim pass: reduced number of refs from ${oldSize} to ${newSize}, i.e. by factor of ${1.0 - newSize.toFloat / oldSize.toFloat}")
     Iterator.empty[DiffGraph]
   }
 }
