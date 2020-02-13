a0169ea556961f7418222cebae967b976e814e87
Denis Yermakov <lfecdr@gmail.com>
2020-02-13 23:02:23 +0200

    cpg2overflowdb script

--- cpg2overflowdb/src/main/scala/io/shiftleft/codepropertygraph/cpgloading/ProtoToOverflowDb.scala
+++ cpg2overflowdb/src/main/scala/io/shiftleft/codepropertygraph/cpgloading/ProtoToOverflowDb.scala
@@ -1,97 +1,62 @@
-// package io.shiftleft.codepropertygraph.cpgloading
+ package io.shiftleft.codepropertygraph.cpgloading
 
-// import java.io.{File, FileInputStream}
-// import java.nio.file.{Files, Path}
-// import java.util.{HashMap => JHashMap, Map => JMap}
+ import java.io.{File}
+ import io.shiftleft.overflowdb.{OdbConfig, OdbGraph}
+ import org.apache.logging.log4j.LogManager
 
-// import gnu.trove.set.TLongSet
-// import gnu.trove.set.hash.TLongHashSet
-// import io.shiftleft.proto.cpg.Cpg.CpgStruct
-// import org.apache.logging.log4j.LogManager
-// import org.apache.tinkerpop.gremlin.tinkergraph.storage.OndiskOverflow
+ /**
+   * converts cpg.bin.zip proto to OverflowDb, so we don't need to first import into Tinkergraph and then serialize it out again
+   * Useful especially for large cpgs. Uses scala parallel collections.
+   *
+   * Usage example: create the overflowdb and load it in ocular. This will load all references in memory, and accessed elements/properties will be lazily fetched from disk.
+   * ./proto2overflowdb.sh --cpg cpg.bin.zip --out overflowdb.bin
+   * ./ocular.sh
+   * Cpg.withStorage("overflowdb.bin")
+   */
+ object ProtoToOverflowDb {
+   type NodeId = java.lang.Long
+   type EdgeLabel = String
 
-// import scala.jdk.CollectionConverters._
-// import resource.managed
+   private lazy val logger = LogManager.getLogger(getClass)
+ //  private lazy val edgeSerializer = new ProtoEdgeSerializer
+   private lazy val nodeFilter = new NodeFilter
 
-// /**
-//   * converts cpg.bin.zip proto to OverflowDb, so we don't need to first import into Tinkergraph and then serialize it out again
-//   * Useful especially for large cpgs. Uses scala parallel collections.
-//   *
-//   * Usage example: create the overflowdb and load it in ocular. This will load all references in memory, and accessed elements/properties will be lazily fetched from disk.
-//   * ./proto2overflowdb.sh --cpg cpg.bin.zip --out overflowdb.bin
-//   * ./ocular.sh
-//   * Cpg.withStorage("overflowdb.bin")
-//   */
-// object ProtoToOverflowDb extends App {
-//   type NodeId = java.lang.Long
-//   type EdgeLabel = String
+   def main(args: Array[String]): Unit = {
+     parseConfig(args).map(run)
+   }
 
-//   private lazy val logger = LogManager.getLogger(getClass)
-// //  private lazy val edgeSerializer = new ProtoEdgeSerializer
-//   private lazy val nodeFilter = new NodeFilter
 
-//   parseConfig.map(run)
+   def run(config: Config): File = {
+     val writeTo = config.writeTo.getOrElse(new File("overflowdb.bin"))
+     logger.info(s"running ProtoToOverflowDb with cpg=${config.cpg}; writing results to $writeTo")
+     if (writeTo.exists) writeTo.delete()
+     val start = System.currentTimeMillis
+     val odbConfig = OdbConfig.withDefaults()
+       .withStorageLocation(writeTo.getAbsolutePath)
+     if (!config.enableOverflow)
+      odbConfig.disableOverflow()
+     val cpg = CpgLoader.load(filename = config.cpg.getAbsolutePath, CpgLoaderConfig.withDefaults.withOverflowConfig(odbConfig).createIndexesOnLoad)
+     cpg.graph.asInstanceOf[OdbGraph].close()
+     logger.info("OverflowDb construction finished in " + (System.currentTimeMillis - start) + "ms.")
+     writeTo
+   }
 
-//   def run(config: Config): File = {
-//     val writeTo = config.writeTo.getOrElse(new File("overflowdb.bin"))
-//     logger.info(s"running ProtoToOverflowDb with cpg=${config.cpg}; writing results to $writeTo")
-//     if (writeTo.exists) writeTo.delete()
-//     val start = System.currentTimeMillis
+   private def parseConfig(args: Array[String]): Option[Config] = {
+     new scopt.OptionParser[Config](getClass.getSimpleName) {
+       opt[Boolean]("enable-overflow")
+           .optional()
+           .action((x, c) => c.copy(enableOverflow = x))
+           .text("Enable disk overflow when there is not enough of memory")
+       opt[File]("cpg").required
+         .action((x, c) => c.copy(cpg = x))
+         .text("path to cpg.bin.zip")
+       opt[File]("out")
+         .action((x, c) => c.copy(writeTo = Some(x)))
+         .text("where to write overflowdb.bin output to (will delete if exists)")
+       help("help").text("prints this usage text")
+     }.parse(args, Config(cpg = null))
+   }
 
-//     for (overflowDb <- managed(OndiskOverflow.createWithSpecificLocation(writeTo));
-//          zipArchive <- managed(new ZipArchive(config.cpg.getAbsolutePath))) {
-//       zipArchive.getFileEntries.asScala.par.foreach(importProtoBin(overflowDb))
-//     }
-//     logger.info("OverflowDb construction finished in " + (System.currentTimeMillis - start) + "ms.")
-//     writeTo
-//   }
+ }
 
-//   private def importProtoBin(overflowDb: OndiskOverflow)(protoFile: Path): Unit =
-//     for (inputStream <- managed(Files.newInputStream(protoFile))) {
-//       importCpgStruct(CpgStruct.parseFrom(inputStream), overflowDb)
-//     }
-
-//   private def importCpgStruct(cpgProto: CpgStruct, overflowDb: OndiskOverflow): Unit = {
-
-//     /** cpg proto nodes don't know their adjacent edges, but those are required for the OverflowDb serializer,
-//       * so we need to build some helper maps to import the nodes */
-//     val inEdgesByNodeId: JMap[NodeId, JMap[EdgeLabel, TLongSet]] = new JHashMap
-//     val outEdgesByNodeId: JMap[NodeId, JMap[EdgeLabel, TLongSet]] = new JHashMap
-
-//     cpgProto.getEdgeList.asScala.par.foreach { edge =>
-//       val edgeWithId = new ProtoEdgeWithId(edge)
-//       val label = edge.getType.name
-//       inEdgesByNodeId
-//         .computeIfAbsent(edgeWithId.edge.getSrc, _ => new JHashMap)
-//         .computeIfAbsent(label, _ => new TLongHashSet)
-//         .add(edgeWithId.id)
-//       outEdgesByNodeId
-//         .computeIfAbsent(edgeWithId.edge.getDst, _ => new JHashMap)
-//         .computeIfAbsent(label, _ => new TLongHashSet)
-//         .add(edgeWithId.id)
-//       ???
-// //      overflowDb.getEdgeMVMap.put(edgeWithId.id, edgeSerializer.serialize(edgeWithId))
-//     }
-
-// //    val nodeSerializer = new ProtoNodeSerializer(inEdgesByNodeId, outEdgesByNodeId)
-//     cpgProto.getNodeList.asScala.par.filter(nodeFilter.filterNode).foreach { node =>
-//       ???
-// //      overflowDb.getVertexMVMap.put(node.getKey, nodeSerializer.serialize(node))
-//     }
-//   }
-
-//   private def parseConfig: Option[Config] = {
-//     new scopt.OptionParser[Config](getClass.getSimpleName) {
-//       opt[File]("cpg").required
-//         .action((x, c) => c.copy(cpg = x))
-//         .text("path to cpg.bin.zip")
-//       opt[File]("out")
-//         .action((x, c) => c.copy(writeTo = Some(x)))
-//         .text("where to write overflowdb.bin output to (will delete if exists)")
-//       help("help").text("prints this usage text")
-//     }.parse(args, Config(cpg = null))
-//   }
-
-// }
-
-// case class Config(cpg: File, writeTo: Option[File] = None)
+ case class Config(cpg: File, writeTo: Option[File] = None, enableOverflow: Boolean = false)
