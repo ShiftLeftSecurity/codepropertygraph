e3a948b42f25005e5675db6a06b43a9d471fd49b
Fabian Yamaguchi <fabs@shiftleft.io>
2020-02-21 16:38:18 +0100

    Don't run trim pass.

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/layers/EnhancedBaseCreator.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/layers/EnhancedBaseCreator.scala
@@ -64,7 +64,6 @@ class EnhancedBaseCreator(cpg: Cpg, language: String, serializedCpg: SerializedC
           new NamespaceCreator(cpg),
           new CfgDominatorPass(cpg),
           new CdgPass(cpg),
-          new TrimPass(cpg),
         )
     }
   }
