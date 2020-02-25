c606a1ec559ad834372cf857cf9f946bb056b7eb
Fabian Yamaguchi <fabs@shiftleft.io>
2020-02-25 18:45:30 +0100

    Revert "Create TypeDeclStubs in parallel. (#603)"
    This reverts commit b1172fc317fded6d89599d79bc276f44613048bc.

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/languagespecific/fuzzyc/TypeDeclStubCreator.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/languagespecific/fuzzyc/TypeDeclStubCreator.scala
@@ -3,7 +3,7 @@ package io.shiftleft.semanticcpg.passes.languagespecific.fuzzyc
 import gremlin.scala._
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.codepropertygraph.generated.{NodeTypes, nodes}
-import io.shiftleft.passes.{CpgPass, DiffGraph, ParallelIteratorExecutor}
+import io.shiftleft.passes.{CpgPass, DiffGraph}
 import io.shiftleft.semanticcpg.language._
 
 /**
@@ -17,23 +17,25 @@ class TypeDeclStubCreator(cpg: Cpg) extends CpgPass(cpg) {
   private var typeDeclFullNameToNode = Map[String, nodes.TypeDeclBase]()
 
   override def run(): Iterator[DiffGraph] = {
+    val dstGraph = DiffGraph.newBuilder
 
     init()
 
-    val typeNodeIterator = cpg.graph.V.hasLabel(NodeTypes.TYPE).toIterator()
-
-    new ParallelIteratorExecutor[Vertex](typeNodeIterator).map { node =>
-      val typ = node.asInstanceOf[nodes.Type]
-      implicit val dstGraph: DiffGraph.Builder = DiffGraph.newBuilder
-      typeDeclFullNameToNode.get(typ.fullName) match {
-        case Some(_) =>
-        case None =>
-          val newTypeDecl = createTypeDeclStub(typ.name, typ.fullName)
-          typeDeclFullNameToNode += typ.fullName -> newTypeDecl
-          dstGraph.addNode(newTypeDecl)
+    cpg.graph.V
+      .hasLabel(NodeTypes.TYPE)
+      .sideEffectWithTraverser { traverser =>
+        val typ = traverser.get.asInstanceOf[nodes.Type]
+        typeDeclFullNameToNode.get(typ.fullName) match {
+          case Some(_) =>
+          case None =>
+            val newTypeDecl = createTypeDeclStub(typ.name, typ.fullName)
+            typeDeclFullNameToNode += typ.fullName -> newTypeDecl
+            dstGraph.addNode(newTypeDecl)
+        }
       }
-      dstGraph.build()
-    }
+      .iterate()
+
+    Iterator(dstGraph.build())
   }
 
   private def createTypeDeclStub(name: String, fullName: String): nodes.NewTypeDecl = {
