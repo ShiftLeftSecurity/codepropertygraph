26ee04c88690fc0ee822455d06a8646098a1710d
Michael Pollmeier <michael@michaelpollmeier.com>
2020-03-12 12:22:31 +1300

    ARM all the things

--- dataflowengine/src/main/scala/io/shiftleft/dataflowengine/language/Path.scala
+++ dataflowengine/src/main/scala/io/shiftleft/dataflowengine/language/Path.scala
@@ -4,38 +4,42 @@ import dnl.utils.text.table.TextTable
 import io.shiftleft.semanticcpg.language.Show
 import java.io.{ByteArrayOutputStream, PrintStream}
 import java.nio.charset.StandardCharsets
+
 import io.shiftleft.codepropertygraph.generated.nodes
 import io.shiftleft.semanticcpg.language._
 import io.shiftleft.semanticcpg.language.nodemethods.CfgNodeMethods
+
+import scala.util.Using
 
 case class Path(elements: List[nodes.TrackingPoint])
 
 object Path {
 
-  implicit val show: Show[Path] = (path: Path) => {
-    val baos = new ByteArrayOutputStream
-    val ps = new PrintStream(baos, true, "utf-8")
-    val rows = path.elements.map { trackingPoint =>
-      val method = trackingPoint.method
-      val methodName= method.name
-      val lineNumber = trackingPoint.cfgNode.lineNumber.getOrElse("N/A")
-      val fileName = method.start.file.name.headOption.getOrElse("N/A")
+  implicit val show: Show[Path] = (path: Path) =>
+    Using.Manager { use =>
+      val baos = use(new ByteArrayOutputStream)
+      val ps = use(new PrintStream(baos, true, "utf-8"))
 
-      val trackedSymbol = trackingPoint match {
-        case _: nodes.MethodParameterIn =>
-          val paramsPretty = method.parameter.l.sortBy(_.order).map(_.code).mkString(", ")
-          s"$methodName($paramsPretty)"
-        case _ => CfgNodeMethods.repr(trackingPoint.cfgNode)
+      val rows = path.elements.map { trackingPoint =>
+        val method = trackingPoint.method
+        val methodName= method.name
+        val lineNumber = trackingPoint.cfgNode.lineNumber.getOrElse("N/A")
+        val fileName = method.start.file.name.headOption.getOrElse("N/A")
+
+        val trackedSymbol = trackingPoint match {
+          case _: nodes.MethodParameterIn =>
+            val paramsPretty = method.parameter.l.sortBy(_.order).map(_.code).mkString(", ")
+            s"$methodName($paramsPretty)"
+          case _ => CfgNodeMethods.repr(trackingPoint.cfgNode)
+        }
+
+        s"$trackedSymbol\t$lineNumber\t$methodName\t$fileName"
       }
 
-      s"$trackedSymbol\t$lineNumber\t$methodName\t$fileName"
-    }
-    val columnNames = List("tracked", "lineNumber", "method", "file").toArray
-    val data = rows.map(_.split("\t").toArray[Object]).toArray
-    new TextTable(columnNames, data).printTable(ps, 1)
-    val content = new String(baos.toByteArray, StandardCharsets.UTF_8)
-    ps.close()
-    content
-  }
+      val columnNames = Array("tracked", "lineNumber", "method", "file")
+      val data = rows.map(_.split("\t").toArray[Object]).toArray
+      new TextTable(columnNames, data).printTable(ps, 1)
+      new String(baos.toByteArray, StandardCharsets.UTF_8)
+    }.get
 
 }
