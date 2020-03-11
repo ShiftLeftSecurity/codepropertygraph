1405dbcf39b4f5b1816b1bbaa4a8ba8b12b10e40
Michael Pollmeier <michael@michaelpollmeier.com>
2020-03-12 12:13:53 +1300

    refactor path Show instance
    * no more `.start`
    * easier to read string concat
    * ...

--- dataflowengine/src/main/scala/io/shiftleft/dataflowengine/language/Path.scala
+++ dataflowengine/src/main/scala/io/shiftleft/dataflowengine/language/Path.scala
@@ -11,31 +11,31 @@ import io.shiftleft.semanticcpg.language.nodemethods.CfgNodeMethods
 case class Path(elements: List[nodes.TrackingPoint])
 
 object Path {
+
   implicit val show: Show[Path] = (path: Path) => {
     val baos = new ByteArrayOutputStream
     val ps = new PrintStream(baos, true, "utf-8")
     val rows = path.elements.map { trackingPoint =>
-      implicit val graph = trackingPoint.underlying.graph
       val method = trackingPoint.method
+      val methodName= method.name
+      val lineNumber = trackingPoint.cfgNode.lineNumber.getOrElse("N/A")
+      val fileName = method.start.file.name.headOption.getOrElse("N/A")
 
       val trackedSymbol = trackingPoint match {
-        case methodParamIn: nodes.MethodParameterIn =>
-          s"${method.name}(${method.start.parameter.l.sortBy(_.order).map(_.code).mkString(", ")})"
+        case _: nodes.MethodParameterIn =>
+          val paramsPretty = method.parameter.l.sortBy(_.order).map(_.code).mkString(", ")
+          s"$methodName($paramsPretty)"
         case _ => CfgNodeMethods.repr(trackingPoint.cfgNode)
       }
 
-      val lineNumberOption = trackingPoint.cfgNode.lineNumber
-      val methodNameOption = Some(method.name)
-      val fileOption = method.start.file.name.headOption
-
-      s"$trackedSymbol\t${lineNumberOption.getOrElse("N/A")}\t" +
-        s"${methodNameOption.getOrElse("N/A")}\t${fileOption.getOrElse("N/A")}"
+      s"$trackedSymbol\t$lineNumber\t$methodName\t$fileName"
     }
     val columnNames = List("tracked", "lineNumber", "method", "file").toArray
-    val data = rows.map(_.split("\t").toArray).toArray.asInstanceOf[Array[Array[Object]]]
+    val data = rows.map(_.split("\t").toArray[Object]).toArray
     new TextTable(columnNames, data).printTable(ps, 1)
     val content = new String(baos.toByteArray, StandardCharsets.UTF_8)
     ps.close()
-    content.toString
+    content
   }
+
 }
