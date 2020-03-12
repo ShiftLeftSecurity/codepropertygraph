ec4158e34875c6773b8b9cedc0f8e18d496d7553
Michael Pollmeier <michael@michaelpollmeier.com>
2020-03-13 03:03:02 +1300

    refactor Show instances: cpg, cs: Path, Finding, Flow/NewFlow (#626)
    * refactor path Show instance
    
    * no more `.start`
    * easier to read string concat
    * ...
    
    * ARM all the things
    
    * fmt

--- dataflowengine/src/main/scala/io/shiftleft/dataflowengine/language/Path.scala
+++ dataflowengine/src/main/scala/io/shiftleft/dataflowengine/language/Path.scala
@@ -4,38 +4,42 @@ import dnl.utils.text.table.TextTable
 import io.shiftleft.semanticcpg.language.Show
 import java.io.{ByteArrayOutputStream, PrintStream}
 import java.nio.charset.StandardCharsets
+
 import io.shiftleft.codepropertygraph.generated.nodes
 import io.shiftleft.semanticcpg.language._
 import io.shiftleft.semanticcpg.language.nodemethods.CfgNodeMethods
 
+import scala.util.Using
+
 case class Path(elements: List[nodes.TrackingPoint])
 
 object Path {
-  implicit val show: Show[Path] = (path: Path) => {
-    val baos = new ByteArrayOutputStream
-    val ps = new PrintStream(baos, true, "utf-8")
-    val rows = path.elements.map { trackingPoint =>
-      implicit val graph = trackingPoint.underlying.graph
-      val method = trackingPoint.method
 
-      val trackedSymbol = trackingPoint match {
-        case methodParamIn: nodes.MethodParameterIn =>
-          s"${method.name}(${method.start.parameter.l.sortBy(_.order).map(_.code).mkString(", ")})"
-        case _ => CfgNodeMethods.repr(trackingPoint.cfgNode)
+  implicit val show: Show[Path] = (path: Path) =>
+    Using.Manager { use =>
+      val baos = use(new ByteArrayOutputStream)
+      val ps = use(new PrintStream(baos, true, "utf-8"))
+
+      val rows = path.elements.map { trackingPoint =>
+        val method = trackingPoint.method
+        val methodName = method.name
+        val lineNumber = trackingPoint.cfgNode.lineNumber.getOrElse("N/A")
+        val fileName = method.start.file.name.headOption.getOrElse("N/A")
+
+        val trackedSymbol = trackingPoint match {
+          case _: nodes.MethodParameterIn =>
+            val paramsPretty = method.parameter.l.sortBy(_.order).map(_.code).mkString(", ")
+            s"$methodName($paramsPretty)"
+          case _ => CfgNodeMethods.repr(trackingPoint.cfgNode)
+        }
+
+        s"$trackedSymbol\t$lineNumber\t$methodName\t$fileName"
       }
 
-      val lineNumberOption = trackingPoint.cfgNode.lineNumber
-      val methodNameOption = Some(method.name)
-      val fileOption = method.start.file.name.headOption
+      val columnNames = Array("tracked", "lineNumber", "method", "file")
+      val data = rows.map(_.split("\t").toArray[Object]).toArray
+      new TextTable(columnNames, data).printTable(ps, 1)
+      new String(baos.toByteArray, StandardCharsets.UTF_8)
+    }.get
 
-      s"$trackedSymbol\t${lineNumberOption.getOrElse("N/A")}\t" +
-        s"${methodNameOption.getOrElse("N/A")}\t${fileOption.getOrElse("N/A")}"
-    }
-    val columnNames = List("tracked", "lineNumber", "method", "file").toArray
-    val data = rows.map(_.split("\t").toArray).toArray.asInstanceOf[Array[Array[Object]]]
-    new TextTable(columnNames, data).printTable(ps, 1)
-    val content = new String(baos.toByteArray, StandardCharsets.UTF_8)
-    ps.close()
-    content.toString
-  }
 }
