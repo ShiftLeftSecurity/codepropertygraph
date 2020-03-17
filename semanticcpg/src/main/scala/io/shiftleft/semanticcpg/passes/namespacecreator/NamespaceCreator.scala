b44c718b7b598f56cf240424b0121210d5bc1c4f
Michael Pollmeier <michael@michaelpollmeier.com>
2020-03-17 13:54:41 +1300

    fix

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/namespacecreator/NamespaceCreator.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/namespacecreator/NamespaceCreator.scala
@@ -1,12 +1,9 @@
 package io.shiftleft.semanticcpg.passes.namespacecreator
 
-import gremlin.scala._
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.codepropertygraph.generated.{EdgeTypes, nodes}
 import io.shiftleft.passes.{CpgPass, DiffGraph}
 import io.shiftleft.semanticcpg.language._
-
-import scala.collection.mutable
 
 /**
   * Creates NAMESPACE nodes and connects NAMESPACE_BLOCKs
@@ -21,17 +18,12 @@ class NamespaceCreator(cpg: Cpg) extends CpgPass(cpg) {
     * to corresponding NAMESPACE nodes.
     * */
   override def run(): Iterator[DiffGraph] = {
-    val blocksByName = cpg.namespaceBlock.toBuffer.groupBy(_.name)
     val dstGraph = DiffGraph.newBuilder
-
-    blocksByName.foreach {
-      case (name: String, blocks: mutable.Buffer[Vertex]) =>
+    cpg.namespaceBlock.toBuffer.groupBy(_.name).foreach {
+      case (name: String, blocks) =>
         val namespace = new nodes.NewNamespace(name)
         dstGraph.addNode(namespace)
-        blocks.foreach {
-          case block: nodes.NamespaceBlock =>
-            dstGraph.addEdgeFromOriginal(block, namespace, EdgeTypes.REF)
-        }
+        blocks.foreach(block => dstGraph.addEdgeFromOriginal(block, namespace, EdgeTypes.REF))
     }
     Iterator(dstGraph.build())
   }
