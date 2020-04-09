6410ac98c22a1e9cc06086cb614502cf018f33f4
Markus Lottmann <markus@shiftleft.io>
2020-04-09 16:26:40 +0200

    Route all call site to method expansions via ICallResolver interface #662
    Route all call site to method expansions via ICallResolver interface.
    
    This enables us to not write call resolution results directly into the
    graph. Instead those results will be provided by the new ICallResolver
    method getResolvedCalledMethods and getResolvedCallsites.
    
    From now on all call site to method expansion HAVE TO go via ICallResolver.
    
    Reworked ICallResolver interface.
    The interface now how getCalledMethods and getMethodCallsites as
    main user facing methods which trigger the resolution and return
    results. Additonally it is possible to just trigger callsite or method
    resolution or just get the resolution results.

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/callgraphextension/Call.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/callgraphextension/Call.scala
@@ -1,7 +1,7 @@
 package io.shiftleft.semanticcpg.language.callgraphextension
 
 import gremlin.scala.GremlinScala
-import io.shiftleft.codepropertygraph.generated.{EdgeTypes, nodes}
+import io.shiftleft.codepropertygraph.generated.nodes
 import io.shiftleft.semanticcpg.language._
 
 class Call(val wrapped: NodeSteps[nodes.Call]) extends AnyVal {
@@ -9,12 +9,9 @@ class Call(val wrapped: NodeSteps[nodes.Call]) extends AnyVal {
 
   /** The callee method */
   def calledMethod(implicit callResolver: ICallResolver): NodeSteps[nodes.Method] = {
-    new NodeSteps(
-      wrapped
-        .sideEffect(callResolver.resolveDynamicCallSite)
-        .raw
-        .out(EdgeTypes.CALL)
-        .cast[nodes.Method])
+    new NodeSteps(wrapped.raw.flatMap { call =>
+      callResolver.getCalledMethodsAsTraversal(call)
+    })
   }
 
 }
