ac220bf591af7f8f13b10a11fd51010a811dc68a
Fabian Yamaguchi <fabs@shiftleft.io>
2020-04-18 18:01:40 +0200

    Make EnhancementCreator a LayerCreator (#678)
    * Introduce LayerCreator.
    
    * Make `EnhancementBaseCreator` a `LayerCreator` named `Semanticcpg`

--- dataflowengine/src/test/scala/io/shiftleft/dataflowengine/language/DataFlowCodeToCpgFixture.scala
+++ dataflowengine/src/test/scala/io/shiftleft/dataflowengine/language/DataFlowCodeToCpgFixture.scala
@@ -3,7 +3,7 @@ package io.shiftleft.dataflowengine.language
 import io.shiftleft.SerializedCpg
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.dataflowengine.layers.dataflows.DataFlowRunner
-import io.shiftleft.semanticcpg.layers.EnhancementRunner
+import io.shiftleft.semanticcpg.layers.{LayerCreatorContext, Semanticcpg}
 import io.shiftleft.semanticcpg.testfixtures.{CodeToCpgFixture, LanguageFrontend}
 import io.shiftleft.dataflowengine.semanticsloader.SemanticsLoader
 
@@ -15,7 +15,8 @@ object DataFlowCodeToCpgFixture {
     new CodeToCpgFixture(frontend).buildCpg(sourceCode, passes)(fun)
 
   private def passes(cpg: Cpg): Unit = {
-    new EnhancementRunner().run(cpg, new SerializedCpg())
+    val context = new LayerCreatorContext(cpg, new SerializedCpg())
+    new Semanticcpg().run(context)
     val semantics = new SemanticsLoader("dataflowengine/src/test/resources/default.semantics").load()
     new DataFlowRunner(semantics).run(cpg, new SerializedCpg())
   }
