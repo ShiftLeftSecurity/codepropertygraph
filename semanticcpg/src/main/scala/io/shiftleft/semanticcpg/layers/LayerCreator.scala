5a0ed245bcf72b1fddd07056a604b48ed9afc046
Fabian Yamaguchi <fabs@shiftleft.io>
2020-04-18 03:26:31 +0200

    Introduce LayerCreator.

---
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/layers/LayerCreator.scala
@@ -0,0 +1,45 @@
+package io.shiftleft.semanticcpg.layers
+
+import io.shiftleft.SerializedCpg
+import io.shiftleft.codepropertygraph.Cpg
+import org.apache.logging.log4j.LogManager
+import io.shiftleft.semanticcpg.Overlays
+
+abstract class LayerCreator {
+
+  private val logger = LogManager.getLogger(this.getClass)
+
+  val overlayName: String
+  val description: String
+  val dependsOn: List[String] = List()
+
+  def run(context: LayerCreatorContext,
+          options: Option[LayerCreatorOptions] = None,
+          serializeInverse: Boolean = false): Unit = {
+    val appliedOverlays = Overlays.appliedOverlays(context.cpg).toSet
+    if (!dependsOn.toSet.subsetOf(appliedOverlays)) {
+      logger.warn(
+        s"${this.getClass.getName} depends on $dependsOn but CPG only has $appliedOverlays - skipping creation")
+    } else if (appliedOverlays.contains(overlayName)) {
+      logger.warn(s"The overlay $overlayName already exists - skipping creation")
+    } else {
+      create(context, options, serializeInverse)
+    }
+  }
+
+  def create(context: LayerCreatorContext,
+             options: Option[LayerCreatorOptions],
+             serializeInverse: Boolean = false): Unit
+
+  /**
+    * Heuristically determine if overlay has been
+    * applied to the CPG already. Used as a fallback
+    * when `metaData.overlays` is not present due
+    * to conversion from legacy CPG.
+    * */
+  def probe(cpg: Cpg): Boolean
+
+}
+
+class LayerCreatorContext(val cpg: Cpg, val serializedCpg: SerializedCpg = new SerializedCpg()) {}
+class LayerCreatorOptions()
