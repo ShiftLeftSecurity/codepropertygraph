f9bc57b38d7eee3148552bc29f95743a92bcfe87
Fabian Yamaguchi <fabs@shiftleft.io>
2020-04-19 21:36:44 +0200

    Simplify Console/ScriptManager relation.

---
+++ console/src/main/scala/io/shiftleft/console/Console.scala
@@ -0,0 +1,7 @@
+package io.shiftleft.console
+
+import io.shiftleft.console.scripting.{AmmoniteExecutor, ScriptManager}
+
+class Console(executor : AmmoniteExecutor) extends ScriptManager(executor) {
+
+}
