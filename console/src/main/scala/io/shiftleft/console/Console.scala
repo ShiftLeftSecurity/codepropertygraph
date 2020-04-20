f325baa03f71205efa6f84e2ba81ab3e899cd6f7
Fabian Yamaguchi <fabs@shiftleft.io>
2020-04-20 14:43:22 +0200

    Move more code over from `io.shiftleft.repl`

--- console/src/main/scala/io/shiftleft/console/Console.scala
+++ console/src/main/scala/io/shiftleft/console/Console.scala
@@ -1,9 +1,13 @@
 package io.shiftleft.console
 
+import better.files.Dsl.rm
+import better.files.File
 import gremlin.scala.ScalaGraph
 import io.shiftleft.codepropertygraph.Cpg
+import io.shiftleft.codepropertygraph.cpgloading.CpgLoader
 import io.shiftleft.console.scripting.{AmmoniteExecutor, ScriptManager}
 import io.shiftleft.console.workspacehandling.{Project, WorkspaceLoader, WorkspaceManager}
+import io.shiftleft.semanticcpg.Overlays
 import io.shiftleft.semanticcpg.layers.LayerCreator
 
 abstract class Console(executor: AmmoniteExecutor, loader : WorkspaceLoader) extends ScriptManager(executor) {
@@ -75,5 +79,112 @@ abstract class Console(executor: AmmoniteExecutor, loader : WorkspaceLoader) ext
   def cpg: Cpg = workspace.cpg
 
   implicit def graph: ScalaGraph = cpg.scalaGraph
+
+  @Doc(
+    "Open project",
+    """
+      |open([projectName])
+      |
+      |Opens the project named `name` and make it the active project.
+      |If `name` is not provided, the active project is opened. If `name`
+      |is a path, the project name is derived from and a deprecation
+      |warning is printed.
+      |
+      |Upon completion of this operation, the CPG stored in this project
+      |can be queried via `cpg`. Returns an optional reference to the
+      |project, which is empty on error.
+      |""".stripMargin,
+    """open("projectName")"""
+  )
+  def open(name: String): Option[Project] = {
+    val projectName = fixProjectNameAndComplainOnFix(name)
+    workspace.openProject(projectName).map { project =>
+      project
+    }
+  }
+
+  /**
+    * Open the active project
+    * */
+  def open: Option[Project] = {
+    workspace.projects.lastOption.flatMap { p =>
+      open(p.name)
+    }
+  }
+
+  // We still tie the project name to the input path here
+  // if no project name has been provided.
+
+  protected def fixProjectNameAndComplainOnFix(name: String): String = {
+    val projectName = Some(name)
+      .filter(_.contains(java.io.File.pathSeparator))
+      .map(deriveNameFromInputPath)
+      .getOrElse(name)
+    if (name != projectName) {
+      System.err.println("Passing paths to `loadCpg` is deprecated, please use a project name")
+    }
+    projectName
+  }
+
+  protected def deriveNameFromInputPath(inputPath: String): String = {
+    val name    = File(inputPath).name
+    val project = workspace.project(name)
+    if (project.isDefined && project.exists(_.inputPath != inputPath)) {
+      var i = 1
+      while (workspace.project(name + i).isDefined) {
+        i += 1
+      }
+      name + i
+    } else {
+      name
+    }
+  }
+
+  /**
+    * Delete project from disk and remove it from
+    * the workspace manager. Returns the (now invalid)
+    * project.
+    * @param name the name of the project
+    * */
+  @Doc("Close and remove project from disk", "", "delete(projectName)")
+  def delete(name: String): Option[Unit] = {
+    defaultProjectNameIfEmpty(name).flatMap(workspace.deleteProject)
+  }
+
+  /**
+    * Delete the active project
+    * */
+  def delete: Option[Unit] = delete("")
+
+  protected def defaultProjectNameIfEmpty(name: String): Option[String] = {
+    if (name.isEmpty) {
+      val projectNameOpt = workspace.projectByCpg(cpg).map(_.name)
+      if (projectNameOpt.isEmpty) {
+        report("Fatal: cannot find project for active CPG")
+      }
+      projectNameOpt
+    } else {
+      Some(fixProjectNameAndComplainOnFix(name))
+    }
+  }
+
+  @Doc(
+    "undo effects of analyzer",
+    """|Undo the last change, that is, unapply the last
+       |overlay applied to the active project.
+       |""".stripMargin,
+    "undo"
+  )
+  def undo: Unit = {
+    project.overlayFiles.lastOption
+      .map { file =>
+        CpgLoader.addDiffGraphs(List(file.path.toString), cpg)
+        Overlays.removeLastOverlayName(cpg)
+        rm(file)
+      }
+      .getOrElse(throw new RuntimeException("No overlays present"))
+  }
+
+  protected def report(string: String): Unit = System.err.println(string)
 
 }
