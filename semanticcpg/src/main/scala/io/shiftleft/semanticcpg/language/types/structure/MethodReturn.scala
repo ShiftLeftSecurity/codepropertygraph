f3d99754e2e7bfc1df0a787f233e96d25ba05d50
Michael Pollmeier <michael@michaelpollmeier.com>
2020-04-26 11:48:09 +1200

    add some @Doc entries and cleanup Steps

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/types/structure/MethodReturn.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/types/structure/MethodReturn.scala
@@ -2,11 +2,14 @@ package io.shiftleft.semanticcpg.language.types.structure
 
 import gremlin.scala._
 import io.shiftleft.codepropertygraph.generated.{EdgeTypes, nodes}
+import io.shiftleft.overflowdb.traversal.help.{Doc, TraversalExt}
 import io.shiftleft.semanticcpg.language._
 
+@TraversalExt(elementType = classOf[nodes.MethodReturn])
 class MethodReturn(val wrapped: NodeSteps[nodes.MethodReturn]) extends AnyVal {
   private def raw: GremlinScala[nodes.MethodReturn] = wrapped.raw
 
+  @Doc("traverse to parent method")
   def method: NodeSteps[nodes.Method] =
     new NodeSteps(raw.in(EdgeTypes.AST).cast[nodes.Method])
 
@@ -17,12 +20,14 @@ class MethodReturn(val wrapped: NodeSteps[nodes.MethodReturn]) extends AnyVal {
     *  Traverse to last expressions in CFG.
     *  Can be multiple.
     */
+  @Doc("traverse to last expressions in CFG (can be multiple)")
   def cfgLast: NodeSteps[nodes.Expression] =
     new NodeSteps(raw.in(EdgeTypes.CFG).cast[nodes.Expression])
 
   /**
     * Traverse to return type
     * */
+  @Doc("traverse to return type")
   def typ: NodeSteps[nodes.Type] =
     new NodeSteps(raw.out(EdgeTypes.EVAL_TYPE).cast[nodes.Type])
 
