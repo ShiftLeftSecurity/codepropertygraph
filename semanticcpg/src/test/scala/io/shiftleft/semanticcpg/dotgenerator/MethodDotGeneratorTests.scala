7657f39038085093aa74ca9d6571871cf84211ae
Fabian Yamaguchi <fabs@shiftleft.io>
2020-05-06 19:31:42 +0200

    Rewrite AST plotting (it was broken)

--- semanticcpg/src/test/scala/io/shiftleft/semanticcpg/dotgenerator/MethodDotGeneratorTests.scala
+++ semanticcpg/src/test/scala/io/shiftleft/semanticcpg/dotgenerator/MethodDotGeneratorTests.scala
@@ -25,50 +25,23 @@ class MethodDotGeneratorTests extends WordSpec with Matchers {
        |}
        |""".stripMargin
 
-  private val expectedMyFuncRegex =
-    """|digraph my_func \{
-       | "\d+" -> "\d+" \[label="y = x \* 2"\];
-       | "\d+" -> "\d+" \[label="if \(y > 42\)"\];
-       | "\d+" -> "\d+" \[label="y > 42"\];
-       | "\d+" -> "\d+" \[label="BLOCK"\];
-       | "\d+" -> "\d+" \[label="return y;"\];
-       | "\d+" -> "\d+" \[label="else"\];
-       | "\d+" -> "\d+" \[label="BLOCK"\];
-       | "\d+" -> "\d+" \[label="return sqrt\(y\);"\];
-       |\}
-       |""".stripMargin
-
   "A MethodDotGenerator" should {
     CodeToCpgFixture(code) { cpg =>
-      "return an empty list for an empty method set" in {
-        MethodDotGenerator.toDotGraph(cpg.method.name("ohnoes")) shouldBe empty
+      "generate dot graph" in {
+        val dotLines = cpg.method.name("my_func").dot
+        dotLines.size shouldBe 1
+        val myFuncDot = dotLines.head
+        myFuncDot.startsWith("digraph my_func") shouldBe true
+        myFuncDot.contains("""[label = "(CONTROL_STRUCTURE,if (y > 42))" ]""") shouldBe true
+        myFuncDot.endsWith("}")
       }
 
-      "return a dot representation of all methods" in {
-        val List(boopGraph, myFuncGraph) = MethodDotGenerator.toDotGraph(cpg.method).sortBy(_.length)
-
-        val expectedBoopRegex =
-          """|digraph boop \{
-             | "\d+" -> "\d+" \[label="printf\(\\"Boop!\\"\)"\];
-             | "\d+" -> "\d+" \[label="return;"\];
-             |\}
-             |""".stripMargin
-
-        myFuncGraph should fullyMatch regex expectedMyFuncRegex
-        boopGraph should fullyMatch regex expectedBoopRegex
+      "allow selection method" in {
+        val dotLines = cpg.method.name("boop").dot
+        dotLines.size shouldBe 1
+        dotLines.head.startsWith("digraph boop")
       }
 
-      "return a dot representation of all methods matching the query criterion" in {
-        val List(dotGraph) = MethodDotGenerator.toDotGraph(cpg.method.nameExact("my_func"))
-
-        dotGraph should fullyMatch regex expectedMyFuncRegex
-      }
-
-      "return the dot representation of a method using the query language extensions" in {
-        val List(dotGraph) = cpg.method.nameExact("my_func").dot
-
-        dotGraph should fullyMatch regex expectedMyFuncRegex
-      }
     }
   }
 }
