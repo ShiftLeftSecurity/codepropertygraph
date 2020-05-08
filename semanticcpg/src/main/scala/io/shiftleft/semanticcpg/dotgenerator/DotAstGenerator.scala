142836c315093337f8511893e51b9b3bd0b11dbe
Fabian Yamaguchi <fabs@shiftleft.io>
2020-05-08 17:19:38 +0200

    Allow plotting of CFGs from console (#725)
    * WIP plot CFG.
    
    * Completed CFG plotting
    
    * Tests.

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/dotgenerator/DotAstGenerator.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/dotgenerator/DotAstGenerator.scala
@@ -8,15 +8,9 @@ object DotAstGenerator {
   def toDotAst[T <: nodes.AstNode](step: NodeSteps[T]): Steps[String] = step.map(dotAst)
 
   def dotAst(astRoot: nodes.AstNode): String = {
-    val sb = new StringBuilder
-    val name = astRoot match {
-      case method: nodes.Method => method.name
-      case _                    => ""
-    }
-    sb.append(s"digraph $name {\n")
+    val sb = Shared.namedGraphBegin(astRoot)
     sb.append(nodesAndEdges(astRoot).mkString("\n"))
-    sb.append("\n}\n")
-    sb.toString
+    Shared.graphEnd(sb)
   }
 
   private def nodesAndEdges(astRoot: nodes.AstNode): List[String] = {
@@ -27,7 +21,7 @@ object DotAstGenerator {
     val edges = vertices.map(v => (v.getId, v.start.astChildren.where(shouldBeDisplayed).id.l))
 
     val nodeStrings = vertices.map { node =>
-      s""""${node.getId}" [label = "${escape(stringRepr(node))}" ]""".stripMargin
+      s""""${node.getId}" [label = "${Shared.stringRepr(node)}" ]""".stripMargin
     }
 
     val edgeStrings = edges.flatMap {
@@ -38,18 +32,4 @@ object DotAstGenerator {
     nodeStrings ++ edgeStrings
   }
 
-  private def stringRepr(vertex: nodes.AstNode): String = {
-    vertex match {
-      case call: nodes.Call               => (call.name, call.code).toString
-      case expr: nodes.Expression         => (expr.label, expr.code).toString
-      case method: nodes.Method           => (method.label, method.name).toString
-      case ret: nodes.MethodReturn        => (ret.label, ret.typeFullName).toString
-      case param: nodes.MethodParameterIn => ("PARAM", param.code).toString
-      case _                              => ""
-    }
-  }
-
-  private def escape(str: String): String = {
-    str.replaceAllLiterally("\"", "\\\"")
-  }
 }
