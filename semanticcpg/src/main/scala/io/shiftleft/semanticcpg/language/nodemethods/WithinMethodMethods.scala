27a396a90ac5e0b38d1c3b49300b2dafb343a98e
Michael Pollmeier <michael@michaelpollmeier.com>
2020-05-19 10:59:05 +1200

    fix inconsistency in literal.method between traversal and node level (#740)
    re https://github.com/ShiftLeftSecurity/product/issues/4707

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/WithinMethodMethods.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/WithinMethodMethods.scala
@@ -1,9 +1,9 @@
 package io.shiftleft.semanticcpg.language.nodemethods
 
-import io.shiftleft.codepropertygraph.generated.nodes
-import io.shiftleft.codepropertygraph.generated.EdgeTypes
 import io.shiftleft.Implicits.JavaIteratorDeco
-import org.apache.tinkerpop.gremlin.structure.Direction
+import io.shiftleft.codepropertygraph.generated.nodes
+
+import scala.annotation.tailrec
 
 class WithinMethodMethods(val node: nodes.WithinMethod) extends AnyVal {
   def method: nodes.Method = node match {
@@ -12,13 +12,17 @@ class WithinMethodMethods(val node: nodes.WithinMethod) extends AnyVal {
     case node: nodes.MethodParameterOut => walkUpAst(node)
     case node: nodes.MethodReturn       => walkUpAst(node)
     case node: nodes.ImplicitCall       => walkUpAst(node)
-    case node: nodes.Expression         => walkUpContainsEdges(node)
+    case node: nodes.Expression         => walkUpAstRecursively(node)
   }
 
   private def walkUpAst(node: nodes.WithinMethod): nodes.Method =
     node._astIn.onlyChecked.asInstanceOf[nodes.Method]
 
-  private def walkUpContainsEdges(node: nodes.WithinMethod): nodes.Method =
-    node._containsIn.onlyChecked.asInstanceOf[nodes.Method]
+  @tailrec
+  private def walkUpAstRecursively(node: nodes.WithinMethod): nodes.Method =
+    node._astIn.onlyChecked match {
+      case method: nodes.Method      => method
+      case other: nodes.WithinMethod => walkUpAstRecursively(other)
+    }
 
 }
