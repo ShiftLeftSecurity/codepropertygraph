e87c0ebbc207d6c9b9b9951dd8f6e5073a76123b
Michael Pollmeier <michael@michaelpollmeier.com>
2020-05-25 16:27:59 +1200

    use new graph api (just a POC for now)
    depends on https://github.com/ShiftLeftSecurity/overflowdb/pull/99

--- codepropertygraph/build.sbt
+++ codepropertygraph/build.sbt
@@ -3,7 +3,7 @@ name := "codepropertygraph"
 dependsOn(Projects.protoBindings)
 
 libraryDependencies ++= Seq(
-  "io.shiftleft" %% "overflowdb-traversal" % "0.75",
+  "io.shiftleft" %% "overflowdb-traversal" % "534b1019efa1c438cfeffa3c55ffc5cead83977b",
   "com.michaelpollmeier" %% "gremlin-scala" % "3.4.4.5",
   "com.google.guava" % "guava" % "21.0",
   "org.apache.commons" % "commons-lang3" % "3.5",
--- semanticcpg/src/test/scala/io/shiftleft/semanticcpg/passes/CapturingLinkerTests.scala
+++ semanticcpg/src/test/scala/io/shiftleft/semanticcpg/passes/CapturingLinkerTests.scala
@@ -1,29 +1,32 @@
 package io.shiftleft.semanticcpg.passes
 
-import gremlin.scala._
 import io.shiftleft.codepropertygraph.Cpg
-import io.shiftleft.codepropertygraph.generated.{EdgeTypes, NodeKeys, NodeTypes}
+import io.shiftleft.codepropertygraph.generated.{EdgeTypes, NodeKeyNames, NodeTypes}
+import io.shiftleft.overflowdb._
 import io.shiftleft.semanticcpg.passes.linking.capturinglinker.CapturingLinker
 import io.shiftleft.semanticcpg.testfixtures.EmptyGraphFixture
 import org.scalatest.{Matchers, WordSpec}
 
+import scala.jdk.CollectionConverters._
+
 class CapturingLinkerTests extends WordSpec with Matchers {
 
   "link CLOSURE_BINDING and LOCALS with same CLOSURE_BINDING_IDs" in EmptyGraphFixture { graph =>
-    val closureBinding1 = graph + (NodeTypes.CLOSURE_BINDING, NodeKeys.CLOSURE_BINDING_ID -> "id1")
-    val closureBinding2 = graph + (NodeTypes.CLOSURE_BINDING, NodeKeys.CLOSURE_BINDING_ID -> "id2")
-    val local1 = graph + (NodeTypes.LOCAL, NodeKeys.CLOSURE_BINDING_ID -> "id1")
-    val local2 = graph + (NodeTypes.LOCAL, NodeKeys.CLOSURE_BINDING_ID -> "id2")
+    // TODO generate in codegen
+    val closureBindingId = io.shiftleft.overflowdb.traversal.PropertyKey[String](NodeKeyNames.CLOSURE_BINDING_ID)
+
+    val closureBinding1 = graph + (NodeTypes.CLOSURE_BINDING, closureBindingId -> "id1")
+    val closureBinding2 = graph + (NodeTypes.CLOSURE_BINDING, closureBindingId -> "id2")
+    val local1 = graph + (NodeTypes.LOCAL, closureBindingId -> "id1")
+    val local2 = graph + (NodeTypes.LOCAL, closureBindingId -> "id2")
 
     val capturingLinker = new CapturingLinker(new Cpg(graph))
     capturingLinker.createAndApply()
 
-    val capturedByDest1 = local1.out(EdgeTypes.CAPTURED_BY).toList()
-    capturedByDest1.size shouldBe 1
-    capturedByDest1.head shouldBe closureBinding1
+    val capturedByDest1 = local1.nodesOut(EdgeTypes.CAPTURED_BY).asScala.toList
+    capturedByDest1 shouldBe List(closureBinding1)
 
-    val capturedByDest2 = local2.out(EdgeTypes.CAPTURED_BY).toList()
-    capturedByDest2.size shouldBe 1
-    capturedByDest2.head shouldBe closureBinding2
+    val capturedByDest2 = local2.nodesOut(EdgeTypes.CAPTURED_BY).asScala.toList
+    capturedByDest2 shouldBe List(closureBinding2)
   }
 }
