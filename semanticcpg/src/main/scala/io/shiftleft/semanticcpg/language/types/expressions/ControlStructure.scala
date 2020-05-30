48cbc55ffd36ca576520d735e464fe33e381a873
Fabian Yamaguchi <fabs@shiftleft.io>
2020-05-30 14:19:35 +0200

    A bit of polishing on `ControlStructure` step.

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/types/expressions/ControlStructure.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/types/expressions/ControlStructure.scala
@@ -2,6 +2,7 @@ package io.shiftleft.semanticcpg.language.types.expressions
 
 import gremlin.scala.GremlinScala
 import io.shiftleft.codepropertygraph.generated.{EdgeTypes, NodeKeys, nodes}
+import io.shiftleft.overflowdb.traversal.help.Doc
 import io.shiftleft.semanticcpg.language._
 
 object ControlStructure {
@@ -13,15 +14,19 @@ class ControlStructure(val wrapped: NodeSteps[nodes.ControlStructure]) extends A
   import ControlStructure._
   private def raw: GremlinScala[nodes.ControlStructure] = wrapped.raw
 
-  /**
-    * The expression introduced by this control structure, if any
-    * */
+  @Doc("The condition associated with this control structure")
   def condition: NodeSteps[nodes.Expression] =
     new NodeSteps(raw.out(EdgeTypes.CONDITION).cast[nodes.Expression])
 
+  @Doc("Control structures where condition.code matches regex")
+  def condition(regex: String): NodeSteps[nodes.ControlStructure] =
+    wrapped.filter(_.condition.code(regex))
+
+  @Doc("Sub tree taken when condition evaluates to true")
   def whenTrue: NodeSteps[nodes.AstNode] =
     new NodeSteps(raw.out.has(NodeKeys.ORDER, secondChildIndex).cast[nodes.AstNode])
 
+  @Doc("Sub tree taken when condition evaluates to false")
   def whenFalse: NodeSteps[nodes.AstNode] =
     new NodeSteps(raw.out.has(NodeKeys.ORDER, thirdChildIndex).cast[nodes.AstNode])
 
