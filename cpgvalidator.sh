e98dea4911cdf457479e14142076bbf31aa65f66
Michael Pollmeier <michael@michaelpollmeier.com>
2020-06-02 21:17:19 +1200

    cpgvalidator: support for both old and new cpg formats (#781)
    defaulting to the new format

--- cpgvalidator.sh
+++ cpgvalidator.sh
@@ -2,8 +2,6 @@
 
 SCRIPT_ABS_PATH=$(readlink -f "$0")
 SCRIPT_ABS_DIR=$(dirname $SCRIPT_ABS_PATH)
-CPG_FILE=$(readlink -f $@)
 
-cd $SCRIPT_ABS_DIR
-$SCRIPT_ABS_DIR/cpgvalidator/target/universal/stage/bin/cpgvalidator $CPG_FILE
+$SCRIPT_ABS_DIR/cpgvalidator/target/universal/stage/bin/cpgvalidator $@
 
--- cpgvalidator/src/main/scala/io/shiftleft/cpgvalidator/CpgValidatorMain.scala
+++ cpgvalidator/src/main/scala/io/shiftleft/cpgvalidator/CpgValidatorMain.scala
@@ -1,19 +1,46 @@
 package io.shiftleft.cpgvalidator
 
+import java.io.File
+
 import io.shiftleft.codepropertygraph.cpgloading.{CpgLoader, CpgLoaderConfig}
 import io.shiftleft.cpgvalidator.validators.CpgValidator
+import io.shiftleft.overflowdb.OdbConfig
 
 object CpgValidatorMain extends App {
-  val cpgFileName = args(0)
-  val cpg = CpgLoader.load(cpgFileName, CpgLoaderConfig.withoutOverflow)
-  val errorRegistry = new ValidationErrorRegistry
-  val validator = new CpgValidator(errorRegistry)
-  val cpgValid = validator.validate(cpg)
-  errorRegistry.logValidationErrors()
+  case class Config(cpgPath: String, isOldProtoCpg: Boolean = false)
 
-  if (cpgValid) {
-    System.exit(0)
-  } else {
-    System.exit(1)
+  def parseConfig: Option[Config] =
+    new scopt.OptionParser[Config](getClass.getSimpleName) {
+      opt[String]("cpg").required
+        .action((x, c) => c.copy(cpgPath = x))
+        .text("path to cpg")
+        .validate { cpgPath =>
+          if (new File(cpgPath).exists) success
+          else failure(s"$cpgPath does not exist")
+        }
+      opt[Unit]("oldProtoCpg").optional
+        .action((x, c) => c.copy(isOldProtoCpg = true))
+        .text("set flag if input is an old cpg proto (usually cpg.bin.zip)")
+      help("help").text("prints this usage text")
+    }.parse(args, Config(""))
+
+  parseConfig.foreach { config =>
+    val cpg =
+      if (config.isOldProtoCpg) CpgLoader.load(config.cpgPath, CpgLoaderConfig.withoutOverflow)
+      else
+        CpgLoader.loadFromOverflowDb(
+          CpgLoaderConfig.withoutOverflow.doNotCreateIndexesOnLoad
+            .withOverflowConfig(OdbConfig.withoutOverflow.withStorageLocation(config.cpgPath)))
+
+    val errorRegistry = new ValidationErrorRegistry
+    val validator = new CpgValidator(errorRegistry)
+    val cpgValid = validator.validate(cpg)
+    errorRegistry.logValidationErrors()
+
+    if (cpgValid) {
+      System.exit(0)
+    } else {
+      System.exit(1)
+    }
   }
 }
