9893ed91705bc111aa4ffdc5ed466d30cbbca3d8
Fabian Yamaguchi <fabs@shiftleft.io>
2020-06-11 20:42:02 +0200

    Rename `calledMethod` to `callee`

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/callgraphextension/Call.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/callgraphextension/Call.scala
@@ -1,14 +1,12 @@
 package io.shiftleft.semanticcpg.language.callgraphextension
 
-import gremlin.scala.GremlinScala
 import io.shiftleft.codepropertygraph.generated.nodes
 import io.shiftleft.semanticcpg.language._
 
 class Call(val wrapped: NodeSteps[nodes.Call]) extends AnyVal {
-  private def raw: GremlinScala[nodes.Call] = wrapped.raw
 
   /** The callee method */
-  def calledMethod(implicit callResolver: ICallResolver): NodeSteps[nodes.Method] = {
+  def callee(implicit callResolver: ICallResolver): NodeSteps[nodes.Method] = {
     new NodeSteps(wrapped.raw.flatMap { call =>
       callResolver.getCalledMethodsAsTraversal(call)
     })
