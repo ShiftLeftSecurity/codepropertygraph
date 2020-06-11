44b9d3654645270d913df878b9341eaa2b87d39d
Fabian Yamaguchi <fabs@shiftleft.io>
2020-06-11 23:25:58 +0200

    Revert "Cleanup for call graph language (#798)"
    This reverts commit d47f3465505ef6b28b4f670cb587ab6e73414f7a.

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/callgraphextension/Call.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/callgraphextension/Call.scala
@@ -1,15 +1,14 @@
 package io.shiftleft.semanticcpg.language.callgraphextension
 
+import gremlin.scala.GremlinScala
 import io.shiftleft.codepropertygraph.generated.nodes
 import io.shiftleft.semanticcpg.language._
 
 class Call(val wrapped: NodeSteps[nodes.Call]) extends AnyVal {
-
-  @deprecated("Use callee", "")
-  def calledMethod(implicit callResolver: ICallResolver): NodeSteps[nodes.Method] = callee
+  private def raw: GremlinScala[nodes.Call] = wrapped.raw
 
   /** The callee method */
-  def callee(implicit callResolver: ICallResolver): NodeSteps[nodes.Method] = {
+  def calledMethod(implicit callResolver: ICallResolver): NodeSteps[nodes.Method] = {
     new NodeSteps(wrapped.raw.flatMap { call =>
       callResolver.getCalledMethodsAsTraversal(call)
     })
