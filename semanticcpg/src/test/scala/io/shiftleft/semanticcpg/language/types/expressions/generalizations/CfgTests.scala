3e19d5dcb7205d3a9abafd2a8c0e4709a130a0b4
Fabian Yamaguchi <fabs@shiftleft.io>
2020-06-13 22:03:16 +0200

    Add test

---
+++ semanticcpg/src/test/scala/io/shiftleft/semanticcpg/language/types/expressions/generalizations/CfgTests.scala
@@ -0,0 +1,32 @@
+package io.shiftleft.semanticcpg.language.types.expressions.generalizations
+
+import io.shiftleft.semanticcpg.testfixtures.CodeToCpgFixture
+import org.scalatest.{Matchers, WordSpec}
+import io.shiftleft.semanticcpg.language._
+
+class CfgTests extends WordSpec with Matchers {
+
+  val code =
+    """
+      | int foo(int y, int y) {
+      |  if (y < 10)
+      |    goto end;
+      |  if (x < 10) {
+      |    sink(x);
+      |  }
+      |  end:
+      |  printf("foo");
+      | }
+    """.stripMargin
+
+  CodeToCpgFixture(code) { cpg =>
+    "should find that sink is control dependent on condition" in {
+      val controllers = cpg.call("sink").controlledBy.toSet
+      val controllerCalls = controllers.start.isCall.toSet()
+      controllers shouldBe controllerCalls
+      controllerCalls.map(_.code) should contain("y < 10")
+      controllerCalls.map(_.code) should contain("x < 10")
+    }
+  }
+
+}
