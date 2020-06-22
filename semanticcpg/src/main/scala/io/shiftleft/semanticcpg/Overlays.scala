5bc9e00df86242117c05eba4cd603863b1bebb32
Michael Pollmeier <michael@michaelpollmeier.com>
2020-06-22 17:49:56 +1200

    overlays

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/Overlays.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/Overlays.scala
@@ -1,17 +1,17 @@
 package io.shiftleft.semanticcpg
 
-import gremlin.scala._
 import io.shiftleft.codepropertygraph.Cpg
-import io.shiftleft.codepropertygraph.generated.nodes.MetaData
-import org.apache.tinkerpop.gremlin.structure.VertexProperty.Cardinality
+import io.shiftleft.codepropertygraph.generated.NodeKeysOdb
 import io.shiftleft.semanticcpg.language._
+import overflowdb._
 
 object Overlays {
 
   def appendOverlayName(cpg: Cpg, name: String): Unit = {
-    cpg.metaData.l.headOption match {
+    cpg.metaData.headOption match {
       case Some(metaData) =>
-        metaData.property(Cardinality.list, MetaData.PropertyNames.Overlays, name)
+        val newValue = metaData.overlays :+ name
+        metaData.setProperty2(NodeKeysOdb.OVERLAYS, newValue)
       case None =>
         System.err.println("Missing metaData block")
     }
@@ -19,22 +19,9 @@ object Overlays {
 
   def removeLastOverlayName(cpg: Cpg): Unit = {
     cpg.metaData.headOption match {
-      case Some(metaData) => {
-        // This is currently sub-optimal: we do not have
-        // and operation to simply set a list as a property,
-        // all we have is `removeProperty` and an append-
-        //operation via `.property(Cardinality.list,...`).
-        // Moreover, `remove` only works if the list has
-        // only a single element, hence the call to
-        // `property("OVERLAYS", "")` right before the remove.
-        // It works, but it's not pretty.
+      case Some(metaData) =>
         val newValue = metaData.overlays.dropRight(1)
-        metaData.property(MetaData.PropertyNames.Overlays, "")
-        metaData.removeProperty(Key(MetaData.PropertyNames.Overlays))
-        newValue.foreach { e =>
-          metaData.property(Cardinality.list, MetaData.PropertyNames.Overlays, e)
-        }
-      }
+        metaData.setProperty2(NodeKeysOdb.OVERLAYS, newValue)
       case None =>
         System.err.println("Missing metaData block")
     }
