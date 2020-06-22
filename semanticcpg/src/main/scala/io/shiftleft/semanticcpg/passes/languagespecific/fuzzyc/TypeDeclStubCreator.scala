7b8af0cc9ed47339a84363b53482ae5bec36bbba
Michael Pollmeier <michael@michaelpollmeier.com>
2020-06-23 10:09:18 +1200

    use odb api instead of tp: several passes (#813)
    * use odb api instead of tp
    
    * more of the same
    
    * port Linker
    
    * port other passes
    
    * overlays
    
    * upgrade odb

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/languagespecific/fuzzyc/TypeDeclStubCreator.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/languagespecific/fuzzyc/TypeDeclStubCreator.scala
@@ -1,10 +1,10 @@
 package io.shiftleft.semanticcpg.passes.languagespecific.fuzzyc
 
-import gremlin.scala._
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.codepropertygraph.generated.{NodeTypes, nodes}
 import io.shiftleft.passes.{CpgPass, DiffGraph}
 import io.shiftleft.semanticcpg.language._
+import overflowdb.traversal.Traversal
 
 /**
   * This pass has no other pass as prerequisite.
@@ -21,25 +21,20 @@ class TypeDeclStubCreator(cpg: Cpg) extends CpgPass(cpg) {
 
     init()
 
-    cpg.scalaGraph.V
-      .hasLabel(NodeTypes.TYPE)
-      .sideEffectWithTraverser { traverser =>
-        val typ = traverser.get.asInstanceOf[nodes.Type]
-        typeDeclFullNameToNode.get(typ.fullName) match {
-          case Some(_) =>
-          case None =>
-            val newTypeDecl = createTypeDeclStub(typ.name, typ.fullName)
-            typeDeclFullNameToNode += typ.fullName -> newTypeDecl
-            dstGraph.addNode(newTypeDecl)
-        }
+    // TODO MP use `cpg.typ` once that's defined in odb api
+    Traversal(cpg.graph.nodesByLabel(NodeTypes.TYPE)).cast[nodes.Type].foreach { typ =>
+      if (!typeDeclFullNameToNode.isDefinedAt(typ.fullName)) {
+        val newTypeDecl = createTypeDeclStub(typ.name, typ.fullName)
+        typeDeclFullNameToNode += typ.fullName -> newTypeDecl
+        dstGraph.addNode(newTypeDecl)
       }
-      .iterate()
+    }
 
     Iterator(dstGraph.build())
   }
 
   private def createTypeDeclStub(name: String, fullName: String): nodes.NewTypeDecl = {
-    new nodes.NewTypeDecl(
+    nodes.NewTypeDecl(
       name,
       fullName,
       isExternal = true,
--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/methoddecorations/MethodDecoratorPass.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/methoddecorations/MethodDecoratorPass.scala
@@ -1,12 +1,10 @@
 package io.shiftleft.semanticcpg.passes.methoddecorations
 
-import gremlin.scala._
+import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.codepropertygraph.generated.{EdgeTypes, NodeTypes, nodes}
 import io.shiftleft.passes.{CpgPass, DiffGraph}
-import io.shiftleft.Implicits.JavaIteratorDeco
-import io.shiftleft.codepropertygraph.Cpg
 import org.apache.logging.log4j.{LogManager, Logger}
-import org.apache.tinkerpop.gremlin.structure.Direction
+import overflowdb.traversal.Traversal
 
 /**
   * Adds a METHOD_PARAMETER_OUT for each METHOD_PARAMETER_IN to the graph and
@@ -26,40 +24,39 @@ class MethodDecoratorPass(cpg: Cpg) extends CpgPass(cpg) {
   override def run() = {
     val dstGraph = DiffGraph.newBuilder
 
-    cpg.scalaGraph.V
-      .hasLabel(NodeTypes.METHOD_PARAMETER_IN)
-      .sideEffect {
-        case parameterIn: nodes.MethodParameterIn =>
-          if (!parameterIn._parameterLinkOut().hasNext) {
-            val parameterOut = new nodes.NewMethodParameterOut(
-              parameterIn.code,
-              parameterIn.order,
-              parameterIn.name,
-              parameterIn.evaluationStrategy,
-              parameterIn.typeFullName,
-              parameterIn.lineNumber,
-              parameterIn.columnNumber,
-            )
+    // TODO MP use `cpg.methodParamIn` once that's defined in odb api
+    Traversal(cpg.graph.nodesByLabel(NodeTypes.METHOD_PARAMETER_IN)).cast[nodes.MethodParameterIn].foreach {
+      parameterIn =>
+        if (!parameterIn._parameterLinkOut.hasNext) {
+          val parameterOut = nodes.NewMethodParameterOut(
+            parameterIn.code,
+            parameterIn.order,
+            parameterIn.name,
+            parameterIn.evaluationStrategy,
+            parameterIn.typeFullName,
+            parameterIn.lineNumber,
+            parameterIn.columnNumber,
+          )
 
-            val method = parameterIn._methodViaAstIn
-            if (parameterIn.typeFullName == null) {
-              val evalType = parameterIn._typeViaEvalTypeOut
-              dstGraph.addEdgeToOriginal(parameterOut, evalType, EdgeTypes.EVAL_TYPE)
-              if (!loggedMissingTypeFullName) {
-                logger.warn("Using deprecated CPG format with missing TYPE_FULL_NAME on METHOD_PARAMETER_IN nodes.")
-                loggedMissingTypeFullName = true
-              }
+          val method = parameterIn._methodViaAstIn
+          if (parameterIn.typeFullName == null) {
+            val evalType = parameterIn._typeViaEvalTypeOut
+            dstGraph.addEdgeToOriginal(parameterOut, evalType, EdgeTypes.EVAL_TYPE)
+            if (!loggedMissingTypeFullName) {
+              logger.warn("Using deprecated CPG format with missing TYPE_FULL_NAME on METHOD_PARAMETER_IN nodes.")
+              loggedMissingTypeFullName = true
             }
-
-            dstGraph.addNode(parameterOut)
-            dstGraph.addEdgeFromOriginal(method, parameterOut, EdgeTypes.AST)
-            dstGraph.addEdgeFromOriginal(parameterIn, parameterOut, EdgeTypes.PARAMETER_LINK)
-          } else if (!loggedDeprecatedWarning) {
-            logger.warn("Using deprecated CPG format with PARAMETER_LINK edges")
-            loggedDeprecatedWarning = true
           }
-      }
-      .iterate
+
+          dstGraph.addNode(parameterOut)
+          dstGraph.addEdgeFromOriginal(method, parameterOut, EdgeTypes.AST)
+          dstGraph.addEdgeFromOriginal(parameterIn, parameterOut, EdgeTypes.PARAMETER_LINK)
+        } else if (!loggedDeprecatedWarning) {
+          logger.warn("Using deprecated CPG format with PARAMETER_LINK edges")
+          loggedDeprecatedWarning = true
+        }
+    }
+
     Iterator(dstGraph.build())
   }
 }
