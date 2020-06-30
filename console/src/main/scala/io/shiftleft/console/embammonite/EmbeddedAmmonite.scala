535d673a84a457c05e8c78bf4809b5501b9bdbd4
Fabian Yamaguchi <fabs@shiftleft.io>
2020-06-30 22:27:04 +0200

    Hack for unit testing

--- console/src/main/scala/io/shiftleft/console/embammonite/EmbeddedAmmonite.scala
+++ console/src/main/scala/io/shiftleft/console/embammonite/EmbeddedAmmonite.scala
@@ -1,6 +1,6 @@
 package io.shiftleft.console.embammonite
 
-import java.io.{PipedInputStream, PipedOutputStream, PrintWriter}
+import java.io.{BufferedReader, InputStreamReader, PipedInputStream, PipedOutputStream, PrintWriter}
 import java.util.UUID
 import java.util.concurrent.{BlockingQueue, ConcurrentHashMap, LinkedBlockingQueue}
 
@@ -13,22 +13,6 @@ case class Failed(out: String, err: String) extends Result
 class Result()
 
 class EmbeddedAmmonite(predef: String = "") {
-
-  // The standard frontend attempts to query /dev/tty
-  // for terminal dimensions, which fails when running
-  // tests in intellij and in container environments
-  // (see https://github.com/lihaoyi/Ammonite/issues/276)
-  // Since terminal dimensions do not matter to us, we
-  // just set them manually.
-  val completePredef: String =
-    """
-      | CustomFrontend extends ammonite.repl.AmmoniteFrontEnd() {
-      | override def width=100
-      | override def height=100
-      | }
-      | repl.frontEnd() = new CustomFrontend()
-      |
-      |""".stripMargin + predef
 
   val jobQueue: BlockingQueue[Job] = new LinkedBlockingQueue[Job]()
   val results = new ConcurrentHashMap[UUID, Result]
@@ -44,8 +28,49 @@ class EmbeddedAmmonite(predef: String = "") {
   fromStderr.connect(errStream)
 
   val writer = new PrintWriter(toStdin)
+  val reader = new BufferedReader(new InputStreamReader(fromStdout))
 
   val shellThread = new Thread(() => {
+    // The standard frontend attempts to query /dev/tty
+    // for terminal dimensions, which fails when running
+    // tests in intellij and in container environments
+    // (see https://github.com/lihaoyi/Ammonite/issues/276)
+    // Since terminal dimensions do not matter to us, we
+    // just set them manually.
+    val completePredef: String =
+      """
+      | class CustomFrontend extends ammonite.repl.AmmoniteFrontEnd() {
+      |   override def width = 100
+      |   override def height = 100
+      |
+      |  override def readLine(reader: java.io.Reader,
+      |                        output: java.io.OutputStream,
+      |                        prompt: String,
+      |                        colors: ammonite.util.Colors,
+      |                        compilerComplete: (Int, String) => (Int, Seq[String], Seq[String]),
+      |                        history: IndexedSeq[String]) = {
+      |
+      |  val writer = new java.io.OutputStreamWriter(output)
+      |
+      |  // Enter
+      | val multilineFilter = ammonite.terminal.Filter.action(
+      | ammonite.terminal.SpecialKeys.NewLine,
+      | ti => ammonite.interp.Parsers.split(ti.ts.buffer.mkString).isEmpty
+      | ){
+      | case ammonite.terminal.TermState(rest, b, c, _) => ammonite.terminal.filters.BasicFilters.injectNewLine(b, c, rest)
+      |}
+      |
+      |  val allFilters = ammonite.terminal.filters.BasicFilters.all
+      |
+      |  val res = new ammonite.terminal.LineReader(width, prompt, reader, writer, allFilters)
+      |  .readChar(ammonite.terminal.TermState(ammonite.terminal.LazyList.continually(reader.read()), Vector.empty, 0, ""), 0)
+      |  res
+      | }
+      |}
+      | repl.frontEnd() = new CustomFrontend()
+      |
+      |""".stripMargin + predef
+
     val ammoniteShell =
       ammonite
         .Main(
@@ -54,11 +79,33 @@ class EmbeddedAmmonite(predef: String = "") {
           remoteLogging = false,
           colors = Colors.BlackWhite,
           inputStream = inStream,
-          outputStream = outStream,
-          errorStream = errStream
+          // outputStream = outStream,
+          // errorStream = errStream
         )
     ammoniteShell.run()
   })
+
+  class UserRunnable(queue: BlockingQueue[Job]) extends Runnable {
+    override def run(): Unit = {
+      try {
+        var terminate = false;
+        while (!terminate && !queue.isEmpty) {
+          val job = queue.take()
+          if (job.uuid == null && job.query == null) {
+            terminate = true
+          } else {
+            println(job.query.trim)
+            writer.println(job.query.trim)
+            writer.flush()
+          }
+        }
+      } catch {
+        case _: InterruptedException =>
+          println("Interrupted AmmoniteUserThread")
+      }
+      println("AmmoniteUserThread exited gracefully")
+    }
+  }
 
   val userRunnable = new UserRunnable(jobQueue)
   val userThread = new Thread(userRunnable)
@@ -88,26 +135,7 @@ class EmbeddedAmmonite(predef: String = "") {
   def shutdownShell(): Unit = {
     writer.println("exit")
     writer.close()
+    reader.close()
   }
 
-}
-
-class UserRunnable(queue: BlockingQueue[Job]) extends Runnable {
-  override def run(): Unit = {
-    try {
-      var terminate = false;
-      while (!terminate) {
-        val job = queue.take()
-        if (job.uuid == null && job.query == null) {
-          terminate = true
-        } else {
-          // carry out job
-        }
-      }
-    } catch {
-      case _: InterruptedException =>
-        println("Interrupted AmmoniteUserThread")
-    }
-    println("AmmoniteUserThread exited gracefully")
-  }
 }
--- console/src/test/scala/io/shiftleft/console/embammonite/EmbeddedAmmoniteTests.scala
+++ console/src/test/scala/io/shiftleft/console/embammonite/EmbeddedAmmoniteTests.scala
@@ -1,5 +1,7 @@
 package io.shiftleft.console.embammonite
 
+import java.util.UUID
+
 import org.scalatest.{Matchers, WordSpec}
 
 class EmbeddedAmmoniteTests extends WordSpec with Matchers {
@@ -8,6 +10,15 @@ class EmbeddedAmmoniteTests extends WordSpec with Matchers {
     "start and shutdown" in {
       val shell = new EmbeddedAmmonite()
       shell.start()
+      shell.shutdown()
+    }
+
+    "execute a command and return output" in {
+      val shell = new EmbeddedAmmonite()
+      shell.start()
+      val uuid = UUID.randomUUID()
+      shell.enqueue(uuid, "help")
+      shell.result(uuid)
       shell.shutdown()
     }
   }
