f6af1bf42b8240c38a5d69840bc29994229b85b1
Claudiu-Vlad Ursache <claudiu@shiftleft.io>
2020-07-16 14:01:43 +0200

    Make CPGQLSClient execute fn testable

--- cpgqls-client-python/cpgqls_client/client.py
+++ cpgqls-client-python/cpgqls_client/client.py
@@ -1,12 +1,26 @@
+import asyncio
 import requests
 import websockets
-import asyncio
 
-### TODO
-#### possible configuration
-# 0 retries
-# set the timeout for the HTTP thing
-# set the timeout for the websocket thing
+class CPGQLSTransport(object):
+
+    def __init__(self):
+        self._ws_conn = None
+
+    def connect(self, endpoint):
+        self._ws_conn = websockets.connect(endpoint)
+        return self._ws_conn
+
+    async def recv(self):
+        await self._ws_conn.recv()
+
+    def post(self, uri, **kwargs):
+        return requests.post(uri, **kwargs)
+
+    def get(self, uri):
+        return requests.get(uri)
+
+
 
 class CPGQLSClient(object):
     HTTP_ENDPOINT_PREFIX = "http://"
@@ -14,11 +28,15 @@ class CPGQLSClient(object):
 
     WS_MSG_CONNECTED = "connected"
 
-    def __init__(self, server_endpoint):
+    def __init__(self, server_endpoint, event_loop = None, transport = None):
         if server_endpoint is None:
             raise ValueError("The server endpoint cannot be None")
         if not isinstance(server_endpoint, str):
             raise ValueError("The server_endpoint parameter has to be a string")
+
+        self._loop = asyncio.get_event_loop() if event_loop is None else event_loop
+        self._transport = CPGQLSTransport() if transport is None else transport
+
         self._server_endpoint = server_endpoint
 
         self._http_endpoint = self.HTTP_ENDPOINT_PREFIX + server_endpoint
@@ -27,18 +45,18 @@ class CPGQLSClient(object):
         self._http_get_result_endpoint = self.HTTP_ENDPOINT_PREFIX + server_endpoint + "/result"
 
     def execute(self, query):
-        return asyncio.get_event_loop().run_until_complete(self._send_query(query))
+        return self._loop.run_until_complete(self._send_query(query))
 
     async def _send_query(self, query):
-        async with websockets.connect(self._ws_connect_endpoint) as ws_conn:
+        async with self._transport.connect(self._ws_connect_endpoint) as ws_conn:
             connected_msg = await ws_conn.recv()
             if connected_msg != self.WS_MSG_CONNECTED:
                 raise Exception("Received unexpected first message on websocket endpoint")
-            post_query_res = requests.post(self._http_post_query_endpoint, json = {"query": query})
+            post_query_res = self._transport.post(self._http_post_query_endpoint, json = {"query": query})
             if post_query_res.status_code != 200:
                 raise Exception("Could not post query to the HTTP endpoint of the cpgserver")
             last_msg = await ws_conn.recv()
-            get_query_res = requests.get(self._http_get_result_endpoint + "/" + post_query_res.json()["uuid"])
+            get_query_res = self._transport.get(self._http_get_result_endpoint + "/" + post_query_res.json()["uuid"])
             if get_query_res.status_code != 200:
                 raise Exception("Could not retrieve query result via the HTTP endpoint of the cpgserver")
             return get_query_res.json()
--- cpgqls-client-python/tests/test_client.py
+++ cpgqls-client-python/tests/test_client.py
@@ -1,14 +1,74 @@
 from cpgqls_client import CPGQLSClient
 
+import asyncio
 import pytest
+from unittest.mock import Mock
 
-def test_initializer():
-    with pytest.raises(ValueError):
-        CPGQLSClient(None)
+class MockCPGQLTransportConnection(object):
+    def __init__(self, first_recv_msg, second_recv_msg):
+        self._num_recv_msgs = 0
+        self._first_recv_msg = first_recv_msg
+        self._second_recv_msg = second_recv_msg
 
-    with pytest.raises(ValueError):
-        CPGQLSClient(1)
+    def __await__(self):
+        return self
+        yield None # pylint: disable=unreachable
 
-    out = CPGQLSClient("localhost:8000")
-    assert out is not None
+    async def recv(self):
+        await asyncio.sleep(0)
+
+        lock = asyncio.Lock()
+        async with lock:
+            msg = None
+            if self._num_recv_msgs == 0:
+                msg = self._first_recv_msg
+            elif self._num_recv_msgs == 1:
+                msg = self._second_recv_msg
+            self._num_recv_msgs += 1
+        return msg
+
+    async def __aenter__(self):
+        return await self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        pass
+
+
+class MockCPGQLSTransport(object):
+    def __init__(self, conn, get_response, post_response):
+        self._conn = conn
+        self._get_response = get_response
+        self._post_response = post_response
+
+    def connect(self, *args, **kwargs):
+        return self._conn
+
+    def post(self, *args, **kwargs):
+        return self._post_response
+
+    def get(self, *args, **kwargs):
+        return self._get_response
+
+
+def test_basic_execution():
+    event_loop = asyncio.new_event_loop()
+    conn = MockCPGQLTransportConnection("connected", "received")
+    get_response_mock = Mock(status_code=200, **{'json.return_value':  {'uuid': 'one'}})
+    post_response_mock = Mock(status_code=200, **{'json.return_value':  {'uuid': 'one'}})
+    mt = MockCPGQLSTransport(conn, get_response_mock, post_response_mock)
+    client = CPGQLSClient("localhost:8080", event_loop = event_loop, transport = mt)
+    res = client.execute("val a = 1")
+    assert res == post_response_mock.json()
+
+
+def test_get_response_not_200():
+    event_loop = asyncio.new_event_loop()
+    conn = MockCPGQLTransportConnection("connected", "received")
+    get_response_mock = Mock(status_code=400, **{'json.return_value':  {'uuid': 'one'}})
+    post_response_mock = Mock(status_code=200, **{'json.return_value':  {'uuid': 'one'}})
+    mt = MockCPGQLSTransport(conn, get_response_mock, post_response_mock)
+    client = CPGQLSClient("localhost:8080", event_loop = event_loop, transport = mt)
+    with pytest.raises(Exception):
+        res = client.execute("val a = 1")
+
 
