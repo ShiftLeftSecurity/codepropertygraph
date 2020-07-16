2336e6633d0b4b0dcce6d14c916cc928b48021eb
Claudiu-Vlad Ursache <claudiu@shiftleft.io>
2020-07-16 15:04:55 +0200

    Fix flake8 style issues

--- cpgqls-client-python/cpgqls_client/client.py
+++ cpgqls-client-python/cpgqls_client/client.py
@@ -2,6 +2,7 @@ import asyncio
 import requests
 import websockets
 
+
 class CPGQLSTransport(object):
 
     def __init__(self):
@@ -21,43 +22,50 @@ class CPGQLSTransport(object):
         return requests.get(uri)
 
 
-
 class CPGQLSClient(object):
-    HTTP_ENDPOINT_PREFIX = "http://"
-    WS_ENDPOINT_PREFIX = "ws://"
-
     CPGQLS_MSG_CONNECTED = "connected"
 
-    def __init__(self, server_endpoint, event_loop = None, transport = None):
+    def __init__(self, server_endpoint, event_loop=None, transport=None):
         if server_endpoint is None:
-            raise ValueError("The server endpoint cannot be None")
+            raise ValueError("server_endpoint cannot be None")
         if not isinstance(server_endpoint, str):
-            raise ValueError("The server_endpoint parameter has to be a string")
+            raise ValueError("server_endpoint parameter has to be a string")
 
-        self._loop = asyncio.get_event_loop() if event_loop is None else event_loop
-        self._transport = CPGQLSTransport() if transport is None else transport
-
-        self._server_endpoint = server_endpoint
-
-        self._http_endpoint = self.HTTP_ENDPOINT_PREFIX + server_endpoint
-        self._ws_connect_endpoint = self.WS_ENDPOINT_PREFIX + server_endpoint + "/connect"
-        self._http_post_query_endpoint = self.HTTP_ENDPOINT_PREFIX + server_endpoint + "/query"
-        self._http_get_result_endpoint = self.HTTP_ENDPOINT_PREFIX + server_endpoint + "/result"
+        self._loop = asyncio.get_event_loop() if not event_loop else event_loop
+        self._transport = CPGQLSTransport() if not transport else transport
+        self._endpoint = server_endpoint.rstrip("/")
 
     def execute(self, query):
         return self._loop.run_until_complete(self._send_query(query))
 
     async def _send_query(self, query):
-        async with self._transport.connect(self._ws_connect_endpoint) as ws_conn:
+        endpoint = self._connect_endpoint()
+        async with self._transport.connect(endpoint) as ws_conn:
             connected_msg = await ws_conn.recv()
             if connected_msg != self.CPGQLS_MSG_CONNECTED:
-                raise Exception("Received unexpected first message on websocket endpoint")
-            post_query_res = self._transport.post(self._http_post_query_endpoint, json = {"query": query})
-            if post_query_res.status_code != 200:
-                raise Exception("Could not post query to the HTTP endpoint of the cpgserver")
+                exception_msg = """Received unexpected first message
+                on websocket endpoint"""
+                raise Exception(exception_msg)
+            endpoint = self._post_query_endpoint()
+            post_res = self._transport.post(endpoint, json={"query": query})
+            if post_res.status_code != 200:
+                exception_msg = """Coould not post query to the HTTP
+                endpoint of the server"""
+                raise Exception(exception_msg)
             last_msg = await ws_conn.recv()
-            get_query_res = self._transport.get(self._http_get_result_endpoint + "/" + post_query_res.json()["uuid"])
-            if get_query_res.status_code != 200:
-                raise Exception("Could not retrieve query result via the HTTP endpoint of the cpgserver")
-            return get_query_res.json()
+            endpoint = self._get_result_endpoint(post_res.json()["uuid"])
+            get_res = self._transport.get(endpoint)
+            if get_res.status_code != 200:
+                exception_msg = """Could not retrieve query result via the HTTP endpoint
+                of the server"""
+                raise Exception(exception_msg)
+            return get_res.json()
 
+    def _connect_endpoint(self):
+        return "ws://" + self._endpoint + "/connect"
+
+    def _post_query_endpoint(self):
+        return "http://" + self._endpoint + "/query"
+
+    def _get_result_endpoint(self, uuid):
+        return "http://" + self._endpoint + "/result/" + uuid
--- cpgqls-client-python/cpgqls_client/queries.py
+++ cpgqls-client-python/cpgqls_client/queries.py
@@ -1,16 +1,17 @@
 
-def import_code_query(path, project_name = None, language = None):
+def import_code_query(path, project_name=None, language=None):
     if not path:
         raise Exception('An importCode query requires a project path')
     if project_name and language:
-        return "importCode(inputPath=\"%s\", projectName=\"%s\", language=\"%s\")" % (path, project_name, language)
+        fmt_str = u"""importCode(inputPath=\"%s\", projectName=\"%s\",
+language=\"%s\")"""
+        return fmt_str % (path, project_name, language)
     if project_name and (language is None):
-        return "importCode(inputPath=\"%s\", projectName=\"%s\")" % (path, project_name)
+        fmt_str = u"""importCode(inputPath=\"%s\", projectName=\"%s\")"""
+        return fmt_str % (path, project_name)
     if project_name is None and language is None:
         return u"importCode(\"%s\")" % (path)
 
 
 def workspace_query():
     return "workspace"
-
-
