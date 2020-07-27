e467b3c13b6fefe6e1635f12507b8045767b6116
Michael Pollmeier <michael@michaelpollmeier.com>
2020-07-27 17:10:02 +1200

    operatorextensions

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/ArrayAccess.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/ArrayAccess.scala
@@ -1,11 +1,10 @@
 package io.shiftleft.semanticcpg.language.operatorextension
 
-import gremlin.scala.GremlinScala
 import io.shiftleft.codepropertygraph.generated.nodes
 import io.shiftleft.semanticcpg.language._
+import overflowdb.traversal.Traversal
 
-class ArrayAccess(val wrapped: Steps[opnodes.ArrayAccess]) extends AnyVal {
-  private def raw: GremlinScala[opnodes.ArrayAccess] = wrapped.raw
-  def array: NodeSteps[nodes.Expression] = wrapped.map(_.array)
-  def subscripts: NodeSteps[nodes.Identifier] = wrapped.flatMap(_.subscripts)
+class ArrayAccess(val traversal: Traversal[opnodes.ArrayAccess]) extends AnyVal {
+  def array: Traversal[nodes.Expression] = traversal.map(_.array)
+  def subscripts: Traversal[nodes.Identifier] = traversal.flatMap(_.subscripts)
 }
--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/nodemethods/OpAstNodeMethods.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/nodemethods/OpAstNodeMethods.scala
@@ -1,19 +1,20 @@
 package io.shiftleft.semanticcpg.language.operatorextension.nodemethods
 
 import io.shiftleft.codepropertygraph.generated.nodes
-import io.shiftleft.semanticcpg.language.operatorextension._
 import io.shiftleft.semanticcpg.language._
+import io.shiftleft.semanticcpg.language.operatorextension._
+import overflowdb.traversal.Traversal
 
 class OpAstNodeMethods[A <: nodes.AstNode](val node: A) extends AnyVal {
-  def inAssignment: NodeSteps[opnodes.Assignment] =
+  def inAssignment: Traversal[opnodes.Assignment] =
     node.start.inAstMinusLeaf.isCall.name(NodeTypeStarters.assignmentPattern).map(new opnodes.Assignment(_))
 
-  def assignments: NodeSteps[opnodes.Assignment] =
+  def assignments: Traversal[opnodes.Assignment] =
     rawTravForPattern(NodeTypeStarters.assignmentPattern).map(new opnodes.Assignment(_))
 
-  def arithmetics: NodeSteps[opnodes.Arithmetic] =
+  def arithmetics: Traversal[opnodes.Arithmetic] =
     rawTravForPattern(NodeTypeStarters.arithmeticPattern).map(new opnodes.Arithmetic(_))
 
-  private def rawTravForPattern(pattern: String): NodeSteps[nodes.Call] =
+  private def rawTravForPattern(pattern: String): Traversal[nodes.Call] =
     node.ast.isCall.name(pattern)
 }
