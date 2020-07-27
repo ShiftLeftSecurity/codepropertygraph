c11b85762d99f590a3de8a553936b958bd290a52
Michael Pollmeier <michael@michaelpollmeier.com>
2020-07-27 16:56:04 +1200

    dotextension

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/dotextension/AstNodeDot.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/dotextension/AstNodeDot.scala
@@ -3,15 +3,16 @@ package io.shiftleft.semanticcpg.language.dotextension
 import io.shiftleft.codepropertygraph.generated.nodes
 import io.shiftleft.semanticcpg.dotgenerator.DotAstGenerator
 import io.shiftleft.semanticcpg.language._
+import overflowdb.traversal.Traversal
 
 import scala.language.postfixOps
 
-class AstNodeDot[NodeType <: nodes.AstNode](val wrapped: NodeSteps[NodeType]) extends AnyVal {
+class AstNodeDot[NodeType <: nodes.AstNode](val traversal: Traversal[NodeType]) extends AnyVal {
 
-  def dotAst: Steps[String] = DotAstGenerator.toDotAst(wrapped)
+  def dotAst: Steps[String] = DotAstGenerator.toDotAst(traversal)
 
   def plotDotAst(implicit viewer: ImageViewer): Unit = {
-    Shared.plotAndDisplay(wrapped.dotAst.l, viewer)
+    Shared.plotAndDisplay(traversal.dotAst.l, viewer)
   }
 
 }
--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/dotextension/CfgNodeDot.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/dotextension/CfgNodeDot.scala
@@ -2,14 +2,14 @@ package io.shiftleft.semanticcpg.language.dotextension
 
 import io.shiftleft.codepropertygraph.generated.nodes
 import io.shiftleft.semanticcpg.dotgenerator.DotCfgGenerator
-import io.shiftleft.semanticcpg.language.{NodeSteps, Steps}
+import io.shiftleft.semanticcpg.language.Steps
+import overflowdb.traversal.Traversal
 
-class CfgNodeDot(val wrapped: NodeSteps[nodes.Method]) extends AnyVal {
-
-  def dotCfg: Steps[String] = DotCfgGenerator.toDotCfg(wrapped)
+class CfgNodeDot(val traversal: Traversal[nodes.Method]) extends AnyVal {
+  def dotCfg: Steps[String] = DotCfgGenerator.toDotCfg(traversal)
 
   def plotDotCfg(implicit viewer: ImageViewer): Unit = {
-    Shared.plotAndDisplay(wrapped.dotCfg.l, viewer)
+    Shared.plotAndDisplay(traversal.dotCfg.l, viewer)
   }
 
 }
