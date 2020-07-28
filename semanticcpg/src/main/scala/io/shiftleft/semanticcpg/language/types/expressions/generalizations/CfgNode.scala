99fd8380f373bc6f1f2fa2769e12fe16d0f0ad18
Michael Pollmeier <michael@michaelpollmeier.com>
2020-07-28 12:12:05 +1200

    types

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/types/expressions/generalizations/CfgNode.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/types/expressions/generalizations/CfgNode.scala
@@ -1,111 +1,101 @@
 package io.shiftleft.semanticcpg.language.types.expressions.generalizations
 
-import gremlin.scala.GremlinScala
 import io.shiftleft.codepropertygraph.generated.{EdgeTypes, NodeTypes, nodes}
-import overflowdb.traversal.help
-import overflowdb.traversal.help.Doc
 import io.shiftleft.semanticcpg.language._
 import io.shiftleft.semanticcpg.utils.ExpandTo
+import overflowdb.traversal.help.Doc
+import overflowdb.traversal.{Traversal, help}
 
 @help.Traversal(elementType = classOf[nodes.CfgNode])
-class CfgNode[A <: nodes.CfgNode](val wrapped: NodeSteps[A]) extends AnyVal {
-  private def raw: GremlinScala[A] = wrapped.raw
+class CfgNode[A <: nodes.CfgNode](val traversal: Traversal[A]) extends AnyVal {
 
   /**
     * Textual representation of CFG node
     * */
   @Doc("Textual representation of CFG node")
-  def repr: Steps[String] = {
-    wrapped.map(_.repr)
-  }
+  def repr: Traversal[String] =
+    traversal.map(_.repr)
 
   /**
   Traverse to enclosing method
     */
-  def method: NodeSteps[nodes.Method] =
-    new NodeSteps(
-      raw
-        .map {
-          case method: nodes.Method =>
-            method
-          case methodReturn: nodes.MethodReturn =>
-            ExpandTo.methodReturnToMethod(methodReturn)
-          case expression: nodes.Expression =>
-            ExpandTo.expressionToMethod(expression)
-        }
-        .cast[nodes.Method])
+  def method: Traversal[nodes.Method] =
+    traversal.map {
+      case method: nodes.Method =>
+        method
+      case methodReturn: nodes.MethodReturn =>
+        ExpandTo.methodReturnToMethod(methodReturn)
+      case expression: nodes.Expression =>
+        ExpandTo.expressionToMethod(expression)
+    }
 
   /**
     * Traverse to next expression in CFG.
     */
 
   @Doc("Nodes directly reachable via outgoing CFG edges")
-  def cfgNext: NodeSteps[nodes.CfgNode] =
-    new NodeSteps(
-      raw
-        .out(EdgeTypes.CFG)
-        .filterNot(_.hasLabel(NodeTypes.METHOD_RETURN))
-        .cast[nodes.CfgNode]
-    )
+  def cfgNext: Traversal[nodes.CfgNode] =
+    traversal
+      .out(EdgeTypes.CFG)
+      .not(_.hasLabel(NodeTypes.METHOD_RETURN))
+      .cast[nodes.CfgNode]
 
   /**
     * Traverse to previous expression in CFG.
     */
   @Doc("Nodes directly reachable via incoming CFG edges")
-  def cfgPrev: NodeSteps[nodes.CfgNode] =
-    new NodeSteps(
-      raw
-        .in(EdgeTypes.CFG)
-        .filterNot(_.hasLabel(NodeTypes.METHOD))
-        .cast[nodes.CfgNode]
-    )
+  def cfgPrev: Traversal[nodes.CfgNode] =
+    traversal
+      .in(EdgeTypes.CFG)
+      .not(_.hasLabel(NodeTypes.METHOD))
+      .cast[nodes.CfgNode]
 
   /**
     * Recursively determine all nodes on which any of
     * the nodes in this traversal are control dependent
     * */
   @Doc("All nodes on which this node is control dependent")
-  def controlledBy: NodeSteps[nodes.CfgNode] =
-    wrapped.flatMap(_.controlledBy)
+  def controlledBy: Traversal[nodes.CfgNode] =
+    traversal.flatMap(_.controlledBy)
 
   /**
     * Recursively determine all nodes which are
     * control dependent on this node
     * */
   @Doc("All nodes control dependent on this node")
-  def controls: NodeSteps[nodes.CfgNode] =
-    wrapped.flatMap(_.controls)
+  def controls: Traversal[nodes.CfgNode] =
+    traversal.flatMap(_.controls)
 
   /**
     * Recursively determine all nodes by which
     * this node is dominated
     * */
   @Doc("All nodes by which this node is dominated")
-  def dominatedBy: NodeSteps[nodes.CfgNode] =
-    wrapped.flatMap(_.dominatedBy)
+  def dominatedBy: Traversal[nodes.CfgNode] =
+    traversal.flatMap(_.dominatedBy)
 
   /**
     * Recursively determine all nodes which
     * this node dominates
     * */
   @Doc("All nodes that are dominated by this node")
-  def dominates: NodeSteps[nodes.CfgNode] =
-    wrapped.flatMap(_.dominates)
+  def dominates: Traversal[nodes.CfgNode] =
+    traversal.flatMap(_.dominates)
 
   /**
     * Recursively determine all nodes by which
     * this node is post dominated
     * */
   @Doc("All nodes by which this node is post dominated")
-  def postDominatedBy: NodeSteps[nodes.CfgNode] =
-    wrapped.flatMap(_.postDominatedBy)
+  def postDominatedBy: Traversal[nodes.CfgNode] =
+    traversal.flatMap(_.postDominatedBy)
 
   /**
     * Recursively determine all nodes which
     * this node post dominates
     * */
   @Doc("All nodes that are post dominated by this node")
-  def postDominates: NodeSteps[nodes.CfgNode] =
-    wrapped.flatMap(_.postDominates)
+  def postDominates: Traversal[nodes.CfgNode] =
+    traversal.flatMap(_.postDominates)
 
 }
