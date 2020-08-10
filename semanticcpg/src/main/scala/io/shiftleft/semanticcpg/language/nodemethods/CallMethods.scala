32f5d9894e16ebb32ed4ed0573b93c25159cd391
Bernhard Brehm <bernhard@shiftleft.io>
2020-08-10 13:47:44 +0200

    add more convenient accessor argumentOption for call->argument. Make old accessor faster (don't use gremlin)

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/CallMethods.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/CallMethods.scala
@@ -2,7 +2,12 @@ package io.shiftleft.semanticcpg.language.nodemethods
 
 import io.shiftleft.codepropertygraph.generated.nodes
 import io.shiftleft.semanticcpg.language._
+import scala.jdk.CollectionConverters._
 
 class CallMethods(val node: nodes.Call) extends AnyVal {
-  def argument(index: Int): nodes.Expression = node.start.argument(index).head
+  def argumentOption(index: Int): Option[nodes.Expression] =
+    node._argumentOut.asScala
+      .collectFirst { case expr: nodes.Expression if expr.argumentIndex == index => expr }
+
+  def argument(index: Int): nodes.Expression = argumentOption(index).get
 }
