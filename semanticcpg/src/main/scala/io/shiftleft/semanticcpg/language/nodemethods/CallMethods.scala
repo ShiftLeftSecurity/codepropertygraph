8910553aa68eeddcbc7cc4c1ef839562b9b3847b
bbrehm <bernhard@shiftleft.io>
2020-08-10 20:07:48 +0200

    Add more convenient accessor for arguments of calls (#891)
    * add more convenient accessor "arguments(intex: Int)" for call->argument. Make old accessor faster (don't use gremlin)

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/CallMethods.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/CallMethods.scala
@@ -2,7 +2,13 @@ package io.shiftleft.semanticcpg.language.nodemethods
 
 import io.shiftleft.codepropertygraph.generated.nodes
 import io.shiftleft.semanticcpg.language._
+import scala.jdk.CollectionConverters._
 
 class CallMethods(val node: nodes.Call) extends AnyVal {
-  def argument(index: Int): nodes.Expression = node.start.argument(index).head
+  def arguments(index: Int): Iterable[nodes.Expression] =
+    node._argumentOut.asScala.collect {
+      case expr: nodes.Expression if expr.argumentIndex == index => expr
+    }.toList
+
+  def argument(index: Int): nodes.Expression = arguments(index).head
 }
