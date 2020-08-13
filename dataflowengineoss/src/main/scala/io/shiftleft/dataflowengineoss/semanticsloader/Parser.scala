164c824ebfb4060c41a4a4b7a5011184a046142b
Fabian Yamaguchi <fabs@shiftleft.io>
2020-08-14 01:30:01 +0200

    Improve language for semantics (#896)
    * Include ANTLRv4 as dependency of dataflowengineoss
    
    * Add Grammar for Semantics
    
    * Add Semantics Parser + tests
    
    * Fix method names in default.semantics
    
    * Hook up new semantics parser

---
+++ dataflowengineoss/src/main/scala/io/shiftleft/dataflowengineoss/semanticsloader/Parser.scala
@@ -0,0 +1,56 @@
+package io.shiftleft.dataflowengineoss.semanticsloader
+
+import java.io.InputStream
+
+import io.shiftleft.dataflowengineoss.{SemanticsBaseListener, SemanticsLexer, SemanticsParser}
+import org.antlr.v4.runtime.{CharStream, CharStreams, CommonTokenStream}
+import org.antlr.v4.runtime.tree.ParseTreeWalker
+
+import scala.collection.mutable
+import scala.jdk.CollectionConverters._
+
+case class FlowSemantic(methodFullName: String, mappings: List[(Int, Int)])
+
+class Parser() {
+
+  def parse(input: String): List[FlowSemantic] = {
+    val charStream = CharStreams.fromString(input)
+    parseCharStream(charStream)
+  }
+
+  def parseFile(fileName: String): List[FlowSemantic] = {
+    val charStream = CharStreams.fromFileName(fileName)
+    parseCharStream(charStream)
+  }
+
+  private def parseCharStream(charStream: CharStream): List[FlowSemantic] = {
+    val lexer = new SemanticsLexer(charStream)
+    val tokenStream = new CommonTokenStream(lexer)
+    val parser = new SemanticsParser(tokenStream)
+    val treeWalker = new ParseTreeWalker();
+
+    val tree = parser.taintSemantics()
+    val listener = new Listener()
+    treeWalker.walk(listener, tree)
+    listener.result.toList
+  }
+
+  private class Listener extends SemanticsBaseListener {
+
+    val result: mutable.ListBuffer[FlowSemantic] = mutable.ListBuffer[FlowSemantic]()
+
+    override def enterTaintSemantics(ctx: SemanticsParser.TaintSemanticsContext): Unit = {
+      ctx.singleSemantic().asScala.foreach { semantic =>
+        val methodName = semantic.methodName().name().getText
+        val mappings = semantic.mapping().asScala.toList.map { mapping =>
+          val src = mapping.src().NUMBER().getText.toInt
+          val dst = mapping.dst().NUMBER().getText.toInt
+          (src, dst)
+        }
+        result.addOne(FlowSemantic(methodName, mappings))
+      }
+    }
+
+  }
+
+}
--- dataflowengineoss/src/main/scala/io/shiftleft/dataflowengineoss/semanticsloader/SemanticsLoader.scala
+++ dataflowengineoss/src/main/scala/io/shiftleft/dataflowengineoss/semanticsloader/SemanticsLoader.scala
@@ -1,15 +1,8 @@
 package io.shiftleft.dataflowengineoss.semanticsloader
 
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-
-import scala.io.Source
-
-case class Semantic(methodFullName: String, parameterIndex: Int)
-case class Semantics(elements: List[Semantic])
+case class Semantics(elements: List[FlowSemantic])
 
 object SemanticsLoader {
-  private val logger: Logger = LoggerFactory.getLogger(classOf[SemanticsLoader])
 
   def emptySemantics: Semantics = {
     Semantics(Nil)
@@ -17,48 +10,10 @@ object SemanticsLoader {
 }
 
 class SemanticsLoader(filename: String) {
-  import SemanticsLoader.logger
 
   def load(): Semantics = {
-    val bufferedReader = Source.fromFile(filename)
-    var lineNumber = 0
-
-    try {
-      val semanticElements =
-        bufferedReader
-          .getLines()
-          .flatMap { line =>
-            val parts = line.split(",")
-
-            if (parts.size == 2) {
-              try {
-                val methodFullName = parts(0).trim
-                val parameterIndex = parts(1).trim.toInt
-                lineNumber += 1
-                Some(Semantic(methodFullName, parameterIndex))
-              } catch {
-                case _: NumberFormatException =>
-                  logFormatError("Argument index is not convertable to Int.", lineNumber)
-                  None
-              }
-
-            } else {
-              logFormatError("Invalid number of elements per line. Expected method name followed by argument index.",
-                             lineNumber)
-              None
-            }
-          }
-          .toList
-
-      Semantics(semanticElements)
-    } finally {
-      bufferedReader.close()
-    }
-
-  }
-
-  private def logFormatError(msg: String, lineNumber: Int): Unit = {
-    logger.warn(s"$msg In $filename on line $lineNumber")
+    val elements = new Parser().parseFile(filename)
+    Semantics(elements)
   }
 
 }
