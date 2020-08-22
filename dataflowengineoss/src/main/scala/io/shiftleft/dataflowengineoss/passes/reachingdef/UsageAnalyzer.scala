756f3f81b4f371384d0381099389a52f142f41ce
Fabian Yamaguchi <fabs@shiftleft.io>
2020-08-22 22:27:11 +0200

    Fix potential non-determinism in new `UsageAnalyzer`

--- dataflowengineoss/src/main/scala/io/shiftleft/dataflowengineoss/passes/reachingdef/UsageAnalyzer.scala
+++ dataflowengineoss/src/main/scala/io/shiftleft/dataflowengineoss/passes/reachingdef/UsageAnalyzer.scala
@@ -16,7 +16,7 @@ class UsageAnalyzer(in: Map[nodes.StoredNode, Set[Definition]]) {
       node ->
         uses(node).map { use =>
           use -> in(node).filter { inElement =>
-            declaration(use) == declaration(inElement.node)
+            sameDeclaration(use, inElement.node)
           }
         }.toMap
     }.toMap
@@ -33,15 +33,20 @@ class UsageAnalyzer(in: Map[nodes.StoredNode, Set[Definition]]) {
     n.filterNot(_.isInstanceOf[nodes.FieldIdentifier]).map(_.asInstanceOf[nodes.StoredNode])
   }
 
+  def sameDeclaration(node1: StoredNode, node2: StoredNode): Boolean = {
+    if (node1.isInstanceOf[nodes.Call] || node2.isInstanceOf[nodes.Call]) {
+      node1.isInstanceOf[nodes.Call] && node2.isInstanceOf[nodes.Call] &&
+      node1.asInstanceOf[nodes.Call].code == node2.asInstanceOf[nodes.Call].code
+    } else {
+      (declaration(node1) == declaration(node2)) && declaration(node1).isDefined
+    }
+  }
+
   private def declaration(node: nodes.StoredNode): Option[nodes.StoredNode] = {
     node match {
       case param: nodes.MethodParameterIn => Some(param)
       case _: nodes.Identifier            => node._refOut().nextOption
-      case call: nodes.Call               =>
-        // We map to the first call that has the exact same code. We use
-        // this as a declaration
-        call.method.start.call.codeExact(call.code).headOption
-      case _ => None
+      case _                              => None
     }
   }
 
