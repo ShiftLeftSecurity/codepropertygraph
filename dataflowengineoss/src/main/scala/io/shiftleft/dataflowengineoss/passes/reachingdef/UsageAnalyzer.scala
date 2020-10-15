1626782252a07ec6d5c8219c4317cf5e24717a78
Fabian Yamaguchi <fabs@shiftleft.io>
2020-10-15 20:41:52 +0200

    Another fix for access path handling (#1003)

--- dataflowengineoss/src/main/scala/io/shiftleft/dataflowengineoss/passes/reachingdef/UsageAnalyzer.scala
+++ dataflowengineoss/src/main/scala/io/shiftleft/dataflowengineoss/passes/reachingdef/UsageAnalyzer.scala
@@ -30,9 +30,10 @@ class UsageAnalyzer(in: Map[nodes.StoredNode, Set[Definition]]) {
         val (useBase, useAccessPath) = u.trackedBaseAndAccessPath
         val (inBase, defAccessPath) = i.trackedBaseAndAccessPath
         val (matchResult, elements) = useAccessPath.matchAndDiff(defAccessPath.elements)
-        (useBase == inBase) && // filter *x -> x
+        (useBase == inBase) &&
+        matchResult != MatchResult.NO_MATCH && // filter *x -> x
         !(matchResult == MatchResult.EXTENDED_MATCH && elements.elements.length > 1 && elements.elements.headOption
-          .exists(_.toString == "*")) && matchResult != MatchResult.NO_MATCH
+          .exists(_.toString == "*"))
       case _ => false
     }
   }
