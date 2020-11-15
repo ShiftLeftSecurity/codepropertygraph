c4e9f395733ba5aef91e7ad1a3d4976707739fde
Michael Pollmeier <michael@michaelpollmeier.com>
2020-11-16 12:25:16 +1300

    call.argument with no args
    prior to this:
    ```
    semanticcpg/console
    
    import io.shiftleft.codepropertygraph.Cpg
    import io.shiftleft.semanticcpg.language._
    
    def cpg: Cpg = ???
    def a = cpg.call.argument
    
    def b = cpg.call.head.argument
    cmd1.sc:1: missing argument list for method argument in class CallMethods
    Unapplied methods are only converted to functions when a function type is expected.
    ```

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/CallMethods.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/CallMethods.scala
@@ -1,24 +1,24 @@
 package io.shiftleft.semanticcpg.language.nodemethods
 
 import io.shiftleft.codepropertygraph.generated.nodes
-import io.shiftleft.semanticcpg.language._
-import scala.jdk.CollectionConverters._
 import overflowdb.traversal._
 
 class CallMethods(val node: nodes.Call) extends AnyVal {
   def arguments(index: Int): Traversal[nodes.Expression] =
-    node._argumentOut.asScala
+    node._argumentOut
       .collect {
         case expr: nodes.Expression if expr.argumentIndex == index => expr
       }
       .to(Traversal)
 
+  def argument: Traversal[nodes.Expression] =
+    node._argumentOut.collectAll[nodes.Expression]
+
   def argument(index: Int): nodes.Expression =
     arguments(index).head
 
   def argumentOption(index: Int): Option[nodes.Expression] =
-    node._argumentOut.asScala
-      .collectFirst {
-        case expr: nodes.Expression if expr.argumentIndex == index => expr
-      }
+    node._argumentOut.collectFirst {
+      case expr: nodes.Expression if expr.argumentIndex == index => expr
+    }
 }
