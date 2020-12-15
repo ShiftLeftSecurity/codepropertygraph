21d4effa2a1635ca9a13156423543a56f5621a62
Fabian Yamaguchi <fabs@shiftleft.io>
2020-12-15 13:41:47 +0100

    Support for running bundles from the shell (#1049)
    * Implement `--bundles` to list available bundles
    
    * Finish implementation of `--run` without parameter passing

--- console/src/main/scala/io/shiftleft/console/BridgeBase.scala
+++ console/src/main/scala/io/shiftleft/console/BridgeBase.scala
@@ -9,6 +9,10 @@ import io.shiftleft.console.cpgqlserver.CPGQLServer
 
 case class Config(
     scriptFile: Option[Path] = None,
+    bundleToRun: Option[String] = None,
+    listBundles: Boolean = false,
+    src: Option[String] = None,
+    overwrite: Boolean = false,
     params: Map[String, String] = Map.empty,
     additionalImports: List[Path] = Nil,
     nocolors: Boolean = false,
@@ -38,6 +42,22 @@ trait BridgeBase {
       opt[Path]("script")
         .action((x, c) => c.copy(scriptFile = Some(x)))
         .text("path to script file: will execute and exit")
+
+      opt[String]("run")
+        .action((x, c) => c.copy(bundleToRun = Some(x)))
+        .text("Run bundle. Get a list via --bundles")
+
+      opt[Unit]("bundles")
+        .action((_, c) => c.copy(listBundles = true))
+        .text("List available bundles")
+
+      opt[String]("src")
+        .action((x, c) => c.copy(src = Some(x)))
+        .text("Source code directory to run bundle on")
+
+      opt[Unit]("overwrite")
+        .action((_, c) => c.copy(overwrite = true))
+        .text("Overwrite CPG if it already exists")
 
       opt[Map[String, String]]('p', "params")
         .valueName("k1=v1,k2=v2")
@@ -85,15 +105,67 @@ trait BridgeBase {
   }
 
   protected def runAmmonite(config: Config, slProduct: SLProduct = OcularProduct): Unit = {
+
+    if (config.listBundles) {
+      listBundles(config)
+      return
+    }
+
     config.scriptFile match {
       case None =>
         if (config.server) {
           startHttpServer(config)
+        } else if (config.bundleToRun.isDefined) {
+          runBundle(config)
         } else {
           startInteractiveShell(config, slProduct)
         }
       case Some(scriptFile) =>
         runScript(scriptFile, config)
+    }
+  }
+
+  private def listBundles(config: Config): Unit = {
+    val code =
+      """
+        |println(run)
+        |
+        |""".stripMargin
+    withTemporaryScript(code) { file =>
+      runScript(os.Path(file.path.toString), config)
+    }
+  }
+
+  private def withTemporaryScript(code: String)(f: File => Unit): Unit = {
+    File.usingTemporaryDirectory("joern-bundle") { dir =>
+      val file = (dir / "script.sc")
+      file.write(code)
+      f(file)
+    }
+  }
+
+  private def runBundle(config: Config): Unit = {
+    if (config.src.isEmpty) {
+      println("You must supply a source directory with the --src flag")
+      return
+    }
+
+    val bundleName = config.bundleToRun.get
+    val src = config.src.get
+    val code = s"""
+        | if (${config.overwrite} || !workspace.projectExists("../targets/vlc-3.0.8/lib")) {
+        |   importCode("$src")
+        | } else {
+        |    println("Using existing CPG - Use `--overwrite` if this is not what you want")
+        |    workspace.projects
+        |    .filter(x => x.inputPath == better.files.File("$src").path.toAbsolutePath.toString)
+        |    .map(_.name).map(open)
+        | }
+        | run.$bundleName
+        |""".stripMargin
+
+    withTemporaryScript(code) { file =>
+      runScript(os.Path(file.path.toString), config)
     }
   }
 
