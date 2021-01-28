69a173c657645e3ad3090f745a8c9067f5254be8
Michael Pollmeier <michael@michaelpollmeier.com>
2021-01-28 21:44:32 +1300

    make queryInit explicitly return a `Query` to avoid casts
    note: in order to reference `Query` from the macro i needed to pull it
    out to another common dependency - you probably want to move it
    somewhere else

---
+++ codepropertygraph/src/main/scala/io/shiftleft/console/Query.scala
@@ -0,0 +1,20 @@
+package io.shiftleft.console
+
+import io.shiftleft.codepropertygraph.generated.nodes
+import io.shiftleft.codepropertygraph.Cpg
+import overflowdb.traversal.Traversal
+
+case class Query(name: String,
+                 author: String,
+                 title: String,
+                 description: String,
+                 score: Double,
+                 // intended to be filled by com.lihaoyi.sourcecode.Line
+                 docStartLine: Int = 0,
+                 traversal: Cpg => Traversal[nodes.StoredNode],
+                 // intended to be filled by com.lihaoyi.sourcecode.Line
+                 docEndLine: Int = 0,
+                 // intended to be filled by com.lihaoyi.sourcecode.FileName
+                 docFileName: String = "",
+                 traversalAsString: String = "",
+                 tags: List[String] = List())
--- macros/src/main/scala/io/shiftleft/macros/Macros.scala
+++ macros/src/main/scala/io/shiftleft/macros/Macros.scala
@@ -2,6 +2,7 @@ package io.shiftleft.macros
 
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.codepropertygraph.generated.nodes
+import io.shiftleft.console.Query
 import overflowdb.traversal.Traversal
 import scala.language.experimental.macros
 import scala.reflect.macros.whitebox
@@ -13,21 +14,21 @@ object QueryMacros {
                 title: String,
                 description: String,
                 score: Double,
-                traversal: Cpg => Traversal[nodes.StoredNode]): Any = macro queryInitImpl
+                traversal: Cpg => Traversal[nodes.StoredNode]): Query = macro queryInitImpl
 
   def queryInitImpl(c: whitebox.Context)(name: c.Tree,
                                          author: c.Tree,
                                          title: c.Tree,
                                          description: c.Tree,
                                          score: c.Tree,
-                                         traversal: c.Tree): c.Expr[Any] = {
+                                         traversal: c.Tree): c.Expr[Query] = {
     import c.universe._
     val fileContent = new String(traversal.pos.source.content)
     val start = traversal.pos.start
     val end = traversal.pos.end
     val traversalAsString: String = fileContent.slice(start, end)
 
-    c.Expr[Any](
+    c.Expr(
       q"""
         Query(
           name = $name,
