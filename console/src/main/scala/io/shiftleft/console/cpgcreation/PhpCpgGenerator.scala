9ebd40792fa912addd08df93d6a8d2289b4d3651
Fabian Yamaguchi <fabs@shiftleft.io>
2021-02-23 19:28:17 +0100

    scan: guess language if it is not provided.

--- console/src/main/scala/io/shiftleft/console/cpgcreation/PhpCpgGenerator.scala
+++ console/src/main/scala/io/shiftleft/console/cpgcreation/PhpCpgGenerator.scala
@@ -7,7 +7,7 @@ import io.shiftleft.console.PhpFrontendConfig
 case class PhpCpgGenerator(config: PhpFrontendConfig, rootPath: Path) extends CpgGenerator {
 
   override def generate(inputPath: String, outputPath: String, namespaces: List[String]): Option[String] = {
-    val command = rootPath.resolve("php2cp").toString
+    val command = rootPath.resolve("php2cpg").toString
     val arguments = Seq("create") ++ List(inputPath) ++ Seq("-o", outputPath) ++ config.cmdLineParams
     runShellCommand(command, arguments).map(_ => outputPath)
   }
