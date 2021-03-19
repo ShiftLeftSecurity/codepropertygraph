ShiftLeftSecurity/codepropertygraph

commit 2c5589c284bf7c1cb5e073ca6991f8d04b93b5cd

Tests for MEMBER and CALL nodes (#1111)
* Testing for MEMBER nodes

* Add another test

* Test cleanup

* Cleanup of CALL + tests

* One more test

fuzzyc2cpg/src/main/scala/io/shiftleft/fuzzyc2cpg/passes/astcreation/AstCreator.scala
@@ -647,7 +647,8 @@ private[astcreation] class AstCreator(diffGraph: DiffGraph.Builder,
         nodes.NewMember(
           code = identifierDecl.getEscapedCodeStr,
           name = identifierDecl.getName.getEscapedCodeStr,
-          typeFullName = registerType(declTypeName)
+          typeFullName = registerType(declTypeName),
+          order = context.childNum
         )
       diffGraph.addNode(member)
       connectAstChild(member)
@@ -889,8 +890,7 @@ private[astcreation] class AstCreator(diffGraph: DiffGraph.Builder,
     nodes.NewCall(
       name = methodName,
       dispatchType = DispatchTypes.STATIC_DISPATCH.name(),
-      signature = "TODO assignment signature",
-      typeFullName = registerType(Defines.anyTypeName),
+      signature = "TODO",
       methodFullName = methodName,
       code = astNode.getEscapedCodeStr,
       order = context.childNum,

fuzzyc2cpg-tests/src/test/scala/io/shiftleft/fuzzyc2cpg/querying/NodeTypeStartersTests.scala
@@ -66,7 +66,7 @@ class NodeTypeStartersTests extends FuzzyCCodeToCpgSuite {
   }
 
   "should allow retrieving (used) types" in {
-    cpg.typ.name.toSet shouldBe Set("ANY", "int", "void", "char * *")
+    cpg.typ.name.toSet shouldBe Set("int", "void", "char * *")
   }
 
   "should allow retrieving namespaces" in {

fuzzyc2cpg-tests/src/test/scala/io/shiftleft/fuzzyc2cpg/querying/StructureTests.scala
@@ -1,33 +1,0 @@
-package io.shiftleft.fuzzyc2cpg.querying
-
-import io.shiftleft.fuzzyc2cpg.testfixtures.FuzzyCCodeToCpgSuite
-import io.shiftleft.semanticcpg.language._
-
-class StructureTests extends FuzzyCCodeToCpgSuite {
-
-  override val code = """
-                   | struct foo {};
-                   | int main() {}
-                 """.stripMargin
-
-  "should return one source file" in {
-    cpg.file.name.l.count(_.endsWith(".c")) shouldBe 1
-  }
-
-  "should allow traversing from file to namespace blocks" in {
-    cpg.file.namespaceBlock.name.toSet shouldBe Set("<global>")
-  }
-
-  "should allow traversing to namespaces" in {
-    cpg.file.namespace.name("<global>").l.size shouldBe 2
-  }
-
-  "should allow traversing to type declarations" in {
-    cpg.file.typeDecl.name.toSet shouldBe Set("foo", "void", "int")
-  }
-
-  "should allow traversing to methods in namespaces" in {
-    cpg.file.namespace.method.name.toSet shouldBe Set("main")
-  }
-
-}

fuzzyc2cpg-tests/src/test/scala/io/shiftleft/fuzzyc2cpg/standard/CallTests.scala
@@ -1,0 +1,68 @@
+package io.shiftleft.fuzzyc2cpg.standard
+
+import io.shiftleft.codepropertygraph.generated.{Operators, nodes}
+import io.shiftleft.fuzzyc2cpg.testfixtures.FuzzyCCodeToCpgSuite
+import io.shiftleft.semanticcpg.language.NoResolve
+import io.shiftleft.semanticcpg.language._
+
+class CallTests extends FuzzyCCodeToCpgSuite {
+
+  implicit val resolver = NoResolve
+
+  override val code = """
+       int add(int x, int y) {
+         return x + y;
+       }
+       int main(int argc, char **argv) {
+         printf("%d\n", add((1+2), 3));
+       }
+    """
+
+  "should contain a call node for `add` with correct fields" in {
+    val List(x) = cpg.call("add").l
+    x.code shouldBe "add((1+2), 3)"
+    x.name shouldBe "add"
+    x.order shouldBe 2
+    x.methodInstFullName shouldBe None // Deprecated
+    x.methodFullName shouldBe "add"
+    x.argumentIndex shouldBe 2
+    // TODO x.signature
+    // x.typeFullName : deprecated
+    x.lineNumber shouldBe Some(6)
+    x.columnNumber shouldBe Some(24)
+  }
+
+  "should allow traversing from call to arguments" in {
+    cpg.call("add").argument.size shouldBe 2
+
+    val List(arg1) = cpg.call("add").argument(1).l
+    arg1.isInstanceOf[nodes.Call] shouldBe true
+    arg1.asInstanceOf[nodes.Call].name shouldBe Operators.addition
+    arg1.code shouldBe "1+2"
+    arg1.order shouldBe 1
+    arg1.argumentIndex shouldBe 1
+
+    val List(arg2) = cpg.call("add").argument(2).l
+    arg2.isInstanceOf[nodes.Literal] shouldBe true
+    arg2.asInstanceOf[nodes.Literal].code shouldBe "3"
+    arg2.code shouldBe "3"
+    arg2.order shouldBe 2
+    arg2.argumentIndex shouldBe 2
+  }
+
+  "should allow traversing from call to surrounding method" in {
+    val List(x) = cpg.call("add").method.l
+    x.name shouldBe "main"
+  }
+
+  "should allow traversing from call to callee method" in {
+    val List(x) = cpg.call("add").callee.l
+    x.name shouldBe "add"
+  }
+
+  "should allow traversing from argument to parameter" in {
+    val List(x) = cpg.call("add").argument(1).parameter.l
+    x.name shouldBe "x"
+  }
+
+}

fuzzyc2cpg-tests/src/test/scala/io/shiftleft/fuzzyc2cpg/standard/FileTests.scala
@@ -44,4 +44,8 @@ class FileTests extends FuzzyCCodeToCpgSuite {
     cpg.file.nameNot(File.UNKNOWN).typeDecl.name.toSet shouldBe Set("my_struct")
   }
 
+  "should allow traversing to namespaces" in {
+    cpg.file.namespace.name("<global>").l.size shouldBe 2
+  }
+
 }

fuzzyc2cpg-tests/src/test/scala/io/shiftleft/fuzzyc2cpg/standard/MemberTests.scala
@@ -1,0 +1,28 @@
+package io.shiftleft.fuzzyc2cpg.standard
+
+import io.shiftleft.fuzzyc2cpg.testfixtures.FuzzyCCodeToCpgSuite
+import io.shiftleft.semanticcpg.language._
+
+class MemberTests extends FuzzyCCodeToCpgSuite {
+
+  override val code =
+    """
+      |struct foo {
+      |  int x;
+      |}
+      |""".stripMargin
+
+  "should contain MEMBER node with correct properties" in {
+    val List(x) = cpg.member.l
+    x.name shouldBe "x"
+    x.code shouldBe "x"
+    x.typeFullName shouldBe "int"
+    x.order shouldBe 1
+  }
+
+  "should allow traversing from MEMBER to TYPE_DECL" in {
+    val List(x) = cpg.member.typeDecl.l
+    x.name shouldBe "foo"
+  }
+
+}

commit 6ea7fbd856bb22b0e31ff783abbe4b7e3eb2409a

restore ANY type in fuzzyc2cpg (#1153)
Flow display in ocular/codescience and the web-ui requires that many nodes have a type. This restores the ANY type for cases where we have nothing better. The bug was introduced in commit 2c5589c284bf7c1cb5e073ca6991f8d04b93b5cd (tag: v1.3.60, tag: v1.3.59)

fuzzyc2cpg/src/main/scala/io/shiftleft/fuzzyc2cpg/passes/astcreation/AstCreator.scala
@@ -898,6 +898,7 @@ private[astcreation] class AstCreator(diffGraph: DiffGraph.Builder,
       .dispatchType(DispatchTypes.STATIC_DISPATCH.name())
       .signature("TODO")
       .methodFullName(methodName)
+      .typeFullName(registerType(Defines.anyTypeName))
       .code(astNode.getEscapedCodeStr)
       .order(context.childNum)
       .argumentIndex(context.childNum)

fuzzyc2cpg-tests/src/test/scala/io/shiftleft/fuzzyc2cpg/querying/NodeTypeStartersTests.scala
@@ -66,7 +66,7 @@ class NodeTypeStartersTests extends FuzzyCCodeToCpgSuite {
   }
 
   "should allow retrieving (used) types" in {
-    cpg.typ.name.toSet shouldBe Set("int", "void", "char * *")
+    cpg.typ.name.toSet shouldBe Set("ANY", "int", "void", "char * *")
   }
 
   "should allow retrieving namespaces" in {