2a80c805ec3a2bdfdccccdcf10a5983536f3f107
Claudiu-Vlad Ursache <claudiu+hw@shiftleft.io>
2021-03-24 21:07:11 +0100

    Add `traversalWithMeta` macro

--- codepropertygraph/src/main/scala/io/shiftleft/console/Query.scala
+++ codepropertygraph/src/main/scala/io/shiftleft/console/Query.scala
@@ -13,3 +13,26 @@ case class Query(name: String,
                  traversalAsString: String = "",
                  tags: List[String] = List(),
                  language: String = "")
+
+case class TraversalWithMeta(traversal: Cpg => Traversal[nodes.StoredNode], traversalAsString: String = "")
+
+object QueryFactory {
+  def create(name: String,
+             author: String,
+             title: String,
+             description: String,
+             score: Double,
+             traversalWithMeta: TraversalWithMeta,
+             tags: List[String] = List()): Query = {
+    Query(
+      name = name,
+      author = author,
+      title = title,
+      description = description,
+      score = score,
+      traversal = traversalWithMeta.traversal,
+      traversalAsString = traversalWithMeta.traversalAsString,
+      tags = tags,
+    )
+  }
+}
--- macros/src/main/scala/io/shiftleft/macros/Macros.scala
+++ macros/src/main/scala/io/shiftleft/macros/Macros.scala
@@ -2,13 +2,29 @@ package io.shiftleft.macros
 
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.codepropertygraph.generated.nodes
-import io.shiftleft.console.Query
+import io.shiftleft.console.{Query, TraversalWithMeta}
 import overflowdb.traversal.Traversal
 
 import scala.language.experimental.macros
 import scala.reflect.macros.whitebox
 
 object QueryMacros {
+
+  def traversalWithMeta(traversal: Cpg => Traversal[nodes.StoredNode]): TraversalWithMeta = macro traversalWithMetaImp
+
+  def traversalWithMetaImp(c: whitebox.Context)(traversal: c.Tree): c.Expr[TraversalWithMeta] = {
+    import c.universe._
+    val fileContent = new String(traversal.pos.source.content)
+    val start = traversal.pos.start
+    val end = traversal.pos.end
+    val traversalAsString: String = fileContent.slice(start, end)
+
+    c.Expr(
+      q"""
+        TraversalWithMeta($traversal, $traversalAsString)
+       """
+    )
+  }
 
   def queryInit(name: String,
                 author: String,
