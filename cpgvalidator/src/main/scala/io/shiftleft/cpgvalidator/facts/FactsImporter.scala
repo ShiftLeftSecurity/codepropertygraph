f61e24f937aa62e44e82ce4705a13cf408fb629f
Michael Pollmeier <michael@michaelpollmeier.com>
2021-03-24 21:38:26 +1300

    fmt

--- cpgvalidator/src/main/scala/io/shiftleft/cpgvalidator/facts/FactsImporter.scala
+++ cpgvalidator/src/main/scala/io/shiftleft/cpgvalidator/facts/FactsImporter.scala
@@ -7,8 +7,7 @@ import play.api.libs.json.{JsValue, Json}
 abstract class FactsImporter {
 
   protected lazy val cpgJson: JsValue =
-    File(ProjectRoot.relativise("schema/target/scala-2.13/src_managed/main/cpg.json"))
-      .fileInputStream
+    File(ProjectRoot.relativise("schema/target/scala-2.13/src_managed/main/cpg.json")).fileInputStream
       .map(Json.parse)
       .get()
 
