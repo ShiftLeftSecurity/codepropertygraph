1428bee94ea367715c16afad64cb37177a595e82
Bernhard <bernhard@shiftleft.io>
2021-04-29 17:02:18 +0200

    ...

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/IsoHashPass.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/IsoHashPass.scala
@@ -24,23 +24,42 @@ object IsoHashPass {
   def getVersion(h: Long): Int = (h >>> 56).toInt
   def getVersion(): Int = VERSION
 
-  def computeHashes(method: nodes.Method): mutable.HashMap[nodes.StoredNode, Long] = {
-    val hashes = mutable.HashMap[nodes.StoredNode, Long]()
+  def computeHashes(method: nodes.Method): mutable.Map[nodes.StoredNode, Long] = {
+    /* Conceptually, we walk through the CFG forwards, then backwards and mix together hashes.
+     *
+     * Unfortunately there are two complications: First, some frontends emit METHOD within METHOD. This is an abomination,
+     * but we need to be able to deal with it.
+     *
+     * Second, there may be unreachable code, and non-returning (i.e. not reachable backwards from METHOD_RETURN) code.
+     * This is valid: Not because the code is really unreachable, but e.g. because of exception handlers.
+     *
+     * However, we want such code to participate in the isohash.
+     * */
+    val hashes = mutable.HashMap[nodes.StoredNode, Long]().withDefault(initHash)
     val visited = mutable.HashSet[nodes.StoredNode]()
     var current = mutable.ArrayBuffer[nodes.StoredNode]()
     var next = mutable.ArrayBuffer[nodes.StoredNode]()
 
     var done = false
-    hashes.put(method, initHash(method))
-    visited.add(method)
+    val totalSet = method._containsOut.asScala.filterNot { _.isInstanceOf[nodes.Method] }.to(mutable.HashSet)
+    totalSet.add(method)
+    totalSet.add(method.methodReturn)
+
     current.append(method)
+    for (node <- totalSet if !node._cfgIn.hasNext) {
+      visited.add(node)
+      current.append(node)
+    }
+    if (visited.add(method)) {
+      current.append(method)
+    }
 
     //we walk through the CFG forwards, in breadth-first-order, and propagate hash values
     while (!done) {
-      for (c <- current if !c.isInstanceOf[nodes.MethodReturn]) {
+      for (c <- current) {
         val h = fmix64(hashes(c))
         hashes(c) = h
-        for (nx <- c._cfgOut.asScala if !nx.isInstanceOf[nodes.Method]) {
+        for (nx <- c._cfgOut.asScala) {
           hashes.updateWith(nx) {
             case Some(hh) => Some(hh + h)
             case None     => Some(initHash(nx) + h)
@@ -58,16 +77,19 @@ object IsoHashPass {
     hashes.mapValuesInPlace { case (n, h) => fmix64(h) }
 
     done = false
-    val ret = method.methodReturn
-    hashes.getOrElseUpdate(ret, initHash(ret))
-    visited.add(ret)
-    current.append(ret)
+    for (node <- totalSet if !node._cfgOut.hasNext) {
+      visited.add(node)
+      current.append(node)
+    }
+    if (visited.add(method.methodReturn)) {
+      current.append(method.methodReturn)
+    }
     //we walk through the CFG backwards, in breadth-first-order, and propagate hash values
     while (!done) {
-      for (c <- current if !c.isInstanceOf[nodes.Method]) {
+      for (c <- current) {
         val h = fmix64(hashes(c))
         hashes(c) = h
-        for (nx <- c._cfgIn.asScala if !nx.isInstanceOf[nodes.MethodReturn]) {
+        for (nx <- c._cfgIn.asScala) {
           hashes.updateWith(nx) {
             case Some(hh) => Some(hh + h)
             case None     => Some(initHash(nx) + h)
@@ -82,11 +104,11 @@ object IsoHashPass {
       done = current.isEmpty
     }
 
-    //there might still be linear relationships, so kill them
-    hashes.mapValuesInPlace { case (n, h) => (fmix64(h) & VERSION_MASK) | VERSION_OR }
-    //the backwards pass has not propagated code that is postdominated by a throw, so let's fix that
-    val sum = hashes.valuesIterator.sum
-    hashes(method) = (fmix64(sum) & VERSION_MASK) | VERSION_OR
+    //there might still be linear relationships (from additions), so kill them
+    hashes.mapValuesInPlace { case (n, h) => fmix64(h) }
+    val sum = fmix64(hashes.valuesIterator.sum)
+    hashes.mapValuesInPlace { case (n, h) => ((h ^ sum) & VERSION_MASK) | VERSION_OR }
+    hashes.filterInPlace { case (n, h)    => totalSet.contains(n) }
     hashes
   }
 
@@ -104,6 +126,7 @@ object IsoHashPass {
         h = fmix64(h + n.name.hashCode)
       case n: nodes.Literal =>
         h = fmix64(h + n.code.hashCode)
+        n.method
       case n: nodes.MethodRef =>
         h = fmix64(h + n.methodFullName.hashCode)
       case n: nodes.TypeRef =>
@@ -149,8 +172,9 @@ class IsoHashPass(cpg: Cpg) extends CpgPass(cpg) {
       case forMethod: mutable.HashMap[nodes.StoredNode, Long] =>
         forMethod.foreach {
           case (n: nodes.CfgNode, h: Long) =>
-            dstGraph.addNodeProperty(n, PropertyNames.ISOHASH, java.lang.Long.valueOf(h))
-            hasHash.add(n)
+            if (hasHash.add(n)) {
+              dstGraph.addNodeProperty(n, PropertyNames.ISOHASH, java.lang.Long.valueOf(h))
+            }
           case _ =>
         }
     }
@@ -158,8 +182,10 @@ class IsoHashPass(cpg: Cpg) extends CpgPass(cpg) {
     //this especially means that code in exception handlers that rethrow the exception may be unvisited.
     //in order to avoid NPE, we zero the remaining ones.
     cpg.all.foreach {
-      case n: nodes.CfgNode if hasHash.add(n) =>
-        dstGraph.addNodeProperty(n, PropertyNames.ISOHASH, java.lang.Long.valueOf(0L))
+      case n: nodes.CfgNode =>
+        if (hasHash.add(n)) {
+          dstGraph.addNodeProperty(n, PropertyNames.ISOHASH, java.lang.Long.valueOf(0L))
+        }
       case _ =>
     }
     Iterator(dstGraph.build)
