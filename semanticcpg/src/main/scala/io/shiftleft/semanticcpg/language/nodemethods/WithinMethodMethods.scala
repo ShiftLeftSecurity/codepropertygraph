b5d104835276b438395f86f84fa632443cdea7f2
bbrehm <bernhard@shiftleft.io>
2021-04-29 17:28:07 +0200

    fix expansion jumptarget->method (#1196)

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/WithinMethodMethods.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/WithinMethodMethods.scala
@@ -5,20 +5,18 @@ import io.shiftleft.codepropertygraph.generated.nodes
 
 class WithinMethodMethods(val node: nodes.WithinMethod) extends AnyVal {
   def method: nodes.Method = node match {
-    case node: nodes.Method             => node
-    case node: nodes.MethodParameterIn  => walkUpAst(node)
-    case node: nodes.MethodParameterOut => walkUpAst(node)
-    case node: nodes.MethodReturn       => walkUpAst(node)
-    case node: nodes.ImplicitCall       => walkUpAst(node)
-    case node: nodes.PostExecutionCall  => walkUpAst(node)
-    case node: nodes.Expression         => expressionToMethod(node)
+    case node: nodes.Method => node
+    case _: nodes.MethodParameterIn | _: nodes.MethodParameterOut | _: nodes.MethodReturn | _: nodes.ImplicitCall |
+        _: nodes.PostExecutionCall =>
+      walkUpAst(node)
+    case _: nodes.Expression | _: nodes.JumpTarget => walkUpContains(node)
   }
 
   private def walkUpAst(node: nodes.WithinMethod): nodes.Method =
     node._astIn.onlyChecked.asInstanceOf[nodes.Method]
 
-  private def expressionToMethod(expression: nodes.Expression): nodes.Method =
-    expression._containsIn.onlyChecked match {
+  private def walkUpContains(node: nodes.StoredNode): nodes.Method =
+    node._containsIn.onlyChecked match {
       case method: nodes.Method => method
       case _: nodes.TypeDecl    =>
         // TODO - there are csharp CPGs that have typedecls here, which is invalid.
