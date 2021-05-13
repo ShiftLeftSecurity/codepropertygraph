b594d9bd901ddb572e8bad5c5f7e232f8ae4a67a
Fabian Yamaguchi <fabs@shiftleft.io>
2021-05-13 09:16:19 +0200

    Sort node types by (schemaName, name) + `isAbstract` field

--- schema2json/src/main/scala/Schema2Json.scala
+++ schema2json/src/main/scala/Schema2Json.scala
@@ -3,7 +3,7 @@ import org.json4s.JsonDSL._
 import org.json4s.native.JsonMethods._
 import org.json4s.native.Serialization
 import org.json4s.{Formats, JsonAST, NoTypeHints}
-import overflowdb.schema.AbstractNodeType
+import overflowdb.schema.{AbstractNodeType, NodeBaseType}
 
 object Schema2Json extends App {
 
@@ -27,13 +27,14 @@ object Schema2Json extends App {
 
   private def nodeTypesAsJson: Seq[JsonAST.JObject] =
     (schema.nodeTypes ++ schema.nodeBaseTypes)
-      .sortBy(x => schemaName(x))
+      .sortBy(x => (schemaName(x), x.name))
       .map { nodeType =>
         val baseTypeNames = nodeType.extendz.map(_.name)
         val allProperties = nodeType.properties.map(_.name)
         val inheritedProperties = nodeType.extendz.flatMap { base =>
           base.properties.map(_.name).map(x => (("baseType" -> base.name), ("name" -> x)))
         }
+
         val inheritedPropertyNames = inheritedProperties.map(_._2._2)
         val nonInheritedProperties = allProperties.filterNot(x => inheritedPropertyNames.contains(x))
         ("name" -> nodeType.name) ~
@@ -41,7 +42,8 @@ object Schema2Json extends App {
           ("extends" -> baseTypeNames) ~
           ("inheritedProperties" -> inheritedProperties.map(x => x._1 ~ x._2)) ~
           ("properties" -> nonInheritedProperties) ~
-          ("schema" -> schemaName(nodeType))
+          ("schema" -> schemaName(nodeType)) ~
+          ("isAbstract" -> nodeType.isInstanceOf[NodeBaseType])
       }
 
 }
