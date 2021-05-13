ebc12a94ebab3e8f36b5a7aa01d0a53831fc573a
Fabian Yamaguchi <fabs@shiftleft.io>
2021-05-13 06:56:17 +0200

    Sort node types by name

--- schema2json/src/main/scala/Schema2Json.scala
+++ schema2json/src/main/scala/Schema2Json.scala
@@ -11,25 +11,30 @@ object Schema2Json extends App {
   implicit val formats: AnyRef with Formats =
     Serialization.formats(NoTypeHints)
 
-  val json = schema.nodeTypes.map { nodeType =>
-    val schemaName = nodeType.schemaInfo.definedIn.map(_.getDeclaringClass.getSimpleName).getOrElse("unknown")
-
-    val baseTypeNames = nodeType.extendz.map(_.name)
-    val allProperties = nodeType.properties.map { prop =>
-      prop.name
+  val json = schema.nodeTypes
+    .map { nodeType =>
+      val schemaName = nodeType.schemaInfo.definedIn.map(_.getDeclaringClass.getSimpleName).getOrElse("unknown")
+      (nodeType, schemaName)
     }
-    val inheritedProperties = nodeType.extendz.flatMap { base =>
-      base.properties.map(_.name).map(x => (("baseType" -> base.name), ("name" -> x)))
+    .sortBy(_._2)
+    .map {
+      case (nodeType, schemaName) =>
+        val baseTypeNames = nodeType.extendz.map(_.name)
+        val allProperties = nodeType.properties.map { prop =>
+          prop.name
+        }
+        val inheritedProperties = nodeType.extendz.flatMap { base =>
+          base.properties.map(_.name).map(x => (("baseType" -> base.name), ("name" -> x)))
+        }
+        val inheritedPropertyNames = inheritedProperties.map(_._2._2)
+        val nonInheritedProperties = allProperties.filterNot(x => inheritedPropertyNames.contains(x))
+        ("name" -> nodeType.name) ~
+          ("comment" -> nodeType.comment) ~
+          ("extends" -> baseTypeNames) ~
+          ("inheritedProperties" -> inheritedProperties.map(x => x._1 ~ x._2)) ~
+          ("properties" -> nonInheritedProperties) ~
+          ("schema" -> schemaName)
     }
-    val inheritedPropertyNames = inheritedProperties.map(_._2._2)
-    val nonInheritedProperties = allProperties.filterNot(x => inheritedPropertyNames.contains(x))
-    ("name" -> nodeType.name) ~
-      ("comment" -> nodeType.comment) ~
-      ("extends" -> baseTypeNames) ~
-      ("inheritedProperties" -> inheritedProperties.map(x => x._1 ~ x._2)) ~
-      ("properties" -> nonInheritedProperties) ~
-      ("schema" -> schemaName)
-  }
 
   val outFileName = "/tmp/schema.json"
   better.files
