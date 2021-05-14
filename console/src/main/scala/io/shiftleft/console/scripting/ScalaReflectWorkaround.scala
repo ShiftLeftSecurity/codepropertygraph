de8cc6c319994b162e48344f35c63097be338c32
Markus Lottmann <markus@shiftleft.io>
2021-05-14 14:21:04 +0200

    Always run scala reflection workaround. (#1211)

--- console/src/main/scala/io/shiftleft/console/scripting/ScalaReflectWorkaround.scala
+++ console/src/main/scala/io/shiftleft/console/scripting/ScalaReflectWorkaround.scala
@@ -9,12 +9,20 @@ import scala.reflect.runtime.currentMirror
   * i asked to move the retry logic into scala reflect as a better way to handle this
   * https://github.com/scala/bug/issues/12038#issuecomment-760629585
   *
+  * AMMENDMENT:
+  * Actually doing this once is not always enough. Not sure why this is the case but if the fact
+  * reflection has happened "long enough" before the intend reflections, the bug can again be
+  * triggered. This was just hidden by the fact that the tests got executed in a forked process,
+  * effectively always resetting the "already worked around" flag.
+  * So we we now do this any time workaroundScalaReflectBugByTriggeringReflection() is called and
+  * should be fine.
+  * AND ALWAYS REMEMBER: Global state is the root of all evil.
+  *
   * to reproduce the issue, comment out this workaround, publish cpg locally and run the following in the codescience repo:
   * `cpg2sp/testOnly *PolicyAmmoniteExecutorTest`
   * if the above is green without this workaround, we don't need it any more
   */
 object ScalaReflectWorkaround {
-  var applied = false
 
   def fromJava(t: FileDescriptorProto): Unit = {
     println(t)
@@ -23,15 +31,12 @@ object ScalaReflectWorkaround {
   }
 
   def workaroundScalaReflectBugByTriggeringReflection() = {
-    if (!applied) {
-      try {
-        currentMirror
-          .reflectModule(currentMirror.staticModule("io.shiftleft.console.scripting.ScalaReflectWorkaround$"))
-          .instance
-      } catch {
-        case _: Throwable => // that's what we want to trigger - it happens the first time, then works - all good
-      }
-      applied = true
+    try {
+      currentMirror
+        .reflectModule(currentMirror.staticModule("io.shiftleft.console.scripting.ScalaReflectWorkaround$"))
+        .instance
+    } catch {
+      case _: Throwable => // that's what we want to trigger - it happens the first time, then works - all good
     }
   }
 }
