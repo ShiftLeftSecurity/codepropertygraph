703c47a07a8614304d7f82c874261f493d25b9ed
Fabian Yamaguchi <fabs@shiftleft.io>
2021-05-28 21:26:08 +0200

    Move Base down because it's super ugly

--- schema/src/main/scala/io/shiftleft/codepropertygraph/schema/Base.scala
+++ schema/src/main/scala/io/shiftleft/codepropertygraph/schema/Base.scala
@@ -5,7 +5,7 @@ import overflowdb.storage.ValueTypes
 
 object Base extends SchemaBase {
 
-  def index: Int = 1
+  def index: Int = Int.MaxValue - 1
   override def providedByFrontend: Boolean = true
   override def description: String =
     """
