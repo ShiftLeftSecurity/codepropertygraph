9e72fd269ddf8114ae853c69c1d27ed70f43e1a4
Fabian Yamaguchi <fabs@shiftleft.io>
2021-06-04 15:28:07 +0200

    Update test on FILE nodes

--- schema/src/main/scala/io/shiftleft/codepropertygraph/schema/FileSystem.scala
+++ schema/src/main/scala/io/shiftleft/codepropertygraph/schema/FileSystem.scala
@@ -7,6 +7,11 @@ object FileSystem extends SchemaBase {
   def index: Int = 3
   override def description: String =
     """
+      |CPGs are created from sets of files and the File System Layer describes the layout of
+      |these files, that is, it provides information about source files
+      |and shared objects for source-based and machine-code-based frontends
+      |respectively. The purpose of including this information in the CPG
+      |is to allow nodes of the graph to be mapped back to file system locations.
       |""".stripMargin
 
   def apply(builder: SchemaBuilder,
@@ -30,31 +35,30 @@ object FileSystem extends SchemaBase {
     val sourceFile = builder
       .addEdgeType(
         name = "SOURCE_FILE",
-        comment = "Source file of a node, in which its LINE_NUMBER and COLUMN_NUMBER are valid"
+        comment = "This edge connects a node to the node that represents it source file."
       )
       .protoId(157)
 
     val file: NodeType = builder
       .addNodeType(
         name = "FILE",
-        comment = """Node representing a source file - the root of the AST.
-                    |Code property graphs are created from sets of files.
-                    |Information about these files is stored in the graph to enable queries to map nodes
-                    |of the graph back to the files that contain the code they represent.
-                    |For each file, the graph must contain exactly one File.
-                    |As file nodes are root nodes of abstract syntax tress, they are AstNodes and their
-                    |order field is set to 0.
+        comment = """File nodes represent source files or a shared objects from which the CPG
+                    |was generated. File nodes serve as indices, that is, they allow looking up all
+                    |elements of the code by file.
                     |
-                    |Each code property graph must contain a special file node with name set to
+                    |For each file, the graph MUST contain exactly one File node.
+                    |As file nodes are root nodes of abstract syntax tress, they are AstNodes and
+                    |their order field is set to 0. This is because they have no sibling nodes,
+                    |not because they are the first node of the AST.
+                    |
+                    |Each CPG MUST contain a special file node with name set to
                     |"<unknown>". This node is a placeholder used in cases where a file cannot be
-                    |determined at compile time. As an example, consider the case where an external
-                    |type is introduced only at link time.
-                    |Conceptually file nodes serve as indices, e.g., they map all filenames to the
-                    |list of methods they contain.
+                    |determined at compile time. As an example, consider external library functions.
+                    |As their code is not available on CPG construction, the file name is unknown.
                     |
                     |File nodes MUST NOT be created by the language frontend. Instead, the language
-                    |frontend is assumed to fill out the FILENAME field wherever possible,
-                    |allowing File nodes to be created automatically when the semantic CPG layer is created.
+                    |frontend is assumed to fill out the `FILENAME` field wherever possible,
+                    |allowing File nodes to be created automatically upon first loading the CPG.
                     |""".stripMargin
       )
       .protoId(38)
