e33e9f4a0db075ce2ffb32b8f0d8fd26163f9580
Fabian Yamaguchi <fabs@shiftleft.io>
2021-06-17 08:18:10 +0200

    Fix build

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/metadata/MetaDataPass.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/metadata/MetaDataPass.scala
@@ -7,7 +7,7 @@ import io.shiftleft.semanticcpg.language.types.structure.{FileTraversal, Namespa
 
 /**
   * A pass that creates a MetaData node, specifying that this
-  * is a CPG for $language, and a NamespaceBlock for anything that
+  * is a CPG for language, and a NamespaceBlock for anything that
   * cannot be assigned to any other namespace.
   * */
 class MetaDataPass(cpg: Cpg, language: String, keyPool: Option[KeyPool] = None)
