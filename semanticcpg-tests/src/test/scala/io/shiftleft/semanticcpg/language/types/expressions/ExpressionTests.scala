3af17927fb74a41924bad998e9bf59e51cb35d01
Michael Pollmeier <michael@michaelpollmeier.com>
2021-06-22 15:11:34 +0200

    WIP

--- semanticcpg-tests/src/test/scala/io/shiftleft/semanticcpg/language/types/expressions/ExpressionTests.scala
+++ semanticcpg-tests/src/test/scala/io/shiftleft/semanticcpg/language/types/expressions/ExpressionTests.scala
@@ -2,7 +2,7 @@ package io.shiftleft.semanticcpg.language.types.expressions
 
 import io.shiftleft.codepropertygraph.generated.EdgeTypes
 import io.shiftleft.codepropertygraph.generated.nodes.Call
-import io.shiftleft.codepropertygraph.generated.traversal._
+// import io.shiftleft.codepropertygraph.generated.traversal._
 import io.shiftleft.semanticcpg.language._
 import io.shiftleft.semanticcpg.testing.MockCpg
 import org.scalatest.matchers.should.Matchers
