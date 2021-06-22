7463e8eb186fb4e227ec84c453ba0ce2af7a3b71
Fabian Yamaguchi <fabs@shiftleft.io>
2021-06-22 16:02:35 +0200

    fuzzyc2cpg: refactor command line parsing (#1269)
    * Upgrade to scopt 4.0.1
    
    * Use functional API
    
    * More cleanup
    
    * Cleanup X2CPG API

--- fuzzyc2cpg/src/main/scala/io/shiftleft/fuzzyc2cpg/FuzzyC2Cpg.scala
+++ fuzzyc2cpg/src/main/scala/io/shiftleft/fuzzyc2cpg/FuzzyC2Cpg.scala
@@ -1,15 +1,15 @@
 package io.shiftleft.fuzzyc2cpg
 
-import better.files.File
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.codepropertygraph.generated.Languages
 import io.shiftleft.fuzzyc2cpg.passes.{AstCreationPass, StubRemovalPass, TypeNodePass}
 import io.shiftleft.passes.IntervalKeyPool
 import io.shiftleft.semanticcpg.passes.CfgCreationPass
 import io.shiftleft.semanticcpg.passes.metadata.MetaDataPass
-import io.shiftleft.x2cpg.SourceFiles
+import io.shiftleft.x2cpg.X2Cpg.newEmptyCpg
+import io.shiftleft.x2cpg.{SourceFiles, X2Cpg, X2CpgConfig}
 import org.slf4j.LoggerFactory
-import overflowdb.{Config, Graph}
+import scopt.OParser
 
 import java.nio.file.Files
 import java.util.concurrent.ConcurrentHashMap
@@ -75,7 +75,7 @@ class FuzzyC2Cpg() {
     val typesKeyPool = new IntervalKeyPool(100, 1000100)
     val functionKeyPool = new IntervalKeyPool(1000100, Long.MaxValue)
 
-    val cpg = initCpg(optionalOutputPath)
+    val cpg = newEmptyCpg(optionalOutputPath)
     val sourceFileNames = SourceFiles.determine(sourcePaths, sourceFileExtensions)
 
     new MetaDataPass(cpg, Languages.C, Some(metaDataKeyPool)).createAndApply()
@@ -87,42 +87,72 @@ class FuzzyC2Cpg() {
     cpg
   }
 
-  /**
-    * Create an empty CPG, backed by the file at `optionalOutputPath` or
-    * in-memory if `optionalOutputPath` is empty.
-    * */
-  private def initCpg(optionalOutputPath: Option[String]): Cpg = {
-    val odbConfig = optionalOutputPath
-      .map { outputPath =>
-        val outFile = File(outputPath)
-        if (outputPath != "" && outFile.exists) {
-          logger.info("Output file exists, removing: " + outputPath)
-          outFile.delete()
-        }
-        Config.withDefaults.withStorageLocation(outputPath)
-      }
-      .getOrElse {
-        Config.withDefaults()
-      }
-
-    val graph = Graph.open(odbConfig,
-                           io.shiftleft.codepropertygraph.generated.nodes.Factories.allAsJava,
-                           io.shiftleft.codepropertygraph.generated.edges.Factories.allAsJava)
-    new Cpg(graph)
-  }
-
 }
 
 object FuzzyC2Cpg {
 
   private val logger = LoggerFactory.getLogger(classOf[FuzzyC2Cpg])
 
+  final case class Config(inputPaths: Set[String] = Set.empty,
+                          outputPath: String = X2CpgConfig.defaultOutputPath,
+                          sourceFileExtensions: Set[String] = Set(".c", ".cc", ".cpp", ".h", ".hpp"),
+                          includeFiles: Set[String] = Set.empty,
+                          includePaths: Set[String] = Set.empty,
+                          defines: Set[String] = Set.empty,
+                          undefines: Set[String] = Set.empty,
+                          preprocessorExecutable: String = "./fuzzypp/bin/fuzzyppcli")
+      extends X2CpgConfig[Config] {
+    lazy val usePreprocessor: Boolean =
+      includeFiles.nonEmpty || includePaths.nonEmpty || defines.nonEmpty || undefines.nonEmpty
+
+    override def withAdditionalInputPath(inputPath: String): Config = copy(inputPaths = inputPaths + inputPath)
+    override def withOutputPath(x: String): Config = copy(outputPath = x)
+  }
+
   def main(args: Array[String]): Unit = {
-    parseConfig(args) match {
+
+    val frontendSpecificOptions = {
+      val builder = OParser.builder[Config]
+      import builder._
+      OParser.sequence(
+        programName(classOf[FuzzyC2Cpg].getSimpleName),
+        opt[String]("out")
+          .text("(DEPRECATED use `output`) output filename")
+          .action { (x, c) =>
+            logger.warn("`--out` is DEPRECATED. Use `--output` instead")
+            c.withOutputPath(x)
+          },
+        opt[String]("source-file-ext")
+          .unbounded()
+          .text(
+            "source file extensions to include when gathering source files. Defaults are .c, .cc, .cpp, .h and .hpp")
+          .action((pat, cfg) => cfg.copy(sourceFileExtensions = cfg.sourceFileExtensions + pat)),
+        opt[String]("include")
+          .unbounded()
+          .text("header include files")
+          .action((incl, cfg) => cfg.copy(includeFiles = cfg.includeFiles + incl)),
+        opt[String]('I', "")
+          .unbounded()
+          .text("header include paths")
+          .action((incl, cfg) => cfg.copy(includePaths = cfg.includePaths + incl)),
+        opt[String]('D', "define")
+          .unbounded()
+          .text("define a name")
+          .action((d, cfg) => cfg.copy(defines = cfg.defines + d)),
+        opt[String]('U', "undefine")
+          .unbounded()
+          .text("undefine a name")
+          .action((u, cfg) => cfg.copy(undefines = cfg.undefines + u)),
+        opt[String]("preprocessor-executable")
+          .text("path to the preprocessor executable")
+          .action((s, cfg) => cfg.copy(preprocessorExecutable = s)),
+      )
+    }
+
+    X2Cpg.parseCommandLine(args, frontendSpecificOptions, Config()) match {
       case Some(config) =>
         try {
           val fuzzyc = new FuzzyC2Cpg()
-
           if (config.usePreprocessor) {
             fuzzyc.runWithPreprocessorAndOutput(
               config.inputPaths,
@@ -138,69 +168,14 @@ object FuzzyC2Cpg {
             val cpg = fuzzyc.runAndOutput(config.inputPaths, config.sourceFileExtensions, Some(config.outputPath))
             cpg.close()
           }
-
         } catch {
           case NonFatal(ex) =>
             logger.error("Failed to generate CPG.", ex)
             System.exit(1)
         }
-      case _ =>
+      case None =>
         System.exit(1)
     }
   }
-
-  final case class Config(inputPaths: Set[String] = Set.empty,
-                          outputPath: String = "cpg.bin",
-                          sourceFileExtensions: Set[String] = Set(".c", ".cc", ".cpp", ".h", ".hpp"),
-                          includeFiles: Set[String] = Set.empty,
-                          includePaths: Set[String] = Set.empty,
-                          defines: Set[String] = Set.empty,
-                          undefines: Set[String] = Set.empty,
-                          preprocessorExecutable: String = "./fuzzypp/bin/fuzzyppcli") {
-    lazy val usePreprocessor: Boolean =
-      includeFiles.nonEmpty || includePaths.nonEmpty || defines.nonEmpty || undefines.nonEmpty
-  }
-
-  def parseConfig(args: Array[String]): Option[Config] =
-    new scopt.OptionParser[Config](classOf[FuzzyC2Cpg].getSimpleName) {
-      arg[String]("<input-dir>")
-        .unbounded()
-        .text("source directories containing C/C++ code")
-        .action((x, c) => c.copy(inputPaths = c.inputPaths + x))
-      opt[String]("out")
-        .text("(DEPRECATED use `output`) output filename")
-        .action { (x, c) =>
-          logger.warn("`--out` is DEPRECATED. Use `--output` instead")
-          c.copy(outputPath = x)
-        }
-      opt[String]("output")
-        .abbr("o")
-        .text("output filename")
-        .action((x, c) => c.copy(outputPath = x))
-      opt[String]("source-file-ext")
-        .unbounded()
-        .text("source file extensions to include when gathering source files. Defaults are .c, .cc, .cpp, .h and .hpp")
-        .action((pat, cfg) => cfg.copy(sourceFileExtensions = cfg.sourceFileExtensions + pat))
-      opt[String]("include")
-        .unbounded()
-        .text("header include files")
-        .action((incl, cfg) => cfg.copy(includeFiles = cfg.includeFiles + incl))
-      opt[String]('I', "")
-        .unbounded()
-        .text("header include paths")
-        .action((incl, cfg) => cfg.copy(includePaths = cfg.includePaths + incl))
-      opt[String]('D', "define")
-        .unbounded()
-        .text("define a name")
-        .action((d, cfg) => cfg.copy(defines = cfg.defines + d))
-      opt[String]('U', "undefine")
-        .unbounded()
-        .text("undefine a name")
-        .action((u, cfg) => cfg.copy(undefines = cfg.undefines + u))
-      opt[String]("preprocessor-executable")
-        .text("path to the preprocessor executable")
-        .action((s, cfg) => cfg.copy(preprocessorExecutable = s))
-      help("help").text("display this help message")
-    }.parse(args, Config())
 
 }
