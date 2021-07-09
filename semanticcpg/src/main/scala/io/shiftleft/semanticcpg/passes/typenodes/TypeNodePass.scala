9abc09a1e1905b2fb1f6e9bc5ac7a8f6e4b41ee7
Fabian Yamaguchi <fabs@shiftleft.io>
2021-07-09 21:29:31 +0200

    Add comment

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/typenodes/TypeNodePass.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/typenodes/TypeNodePass.scala
@@ -4,6 +4,9 @@ import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.codepropertygraph.generated.nodes
 import io.shiftleft.passes.{CpgPass, DiffGraph, KeyPool}
 
+/**
+  * Creates a `TYPE` node for each type in `usedTypes`
+  * */
 class TypeNodePass(usedTypes: List[String], cpg: Cpg, keyPool: Option[KeyPool] = None)
     extends CpgPass(cpg, "types", keyPool) {
   override def run(): Iterator[DiffGraph] = {
