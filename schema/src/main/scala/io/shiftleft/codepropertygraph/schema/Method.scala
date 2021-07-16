0a61d7417ea0662eca93cf55f6d6537ea3f5cbf1
Michael Pollmeier <michael@michaelpollmeier.com>
2021-07-16 21:46:03 +0200

    fix compile and test

--- schema/src/main/scala/io/shiftleft/codepropertygraph/schema/Method.scala
+++ schema/src/main/scala/io/shiftleft/codepropertygraph/schema/Method.scala
@@ -1,6 +1,5 @@
 package io.shiftleft.codepropertygraph.schema
 
-import io.shiftleft.codepropertygraph.schema.CpgSchema.PropertyDefaults
 import overflowdb.schema.Property.ValueType
 import overflowdb.schema.{NodeType, SchemaBuilder, SchemaInfo}
 
