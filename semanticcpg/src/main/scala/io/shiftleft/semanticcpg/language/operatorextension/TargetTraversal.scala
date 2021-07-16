e99e1028cffb444cb401fe31bd57af38dcf33be5
Bernhard <bernhard@shiftleft.io>
2021-07-16 11:21:35 +0200

    document and deprecate isArrayAccess to arrayAccess, due to break of naming conventions

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/TargetTraversal.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/TargetTraversal.scala
@@ -5,5 +5,13 @@ import io.shiftleft.semanticcpg.language._
 import overflowdb.traversal.Traversal
 
 class TargetTraversal(val traversal: Traversal[Expression]) extends AnyVal {
-  def isArrayAccess: Traversal[opnodes.ArrayAccess] = traversal.flatMap(_.isArrayAccess)
+
+  /** arrayAccess traverses to all array accesses below in the AST. For example, when called on the assignment
+    *   `x = buf[idxs[i]];``
+    * then it will return two array accesses.
+    */
+  def arrayAccess: Traversal[opnodes.ArrayAccess] = traversal.flatMap(_.arrayAccess)
+
+  @deprecated("isArrayAccess is deprecated in favor if arrayAccess, due to counterintuitive naming")
+  def isArrayAccess: Traversal[opnodes.ArrayAccess] = arrayAccess
 }
