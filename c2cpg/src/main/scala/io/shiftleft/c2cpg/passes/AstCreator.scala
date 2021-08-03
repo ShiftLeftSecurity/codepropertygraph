e78de6951048100db766fa21359d164ebc9c3056
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2021-08-03 15:13:09 +0200

    Update AstCreator.scala

--- c2cpg/src/main/scala/io/shiftleft/c2cpg/passes/AstCreator.scala
+++ c2cpg/src/main/scala/io/shiftleft/c2cpg/passes/AstCreator.scala
@@ -420,7 +420,7 @@ class AstCreator(filename: String, global: Global, config: C2Cpg.Config) {
       astForParameter(p, order)
     }
 
-    val lastOrder = 2 + parameterAsts.size
+    val lastOrder = 1 + parameterAsts.size
     val r = Ast(methodNode)
       .withChildren(parameterAsts)
       .withChild(astForMethodBody(Option(functDef.getBody), lastOrder))
@@ -1267,7 +1267,7 @@ class AstCreator(filename: String, global: Global, config: C2Cpg.Config) {
       astForParameter(p, order)
     }
 
-    val lastOrder = 2 + parameterAsts.size
+    val lastOrder = 1 + parameterAsts.size
     val r = Ast(methodNode)
       .withChildren(parameterAsts)
       .withChild(astForMethodReturn(funcDecl, lastOrder, returnType))
