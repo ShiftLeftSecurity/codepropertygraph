eb444cd306f9936a42342fc7e1dc38bb768f3b73
bbrehm <bernhard@shiftleft.io>
2021-08-10 16:10:49 +0200

    clean up types and edge api for cfg analysis (#1336)
    * clean up types and edge api for cfg analysis

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/cfgdominator/ReverseCpgCfgAdapter.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/cfgdominator/ReverseCpgCfgAdapter.scala
@@ -1,16 +1,15 @@
 package io.shiftleft.semanticcpg.passes.cfgdominator
 
-import io.shiftleft.codepropertygraph.generated.EdgeTypes
-import overflowdb.Node
+import io.shiftleft.codepropertygraph.generated.nodes.StoredNode
 
 import scala.jdk.CollectionConverters._
 
-class ReverseCpgCfgAdapter extends CfgAdapter[Node] {
-  override def successors(node: Node): IterableOnce[Node] = {
-    node.in(EdgeTypes.CFG).asScala
+class ReverseCpgCfgAdapter extends CfgAdapter[StoredNode] {
+  override def successors(node: StoredNode): IterableOnce[StoredNode] = {
+    node._cfgIn.asScala
   }
 
-  override def predecessors(node: Node): IterableOnce[Node] = {
-    node.out(EdgeTypes.CFG).asScala
+  override def predecessors(node: StoredNode): IterableOnce[StoredNode] = {
+    node._cfgOut.asScala
   }
 }
