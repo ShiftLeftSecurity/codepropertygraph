bdf1b8d907d2538ac58ef385c19f90d4e656fb4c
Johannes Coetzee <jcoetzee@shiftleft.io>
2021-08-12 15:37:39 +0200

    Add binary test fixture and update c2cpg fixture for query-database compatibility

---
+++ semanticcpg-tests/src/test/scala/io/shiftleft/semanticcpg/testfixtures/BinToCpgFixture.scala
@@ -0,0 +1,37 @@
+package io.shiftleft.semanticcpg.testfixtures
+
+import io.shiftleft.codepropertygraph.Cpg
+import io.shiftleft.semanticcpg.layers.{LayerCreatorContext, Scpg}
+import io.shiftleft.utils.ProjectRoot
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import java.io.File
+
+class BinToCpgFixture(val frontend: LanguageFrontend) extends AnyWordSpec with Matchers with BeforeAndAfterAll {
+
+  var cpg: Cpg = Cpg.emptyCpg
+  val binDirectory = ProjectRoot.relativise("src/test/resources/testbinaries/")
+  def passes(cpg: Cpg): Unit = createEnhancements(cpg)
+
+  def createEnhancements(cpg: Cpg): Unit = {
+    val context = new LayerCreatorContext(cpg)
+    new Scpg().run(context)
+  }
+
+  def buildCpgForBin(binName: String): Unit = {
+    if (!cpg.graph.isClosed) {
+      cpg.close()
+    }
+    val bin = new File(binDirectory, binName)
+    cpg = frontend.execute(bin)
+    passes(cpg)
+  }
+
+  override def afterAll(): Unit = {
+    if (!cpg.graph.isClosed) {
+      cpg.close()
+    }
+  }
+}
