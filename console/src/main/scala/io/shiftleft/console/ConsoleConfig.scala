c7d6dc30fa94a488a42e33103f43881bf92f0c65
Michael Pollmeier <michael@michaelpollmeier.com>
2021-09-15 11:20:59 +0200

    fix search for root directory: start with lib dir, not the jar itself
    the previous version only worked with classes dir, but broke at runtime
    in joern/ocular when jars were used

--- console/src/main/scala/io/shiftleft/console/ConsoleConfig.scala
+++ console/src/main/scala/io/shiftleft/console/ConsoleConfig.scala
@@ -15,7 +15,7 @@ class InstallConfig(environment: Map[String, String] = sys.env) {
       environment("SHIFTLEFT_OCULAR_INSTALL_DIR").toFile
     } else {
       val uriToLibDir = classOf[io.shiftleft.console.InstallConfig].getProtectionDomain.getCodeSource.getLocation.toURI
-      val pathToLibDir = File(uriToLibDir)
+      val pathToLibDir = File(uriToLibDir).parent
       findRootDirectory(pathToLibDir).getOrElse(throw new AssertionError(s"""unable to find root installation directory
            | context: tried to find marker file `$rootDirectoryMarkerFilename`
            | started search in $pathToLibDir and searched $maxSearchDepth directories upwards""".stripMargin))
