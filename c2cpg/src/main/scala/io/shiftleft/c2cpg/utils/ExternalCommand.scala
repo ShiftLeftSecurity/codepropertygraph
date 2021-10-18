e080b437fa9aa074391651b5de5c85d74e63b277
Max Leuthäuser <max.leuthaeuser@gmail.com>
2021-10-18 10:24:51 +0200

    Removed unused stuff.

--- c2cpg/src/main/scala/io/shiftleft/c2cpg/utils/ExternalCommand.scala
+++ c2cpg/src/main/scala/io/shiftleft/c2cpg/utils/ExternalCommand.scala
@@ -1,30 +1,17 @@
 package io.shiftleft.c2cpg.utils
 
-import org.slf4j.LoggerFactory
-
-import java.io
 import scala.collection.mutable
 import scala.sys.process.{Process, ProcessLogger}
 import scala.util.{Failure, Success, Try}
 
 object ExternalCommand {
-  private val logger = LoggerFactory.getLogger(ExternalCommand.getClass)
-  private val windowsSystemPrefix = "Windows"
-  private val osNameProperty = "os.name"
+  private val windowsSystemPrefix: String = "Windows"
+  private val osNameProperty: String = "os.name"
+  private val systemString: String = System.getProperty(osNameProperty)
 
-  def run(command: String,
-          inDir: String = ".",
-          extraEnv: Map[String, String] = Map.empty,
-          debugLogging: Boolean = false): Try[Seq[String]] = {
+  def run(command: String): Try[Seq[String]] = {
     val result = mutable.ArrayBuffer.empty[String]
-    val lineHandler: String => Unit = line => {
-      if (debugLogging) {
-        logger.debug(s"\t\t$line")
-      }
-      result.addOne(line)
-    }
-
-    val systemString = System.getProperty(osNameProperty)
+    val lineHandler: String => Unit = result.addOne
     val shellPrefix =
       if (systemString != null && systemString.startsWith(windowsSystemPrefix)) {
         "cmd" :: "/c" :: Nil
@@ -32,13 +19,9 @@ object ExternalCommand {
         "sh" :: "-c" :: Nil
       }
 
-    Process(shellPrefix :+ command, new io.File(inDir), extraEnv.toList: _*)
-      .!(ProcessLogger(lineHandler, lineHandler)) match {
+    Process(shellPrefix :+ command).!(ProcessLogger(lineHandler, lineHandler)) match {
       case 0 =>
         Success(result.toSeq)
-      case code if debugLogging =>
-        logger.debug(s"\t- '$command' failed with exit code '$code': ${result.mkString(System.lineSeparator())}")
-        Failure(new RuntimeException(result.mkString(System.lineSeparator())))
       case _ =>
         Failure(new RuntimeException(result.mkString(System.lineSeparator())))
     }
