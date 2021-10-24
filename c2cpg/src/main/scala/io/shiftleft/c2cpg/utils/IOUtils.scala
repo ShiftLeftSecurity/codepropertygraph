f954480bc2c58d1ff635b6a38234d7dc372e6d22
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2021-10-24 10:18:24 +0200

    Fixed file reading / handling (#1480)
    For proper resource closing.

--- c2cpg/src/main/scala/io/shiftleft/c2cpg/utils/IOUtils.scala
+++ c2cpg/src/main/scala/io/shiftleft/c2cpg/utils/IOUtils.scala
@@ -2,30 +2,27 @@ package io.shiftleft.c2cpg.utils
 
 import org.eclipse.cdt.core.parser.FileContent
 
-import java.io.{BufferedReader, FileInputStream, InputStreamReader}
+import java.nio.charset.CodingErrorAction
 import java.nio.file.{Path, Paths}
-import scala.jdk.CollectionConverters._
+import scala.io.{Codec, Source}
+import scala.util.Using
 
 object IOUtils {
 
-  def readFile(file: Path): FileContent = {
-    val reader = new BufferedReader(new InputStreamReader(new FileInputStream(file.toString), "utf-8"))
-    val fileLines =
-      reader
-        .lines()
-        .iterator()
-        .asScala
-        .flatMap(_.toCharArray.appendedAll(System.lineSeparator().toCharArray))
-        .toArray
+  def readLinesInFile(filePathAsString: String): Seq[String] =
+    readFileAsFileContent(Paths.get(filePathAsString)).toString.split(System.lineSeparator()).toSeq
 
-    FileContent.create(file.toString, true, fileLines)
-  }
+  def readFileAsFileContent(file: Path): FileContent = {
+    val decoder = Codec.UTF8.decoder.onMalformedInput(CodingErrorAction.REPLACE)
+    Using.resource(Source.fromFile(file.toString)(decoder)) { reader =>
+      val fileLines =
+        reader
+          .getLines()
+          .flatMap(_.toCharArray.appendedAll(System.lineSeparator().toCharArray))
+          .toArray
 
-  def readFile(filePathAsString: String): FileContent = readFile(Paths.get(filePathAsString))
-
-  def linesInFile(fileContent: FileContent): Seq[String] = {
-    val content = fileContent.toString
-    content.split(System.lineSeparator()).toSeq
+      FileContent.create(file.toString, true, fileLines)
+    }
   }
 
 }
