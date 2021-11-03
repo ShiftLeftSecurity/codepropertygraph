d95b3d5e973f4dd89e6590df215025a81a904d3b
Markus Lottmann <markus@shiftleft.io>
2021-11-03 12:50:20 +0100

    Removed depedency to small library.
    Remove dependency to a not very popular library and used scala standard
    library classes instead.
    
    Also updated scalatest and json4s dependency versions.

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/controlflow/codepencegraph/CdgPass.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/controlflow/codepencegraph/CdgPass.scala
@@ -37,24 +37,27 @@ class CdgPass(cpg: Cpg) extends ParallelCpgPass[Method](cpg) {
     val postDomFrontiers = dominanceFrontier.calculate(method :: cfgNodes)
 
     postDomFrontiers.foreach {
-      case (node, postDomFrontierNode) =>
-        postDomFrontierNode match {
-          case _: Literal | _: Identifier | _: Call | _: MethodRef | _: Unknown | _: ControlStructure |
-              _: JumpTarget => {
-            dstGraph.addEdgeInOriginal(postDomFrontierNode, node, EdgeTypes.CDG)
-          }
-          case _ =>
-            val nodeLabel = postDomFrontierNode.label
-            val containsIn = postDomFrontierNode._containsIn
-            if (containsIn == null || !containsIn.hasNext) {
-              logger.warn(s"Found CDG edge starting at $nodeLabel node. This is most likely caused by an invalid CFG.")
-            } else {
-              val method = containsIn.next
-              logger.warn(
-                s"Found CDG edge starting at $nodeLabel node. This is most likely caused by an invalid CFG." +
-                  s" Method: ${method match { case m: Method => m.fullName; case other => other.label }}" +
-                  s" number of outgoing CFG edges from $nodeLabel node: ${postDomFrontierNode._cfgOut.asScala.size}")
+      case (node, postDomFrontierNodes) =>
+        postDomFrontierNodes.foreach { postDomFrontierNode =>
+          postDomFrontierNode match {
+            case _: Literal | _: Identifier | _: Call | _: MethodRef | _: Unknown | _: ControlStructure |
+                _: JumpTarget => {
+              dstGraph.addEdgeInOriginal(postDomFrontierNode, node, EdgeTypes.CDG)
             }
+            case _ =>
+              val nodeLabel = postDomFrontierNode.label
+              val containsIn = postDomFrontierNode._containsIn
+              if (containsIn == null || !containsIn.hasNext) {
+                logger.warn(
+                  s"Found CDG edge starting at $nodeLabel node. This is most likely caused by an invalid CFG.")
+              } else {
+                val method = containsIn.next
+                logger.warn(
+                  s"Found CDG edge starting at $nodeLabel node. This is most likely caused by an invalid CFG." +
+                    s" Method: ${method match { case m: Method => m.fullName; case other => other.label }}" +
+                    s" number of outgoing CFG edges from $nodeLabel node: ${postDomFrontierNode._cfgOut.asScala.size}")
+              }
+          }
         }
     }
     Iterator(dstGraph.build())
