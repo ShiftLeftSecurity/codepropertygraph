c2489dc5b381c0651c82afb8b68aeafba84d0c3d
David Baker Effendi <davidbakereffendi@hotmail.com>
2021-11-11 11:54:05 +0200

    Adding missing imports

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/layers/CallGraph.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/layers/CallGraph.scala
@@ -1,7 +1,9 @@
 package io.shiftleft.semanticcpg.layers
 
 import io.shiftleft.codepropertygraph.Cpg
+import io.shiftleft.codepropertygraph.generated.Languages
 import io.shiftleft.passes.CpgPassBase
+import io.shiftleft.semanticcpg.language._
 import io.shiftleft.semanticcpg.passes.callgraph.{DynamicCallLinker, MethodRefLinker, StaticCallLinker}
 
 import scala.annotation.nowarn
