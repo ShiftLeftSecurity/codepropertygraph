a82fc58b018760d5085adde741fbd10ca90edec2
Markus Lottmann <markus@shiftleft.io>
2021-12-06 17:45:52 +0100

    Code adjustments to make code compile with scala 2 and 3.

--- schema/src/main/scala/io/shiftleft/codepropertygraph/schema/Protogen.scala
+++ schema/src/main/scala/io/shiftleft/codepropertygraph/schema/Protogen.scala
@@ -5,26 +5,28 @@ import overflowdb.schema.{ProtoOptions, SchemaBuilder}
 
 import java.io.File
 
-object Protogen extends App {
-  val outputDir =
-    args.headOption.map(new File(_)).getOrElse(throw new AssertionError("please pass outputDir as first parameter"))
+object Protogen {
+  def main(args: Array[String]): Unit = {
+    val outputDir =
+      args.headOption.map(new File(_)).getOrElse(throw new AssertionError("please pass outputDir as first parameter"))
 
-  val builder = new SchemaBuilder(domainShortName = "Cpg", basePackage = "io.shiftleft.codepropertygraph.generated")
+    val builder = new SchemaBuilder(domainShortName = "Cpg", basePackage = "io.shiftleft.codepropertygraph.generated")
 
-  builder.protoOptions(
-    ProtoOptions(
-      pkg = "cpg",
-      javaOuterClassname = "Cpg",
-      javaPackage = "io.shiftleft.proto.cpg",
-      goPackage = "github.com/ShiftLeftSecurity/proto/cpg",
-      csharpNamespace = "io.shiftleft.proto.cpg",
-      uncommonProtoEnumNameMappings = Map(
-        "Languages" -> "LANGUAGES",
-        "ControlStructureTypes" -> "CONTROL_STRUCTURE_TYPES",
-        "Frameworks" -> "FRAMEWORKS",
-      )
-    ))
-  val cpgSchema = new CpgSchema(builder)
+    builder.protoOptions(
+      ProtoOptions(
+        pkg = "cpg",
+        javaOuterClassname = "Cpg",
+        javaPackage = "io.shiftleft.proto.cpg",
+        goPackage = "github.com/ShiftLeftSecurity/proto/cpg",
+        csharpNamespace = "io.shiftleft.proto.cpg",
+        uncommonProtoEnumNameMappings = Map(
+          "Languages" -> "LANGUAGES",
+          "ControlStructureTypes" -> "CONTROL_STRUCTURE_TYPES",
+          "Frameworks" -> "FRAMEWORKS",
+        )
+      ))
+    new CpgSchema(builder)
 
-  new ProtoGen(builder.build).run(outputDir)
+    new ProtoGen(builder.build).run(outputDir)
+  }
 }
