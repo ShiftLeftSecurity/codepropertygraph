ee21195c95457be751856d402d56d816ede70f11
Michael Pollmeier <michael@michaelpollmeier.com>
2021-12-19 09:56:55 +0100

    MethodReturn <-- cfg (toReturn) --- Return

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/MethodReturnMethods.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/MethodReturnMethods.scala
@@ -5,10 +5,6 @@ import io.shiftleft.semanticcpg.NodeExtension
 import io.shiftleft.semanticcpg.language.{HasLocation, LocationCreator, _}
 
 class MethodReturnMethods(val node: MethodReturn) extends AnyVal with NodeExtension with HasLocation {
-
-  def toReturn: Option[Return] =
-    node._returnViaCfgIn
-
   override def location: NewLocation = {
     LocationCreator(
       node,
