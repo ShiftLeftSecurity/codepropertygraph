57bf0e46cf7c18e40e643febbaedc4e584b19630
Fabian Yamaguchi <fabs@shiftleft.io>
2022-01-05 18:42:10 +0100

    Carry over `simpleName` from queryDB

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/ArrayAccessTraversal.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/ArrayAccessTraversal.scala
@@ -7,14 +7,7 @@ import overflowdb.traversal._
 class ArrayAccessTraversal(val traversal: Traversal[OpNodes.ArrayAccess]) extends AnyVal {
   def array: Traversal[Expression] = traversal.map(_.array)
   def offset: Traversal[Expression] = traversal.map(_.offset)
-  def subscripts: Traversal[Identifier] = traversal.flatMap(_.subscripts)
-
-  /**
-    * Determine all array accesses at constant numeric offsets, e.g.,
-    * `buf[10]` but not `buf[i + 10]`, and for simplicity, not even `buf[1+2]`,
-    * `buf[PROBABLY_A_CONSTANT]` or `buf[PROBABLY_A_CONSTANT + 1]`,
-    * or even `buf[PROBABLY_A_CONSTANT]`.
-    */
+  def subscripts: Traversal[Identifier] = traversal.flatMap(_.subscript)
   def usesConstantOffset: Traversal[OpNodes.ArrayAccess] = traversal.filter(_.usesConstantOffset)
-
+  def simpleName: Traversal[String] = traversal.flatMap(_.simpleName)
 }
--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/nodemethods/ArrayAccessMethods.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/nodemethods/ArrayAccessMethods.scala
@@ -6,18 +6,44 @@ import io.shiftleft.semanticcpg.language.operatorextension.OpNodes
 import overflowdb.traversal._
 
 class ArrayAccessMethods(val arrayAccess: OpNodes.ArrayAccess) extends AnyVal {
+
+  /**
+    * The expression representing the array
+    * */
   def array: Expression =
     arrayAccess.argument(1)
 
+  /**
+    * The expression representing the offset at which the array is referenced.
+    * */
   def offset: Expression = arrayAccess.argument(2)
 
-  def subscripts: Traversal[Identifier] =
+  /**
+    * All identifiers that are part of the offset
+    * */
+  def subscript: Traversal[Identifier] =
     offset.ast.isIdentifier
 
+  /**
+    * Determine if array access is at constant numeric offset, e.g.,
+    * `buf[10]` but not `buf[i + 10]`, and for simplicity, not even `buf[1+2]`,
+    * `buf[PROBABLY_A_CONSTANT]` or `buf[PROBABLY_A_CONSTANT + 1]`,
+    * or even `buf[PROBABLY_A_CONSTANT]`.
+    */
   def usesConstantOffset: Boolean = {
     (offset.ast.isIdentifier.size > 0) || {
       val literalIndices = offset.ast.isLiteral.l
       literalIndices.size == 1
+    }
+  }
+
+  /**
+    * If `array` is a lone identifier, return its name
+    */
+  def simpleName: Traversal[String] = {
+    arrayAccess.array match {
+      case id: Identifier => Traversal(id.name)
+      case _              => Traversal()
     }
   }
 
