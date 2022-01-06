2ba87ac198ce15fd0c2b9d3379a1b78e93995db6
Fabian Yamaguchi <fabs@shiftleft.io>
2022-01-06 18:45:01 +0100

    Stubs for FieldAccess extension

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/Implicits.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/Implicits.scala
@@ -7,16 +7,26 @@ import overflowdb.traversal._
 
 trait Implicits {
   implicit def toNodeTypeStartersOperatorExtension(cpg: Cpg): NodeTypeStarters = new NodeTypeStarters(cpg)
+
   implicit def toArrayAccessExt(arrayAccess: OpNodes.ArrayAccess): ArrayAccessMethods =
     new ArrayAccessMethods(arrayAccess)
   implicit def toArrayAccessTrav(steps: Traversal[OpNodes.ArrayAccess]): ArrayAccessTraversal =
     new ArrayAccessTraversal(steps)
+
+  implicit def toFieldAccessExt(fieldAccess: OpNodes.FieldAccess): FieldAccessMethods =
+    new FieldAccessMethods(fieldAccess)
+  implicit def toFieldAccessTrav(steps: Traversal[OpNodes.FieldAccess]): FieldAccessTraversal =
+    new FieldAccessTraversal(steps)
+
   implicit def toAssignmentExt(assignment: OpNodes.Assignment): AssignmentMethods = new AssignmentMethods(assignment)
   implicit def toAssignmentTrav(steps: Traversal[OpNodes.Assignment]): AssignmentTraversal =
     new AssignmentTraversal(steps)
+
   implicit def toTargetExt(call: Expression): TargetMethods = new TargetMethods(call)
   implicit def toTargetTrav(steps: Traversal[Expression]): TargetTraversal = new TargetTraversal(steps)
+
   implicit def toOpAstNodeExt[A <: AstNode](node: A): OpAstNodeMethods[A] = new OpAstNodeMethods(node)
   implicit def toOpAstNodeTrav[A <: AstNode](steps: Traversal[A]): OpAstNode[A] = new OpAstNode(steps)
+
   implicit def toOpMethodNodeTrav(steps: Traversal[Method]): OpMethodNode = new OpMethodNode(steps)
 }
