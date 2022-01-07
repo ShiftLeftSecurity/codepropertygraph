5a5f9b93a680a48012bda1808ed060a3b802b78b
Fabian Yamaguchi <fabs@shiftleft.io>
2022-01-07 19:02:12 +0100

    More work on field access traversal

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/nodemethods/FieldAccessMethods.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/nodemethods/FieldAccessMethods.scala
@@ -1,5 +1,42 @@
 package io.shiftleft.semanticcpg.language.operatorextension.nodemethods
 
+import io.shiftleft.codepropertygraph.generated.nodes.{
+  Call,
+  Expression,
+  FieldIdentifier,
+  Identifier,
+  Literal,
+  Member,
+  TypeDecl
+}
+import io.shiftleft.semanticcpg.language._
 import io.shiftleft.semanticcpg.language.operatorextension.OpNodes
+import overflowdb.traversal.{NodeOps, Traversal}
 
-class FieldAccessMethods(val arrayAccess: OpNodes.FieldAccess) extends AnyVal {}
+class FieldAccessMethods(val arrayAccess: OpNodes.FieldAccess) extends AnyVal {
+
+  def typeDecl: Traversal[TypeDecl] = resolveTypeDecl(arrayAccess.argument(1))
+
+  private def resolveTypeDecl(expr: Expression): Traversal[TypeDecl] = {
+    expr match {
+      case x: Identifier => x.typ.referencedTypeDecl
+      case x: Literal    => x.typ.referencedTypeDecl
+      case x: Call       => x.fieldAccess.member.typ.referencedTypeDecl
+      case _             => None
+    }
+  }
+
+  def fieldIdentifier: Traversal[FieldIdentifier] =
+    arrayAccess.start.argument(2).isFieldIdentifier
+
+  def member: Option[Member] = {
+    val base = typeDecl.headOption
+    val name = fieldIdentifier.map(_.canonicalName).headOption
+    (base, name) match {
+      case (Some(b), Some(n)) =>
+        b.member.nameExact(n).headOption
+      case _ => None
+    }
+  }
+
+}
--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/nodemethods/OpAstNodeMethods.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/nodemethods/OpAstNodeMethods.scala
@@ -2,7 +2,12 @@ package io.shiftleft.semanticcpg.language.operatorextension.nodemethods
 
 import io.shiftleft.codepropertygraph.generated.nodes.{AstNode, Call}
 import io.shiftleft.semanticcpg.language._
-import io.shiftleft.semanticcpg.language.operatorextension.{OpNodes, allArithmeticTypes, allAssignmentTypes}
+import io.shiftleft.semanticcpg.language.operatorextension.{
+  OpNodes,
+  allArithmeticTypes,
+  allAssignmentTypes,
+  allFieldAccessTypes
+}
 import overflowdb.traversal.{NodeOps, Traversal}
 
 class OpAstNodeMethods[A <: AstNode](val node: A) extends AnyVal {
@@ -12,11 +17,17 @@ class OpAstNodeMethods[A <: AstNode](val node: A) extends AnyVal {
       .filter(x => allAssignmentTypes.contains(x.name))
       .map(new OpNodes.Assignment(_))
 
+  // TODO I believe these are actually incorrectly named: these methods
+  // perform safe down casts, so, they should be called `is$Type`
+
   def assignment: Traversal[OpNodes.Assignment] =
     rawTravForPattern(allAssignmentTypes).map(new OpNodes.Assignment(_))
 
   def arithmetic: Traversal[OpNodes.Arithmetic] =
     rawTravForPattern(allArithmeticTypes).map(new OpNodes.Arithmetic(_))
+
+  def fieldAccess: Traversal[OpNodes.FieldAccess] =
+    rawTravForPattern(allFieldAccessTypes).map(new OpNodes.FieldAccess(_))
 
   private def rawTravForPattern(strings: Set[String]): Traversal[Call] =
     node.ast.isCall.filter(x => strings.contains(x.name))
