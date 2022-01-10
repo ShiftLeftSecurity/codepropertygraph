e84b75afb7dfc3328e89971a3868ea1a6bc480bd
Fabian Yamaguchi <fabs@shiftleft.io>
2022-01-10 23:00:04 +0100

    Fix build

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/nodemethods/FieldAccessMethods.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/nodemethods/FieldAccessMethods.scala
@@ -22,7 +22,7 @@ class FieldAccessMethods(val arrayAccess: OpNodes.FieldAccess) extends AnyVal {
       case x: Identifier => x.typ.referencedTypeDecl
       case x: Literal    => x.typ.referencedTypeDecl
       case x: Call       => x.fieldAccess.member.typ.referencedTypeDecl
-      case _             => None
+      case _             => Traversal()
     }
   }
 
