b1079635a29e4193adc4c345a99fd6104924dfc2
Fabian Yamaguchi <fabs@joern.io>
2022-03-04 13:09:03 +0100

    Remove passes.

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/passes/controlflow/cfgdominator/CfgAdapter.scala
+++
@@ -1,6 +0,0 @@
-package io.shiftleft.semanticcpg.passes.controlflow.cfgdominator
-
-trait CfgAdapter[Node] {
-  def successors(node: Node): IterableOnce[Node]
-  def predecessors(node: Node): IterableOnce[Node]
-}
