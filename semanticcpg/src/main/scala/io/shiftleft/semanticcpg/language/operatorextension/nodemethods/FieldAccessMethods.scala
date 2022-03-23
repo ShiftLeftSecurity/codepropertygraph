ac5bfee15b6fbe570e339c115af6b108483837bc
Malte Kraus <mkraus@shiftleft.io>
2022-03-23 14:01:52 +0100

    scalafmt

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/nodemethods/FieldAccessMethods.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/nodemethods/FieldAccessMethods.scala
@@ -1,10 +1,17 @@
 package io.shiftleft.semanticcpg.language.operatorextension.nodemethods
 
-import io.shiftleft.codepropertygraph.generated.nodes.{Call, Expression, FieldIdentifier, Identifier, Literal, Member, TypeDecl}
+import io.shiftleft.codepropertygraph.generated.nodes.{
+  Call,
+  Expression,
+  FieldIdentifier,
+  Identifier,
+  Literal,
+  Member,
+  TypeDecl
+}
 import io.shiftleft.semanticcpg.language._
 import io.shiftleft.semanticcpg.language.operatorextension.OpNodes
 import overflowdb.traversal.{NodeOps, Traversal, jIteratortoTraversal}
-
 
 class FieldAccessMethods(val arrayAccess: OpNodes.FieldAccess) extends AnyVal {
 
