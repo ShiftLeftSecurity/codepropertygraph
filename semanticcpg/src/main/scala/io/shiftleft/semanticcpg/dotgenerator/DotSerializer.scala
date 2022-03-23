01b7688b5aba4bc90f8abbab0f7fb03660d4e67f
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2022-03-23 09:47:26 +0100

    Another take on escape chars
    Previous version did break sptests.

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/dotgenerator/DotSerializer.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/dotgenerator/DotSerializer.scala
@@ -82,13 +82,7 @@ object DotSerializer {
   }
 
   private def escapedChar(ch: Char): String = ch match {
-    case '\b' => "\\b"
-    case '\t' => "\\t"
-    case '\n' => "\\n"
-    case '\f' => "\\f"
-    case '\r' => "\\r"
     case '"'  => "\\\""
-    case '\'' => "\\\'"
     case '\\' => "\\\\"
     case _ =>
       if (ch.isControl) "\\0" + Integer.toOctalString(ch.toInt)
