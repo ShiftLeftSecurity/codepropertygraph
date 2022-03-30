447d580b868832e62f6e52a9d4336c6fc5ed9985
Michael Pollmeier <michael@michaelpollmeier.com>
2022-03-30 10:53:02 +0200

    move semanticcpg to joern (#1618)
    sibling of https://github.com/joernio/joern/pull/1192

--- project/Projects.scala
+++ project/Projects.scala
@@ -5,6 +5,5 @@ object Projects {
   lazy val schema            = project.in(file("schema"))
   lazy val domainClasses     = project.in(file("domainClasses"))
   lazy val protoBindings     = project.in(file("proto-bindings"))
-  lazy val semanticcpg       = project.in(file("semanticcpg"))
   lazy val schema2json       = project.in(file("schema2json"))
 }
