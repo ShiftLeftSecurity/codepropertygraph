33db10c74eadd50440455a28144f5e8343b7dcb2
Max Leuthäuser <max.leuthaeuser@gmail.com>
2022-06-14 11:06:58 +0200

    Added Template DOM node to Schema

--- schema/src/main/scala/io/shiftleft/codepropertygraph/schema/Hidden.scala
+++ schema/src/main/scala/io/shiftleft/codepropertygraph/schema/Hidden.scala
@@ -99,6 +99,19 @@ object Hidden extends SchemaBase {
       .addOutEdge(edge = ref, inNode = local, cardinalityOut = EdgeType.Cardinality.One)
       .addOutEdge(edge = ref, inNode = methodParameterIn)
 
+    /* TemplateDOM
+     */
+
+    // node types
+    val templateDOM: NodeType = builder
+      .addNodeType(
+        name = "TEMPLATE_DOM",
+        comment = "This node represents a DOM node used in template languages, e.g., JSX/TSX"
+      )
+      .protoId(417)
+      .addProperties(name)
+      .extendz(expression)
+
     /*
      * Dependencies
      * */
