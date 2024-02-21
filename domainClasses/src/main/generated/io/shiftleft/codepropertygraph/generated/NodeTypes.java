18305bbc5cc502df1ab2f78271136cfea294c78a
Markus Lottmann <markus.lottmann@mlbox.net>
2024-02-21 16:43:40 +0100

    Correct the FILE node documentation to the reflect status quo.
    Status quo is encoded here: https://github.com/joernio/joern/blob/b272479ccf8264abffb1d7febe71c1d426833893/joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/passes/base/FileCreationPass.scala#L16

--- domainClasses/src/main/generated/io/shiftleft/codepropertygraph/generated/NodeTypes.java
+++ domainClasses/src/main/generated/io/shiftleft/codepropertygraph/generated/NodeTypes.java
@@ -98,19 +98,11 @@ public static final String FIELD_IDENTIFIER = "FIELD_IDENTIFIER";
 was generated. File nodes serve as indices, that is, they allow looking up all
 elements of the code by file.
 
-For each file, the graph MUST contain exactly one File node.
+For each file, the graph CAN contain exactly one File node, if not File nodes
+are created as indicated by `FILENAME` property of other nodes.
 As file nodes are root nodes of abstract syntax tress, they are AstNodes and
 their order field is set to 0. This is because they have no sibling nodes,
-not because they are the first node of the AST.
-
-Each CPG MUST contain a special file node with name set to
-`<unknown>`. This node is a placeholder used in cases where a file cannot be
-determined at compile time. As an example, consider external library functions.
-As their code is not available on CPG construction, the file name is unknown.
-
-File nodes MUST NOT be created by the language frontend. Instead, the language
-frontend is assumed to fill out the `FILENAME` field wherever possible,
-allowing File nodes to be created automatically upon first loading the CPG. */
+not because they are the first node of the AST. */
 public static final String FILE = "FILE";
 
 /** Finding nodes may be used to store analysis results in the graph
