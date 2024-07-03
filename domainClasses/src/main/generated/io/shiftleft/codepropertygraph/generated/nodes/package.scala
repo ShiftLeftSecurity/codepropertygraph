5c7d2b010eae730db7312859e26430c48e5e1d0c
Michael Pollmeier <michael@michaelpollmeier.com>
2024-07-03 14:08:02 +0200

    upgrade codegen

---
+++ domainClasses/src/main/generated/io/shiftleft/codepropertygraph/generated/nodes/package.scala
@@ -0,0 +1,11 @@
+package io.shiftleft.codepropertygraph
+
+package object generated {
+  // some type aliases so that the domain-specific code can avoid referencing the `overflowdb` namespace
+
+  object help {
+    type Doc             = _root_.overflowdb.traversal.help.Doc
+    type Traversal       = _root_.overflowdb.traversal.help.Traversal
+    type TraversalSource = _root_.overflowdb.traversal.help.TraversalSource
+  }
+}
