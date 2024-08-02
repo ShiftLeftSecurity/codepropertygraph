5804e6686f9f41531f51cec28b6e090245feb09f
Markus Lottmann <markus.lottmann@mlbox.net>
2024-08-02 14:58:23 +0200

    Deprecated SerializedCpg APIs. (#1781)
    This PR does not change behaviour.
    The serialization feature was internally not doing anything anymore for
    quite a while. Time to deprecated the API to eventually removed them.

--- codepropertygraph/src/test/scala/io/shiftleft/passes/CpgPassNewTests.scala
+++ codepropertygraph/src/test/scala/io/shiftleft/passes/CpgPassNewTests.scala
@@ -2,7 +2,6 @@ package io.shiftleft.passes
 
 import better.files.File
 import flatgraph.SchemaViolationException
-import io.shiftleft.SerializedCpg
 import io.shiftleft.codepropertygraph.generated.Cpg
 import io.shiftleft.codepropertygraph.generated.nodes.NewFile
 import io.shiftleft.codepropertygraph.generated.language.*
@@ -16,7 +15,7 @@ class CpgPassNewTests extends AnyWordSpec with Matchers {
   private object Fixture {
     def apply(f: (Cpg, CpgPassBase) => Unit): Unit = {
       val cpg = Cpg.empty
-      class MyPass(cpg: Cpg) extends SimpleCpgPass(cpg, "MyPass") {
+      class MyPass(cpg: Cpg) extends CpgPass(cpg, "MyPass") {
         override def run(builder: DiffGraphBuilder): Unit = {
           val builder2 = Cpg.newDiffGraphBuilder
           builder.addNode(NewFile().name("foo"))
@@ -35,21 +34,9 @@ class CpgPassNewTests extends AnyWordSpec with Matchers {
       cpg.all.label.toSet shouldBe Set("FILE")
     }
 
-    "produce a serialized CPG file" in Fixture { (_, pass) =>
-      File.usingTemporaryFile("pass", ".zip") { file =>
-        file.delete()
-        val filename      = file.path.toString
-        val serializedCpg = new SerializedCpg(filename)
-        pass.createApplySerializeAndStore(serializedCpg)
-        serializedCpg.close()
-        file.exists shouldBe true
-        Files.size(file.path) should not be 0
-      }
-    }
-
     "fail for schema violations" in {
       val cpg = Cpg.empty
-      val pass = new SimpleCpgPass(cpg, "pass1") {
+      val pass = new CpgPass(cpg, "pass1") {
         override def run(dst: DiffGraphBuilder): Unit = {
           val file1 = NewFile().name("foo")
           val file2 = NewFile().name("bar")
