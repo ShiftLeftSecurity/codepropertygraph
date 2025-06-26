7f1bd09e4ec8507da04cc2d8ec5f5e2846c75baf
Michael Pollmeier <michael@michaelpollmeier.com>
2025-06-26 12:00:00 +0200

    introduce TempFileCopy and use in test with flatgraphCpg
    fixes PR build, since flatgraph writes to the test file on '.close'
    which leads to changes in the repo, which is not permitted for PR builds

--- codepropertygraph/src/test/scala/io/shiftleft/codepropertygraph/cpgloading/CpgLoaderTests.scala
+++ codepropertygraph/src/test/scala/io/shiftleft/codepropertygraph/cpgloading/CpgLoaderTests.scala
@@ -3,7 +3,7 @@ package io.shiftleft.codepropertygraph.cpgloading
 import flatgraph.{Accessors, DiffGraphApplier}
 import io.shiftleft.codepropertygraph.generated.Cpg
 import io.shiftleft.codepropertygraph.generated.nodes.NewMethod
-import io.shiftleft.utils.ProjectRoot
+import io.shiftleft.utils.{ProjectRoot, TempFileCopy}
 import io.shiftleft.codepropertygraph.generated.nodes.Type
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.matchers.should.Matchers
@@ -44,16 +44,15 @@ class CpgLoaderTests extends AnyWordSpec with Matchers with BeforeAndAfterAll {
   }
 
   "allow loading of CPG in flatgraph format" in {
-    val flatgraphCpg = ProjectRoot.relativise("codepropertygraph/src/test/resources/cpg.fg")
-    Using.resource(CpgLoader.load(flatgraphCpg)) { cpg =>
-      // the test graph was created by c2cpg for https://github.com/joernio/joern/blob/master/tests/code/c/test.c
+    Using.Manager { use =>
+      val flatgraphCpg = use(temporaryFlatgraphCpg()).path
+      val cpg          = use(CpgLoader.load(flatgraphCpg))
       cpg.graph.nodes("METHOD").size shouldBe 4
-    }
+    }.get
   }
 
   "allow loading of CPG in flatgraph format and persist changes to separate file" in {
-    val flatgraphCpg = Paths.get(ProjectRoot.relativise("codepropertygraph/src/test/resources/cpg.fg"))
-    val persistTo    = Files.createTempFile(getClass.getSimpleName, "persistToTest")
+    val persistTo = Files.createTempFile(getClass.getSimpleName, "persistToTest")
 
     Using.resource(CpgLoader.load(flatgraphCpg, persistTo)) { cpg =>
       DiffGraphApplier.applyDiff(cpg.graph, Cpg.newDiffGraphBuilder.addNode(NewMethod()))
@@ -63,14 +62,22 @@ class CpgLoaderTests extends AnyWordSpec with Matchers with BeforeAndAfterAll {
       cpg.graph.nodes("METHOD").size shouldBe 5
     }
 
-    Using.resource(CpgLoader.load(flatgraphCpg)) { cpg =>
+    Using.Manager { use =>
+      val flatgraphCpg = use(temporaryFlatgraphCpg()).path
+      val cpg          = use(CpgLoader.load(flatgraphCpg))
       // original cpg should be unchanged
       cpg.graph.nodes("METHOD").size shouldBe 4
-    }
+    }.get
   }
 
   "throw an appropriate exception if the provided filename that refers to a non-existing file" in {
     a[FileNotFoundException] should be thrownBy CpgLoader.load("invalid/path/cpg.bin.zip")
   }
 
+  /** this test graph was created by c2cpg for https://github.com/joernio/joern/blob/master/tests/code/c/test.c */
+  val flatgraphCpg = Paths.get(ProjectRoot.relativise("codepropertygraph/src/test/resources/cpg.fg"))
+
+  /** flatgraph writes to this file on 'close', so we'll create a temporary copy */
+  private def temporaryFlatgraphCpg(): TempFileCopy =
+    TempFileCopy(flatgraphCpg)
 }
